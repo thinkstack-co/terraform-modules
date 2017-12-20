0d4783b05b06b01645fd3044490dafd154a292bd
Zachary Hill <zhill@thinkstack.co>
2017-12-20 15:59:56 -0500

    commented out instance_id as it is causing resources to recompute

--- modules/aws/route/route.tf
+++ modules/aws/route/route.tf
@@ -4,7 +4,7 @@ resource "aws_route" "route" {
   destination_ipv6_cidr_block = "${var.destination_ipv6_cidr_block}"
   egress_only_gateway_id      = "${var.egress_only_gateway_id}"
   gateway_id                  = "${var.gateway_id}"
-  instance_id                 = "${var.instance_id}"
+  # instance_id                 = "${var.instance_id}"
   nat_gateway_id              = "${var.nat_gateway_id}"
   network_interface_id        = "${element(var.network_interface_id, count.index)}"
   route_table_id              = "${element(var.route_table_id, count.index)}"
