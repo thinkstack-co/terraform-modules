c06d85c53812eb519aaeccfd22a77e5756cb9de7
Zachary Hill <zhill@thinkstack.co>
2017-12-20 13:27:22 -0500

    added aws_route module

---
+++ modules/aws/route/variables.tf
@@ -0,0 +1,51 @@
+variable "destination_cidr_block" {
+  type = "string"
+  description = " (Optional) The destination CIDR block."
+}
+
+variable "destination_ipv6_cidr_block" {
+  type = "string"
+  description = "(Optional) The destination IPv6 CIDR block."
+  default = ""
+}
+
+variable "egress_only_gateway_id" {
+  type = "string"
+  description = "(Optional) An ID of a VPC Egress Only Internet Gateway."
+  default = ""
+}
+
+variable "gateway_id" {
+  type = "string"
+  description = "(Optional) An ID of a VPC internet gateway or a virtual private gateway."
+  default = ""
+}
+
+variable "instance_id" {
+  type = "string"
+  description = "(Optional) An ID of an EC2 instance."
+  default = ""
+}
+
+variable "nat_gateway_id" {
+  type = "string"
+  description = "(Optional) An ID of a VPC NAT gateway."
+  default = ""
+}
+
+variable "network_interface_id" {
+  type = "string"
+  description = "(Optional) An ID of a network interface."
+  default = ""
+}
+
+variable "route_table_id" {
+  type = "string"
+  description = "(Required) The ID of the routing table."
+}
+
+variable "vpc_peering_connection_id" {
+  type = "string"
+  description = "(Optional) An ID of a VPC peering connection."
+  default = ""
+}
