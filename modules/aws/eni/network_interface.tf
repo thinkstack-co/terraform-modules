4848880a02e4c9e8810749917d755d5b2e54d5df
Zachary Hill <zhill@thinkstack.co>
2017-12-26 16:45:40 -0500

    reorganized eni module for variables to be alphabetically

--- modules/aws/eni/network_interface.tf
+++ modules/aws/eni/network_interface.tf
@@ -1,15 +1,15 @@
 resource "aws_network_interface" "eni" {
-  subnet_id           = "${var.subnet_id}"
+  attachment          = "${var.attachment}"
   private_ips         = "${var.private_ips}"
   private_ips_count   = "${var.private_ips_count}"
   security_groups     = "${var.security_groups}"
-  attachment          = "${var.attachment}"
   source_dest_check   = "${var.source_dest_check}"
+  subnet_id           = "${var.subnet_id}"
   tags                = "${var.tags}"
 }
 
 resource "aws_network_interface_attachment" "eni_attach" {
+  device_index         = "${var.device_index}"
   instance_id          = "${var.instance_id}"
   network_interface_id = "${aws_network_interface.eni.id}"
-  device_index         = "${var.device_index}"
 }
--- modules/aws/eni/variables.tf
+++ modules/aws/eni/variables.tf
@@ -1,6 +1,14 @@
-variable "subnet_id" {
-  type = "string"
-  description = "Subnet ID to create the eni in"
+variable "attachment" {
+  description = "Attachment block for assigning the eni to an instance"
+  default     = []
+}
+
+variable "device_index" {
+  description = "eni index to attach the eni to on the instance"
+}
+
+variable "instance_id" {
+  description = "instance ID to attach to the eni"
 }
 
 variable "private_ips" {
@@ -18,25 +26,17 @@ variable "security_groups" {
   default     = []
 }
 
-variable "attachment" {
-  description = "Attachment block for assigning the eni to an instance"
-  default     = []
-}
-
 variable "source_dest_check" {
   description = "Whether to enable source destination checking for the eni"
   default     = true
 }
 
+variable "subnet_id" {
+  type = "string"
+  description = "Subnet ID to create the eni in"
+}
+
 variable "tags" {
   description = "tags to assign to the eni"
   type        = "map"
-}
-
-variable "instance_id" {
-  description = "instance ID to attach to the eni"
-}
-
-variable "device_index" {
-  description = "eni index to attach the eni to on the instance"
 }
