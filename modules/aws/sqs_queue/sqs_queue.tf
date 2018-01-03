7604310b0cc2c5da1ff711bbfa26a7b953044c7b
Zachary Hill <zhill@thinkstack.co>
2018-01-03 10:36:47 -0500

    added the variables to sqs module

--- modules/aws/sqs_queue/sqs_queue.tf
+++ modules/aws/sqs_queue/sqs_queue.tf
@@ -1,5 +1,7 @@
 resource "aws_sqs_queue" "queue" {
-  delay_seconds "${var.delay_seconds}"
-  fifo_queue  = "${var.fifo_queue}"
-  name        = "${var.name}" 
+  delay_seconds             = "${var.delay_seconds}"
+  fifo_queue                = "${var.fifo_queue}"
+  message_retention_seconds = "${var.message_retention_seconds}"
+  name                      = "${var.name}" 
+  tags                      = "${var.tags}"
 }
