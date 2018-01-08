b74c11e850dac319e6538be894357eb5e3e8b0b7
Zachary Hill <zhill@thinkstack.co>
2018-01-08 16:03:59 -0500

    added more advanced example

--- modules/aws/ec2_instance/examples/main.tf
+++ modules/aws/ec2_instance/examples/main.tf
@@ -1,3 +1,6 @@
+################
+# Simple Example
+################
 module "aws_sql" {
     source                 = "github.com/thinkstack-co/terraform-modules//modules/aws/ec2_instance"
     
@@ -30,5 +33,62 @@ module "aws_sql" {
         role             = "sql"
         backup           = "true"
         hourly_retention = "7"
+    }
+}
+
+######################
+# Example using
+######################
+
+module "app_server" {
+    source                 = "github.com/thinkstack-co/terraform-modules//modules/aws/ec2_instance"
+    
+    name                   = "app_server"
+    ami                    = "ami-ffffffff"
+    count                  = 1
+    availability_zone      = "${module.vpc.availability_zone[0]}"
+    subnet_id              = "${module.vpc.private_subnet_ids[0]}"
+    instance_type          = "t2.large"
+    key_name               = "${module.keypair.key_name}"
+    vpc_security_group_ids = "${module.app_server_sg.id}"
+    root_volume_type       = "gp2"
+    root_volume_size       = "100"
+
+    volume_tags            = {
+        os_drive    = "c"
+        device_name = "/dev/sda1"
+        terraform   = "yes"
+        created_by  = "terraform"
+        environment = "prod"
+        role        = "app_server"
+        backup      = "true"
+    }
+
+    tags                   = {
+        terraform   = "yes"
+        created_by  = "terraform"
+        environment = "prod"
+        role        = "app_server"
+        backup      = "true"
+        hourly_retention    = "7"
+    }
+}
+
+module "app_server_d_drive" {
+    source = "github.com/thinkstack-co/terraform-modules//modules/aws/aws_ebs_volume"
+
+    availability_zone   = "${module.vpc.availability_zone[0]}"
+    size                = "50"
+    device_name         = "xvdb"
+    instance_id         = "${module.app_server.id[0]}"
+    tags                = {
+        Name        = "app_server"
+        os_drive    = "d"
+        device_name = "xvdb"
+        terraform   = "yes"
+        created_by  = "terraform"
+        environment = "prod"
+        role        = "app_server"
+        backup      = "true"
     }
 }
