8967a9639f3b8c03b1661ed9e7e734c3ce104c8b
Zachary Hill <zhill@thinkstack.co>
2018-01-11 10:54:20 -0500

    commented out availability_zone since it is redudant when using subnet_id

--- modules/aws/ec2_domain_controller/dc.tf
+++ modules/aws/ec2_domain_controller/dc.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "ec2_instance" {
     ami                                  = "${var.ami}"
-    availability_zone                    = "${element(var.availability_zone, count.index)}"
+    # This is redundant with the subnet_id option set. The subnet_id already defines an availability zone
+    # availability_zone                    = "${element(var.availability_zone, count.index)}"
     count                                = "${var.count}"
     disable_api_termination              = "${var.disable_api_termination}"
     ebs_optimized                        = "${var.ebs_optimized}"
