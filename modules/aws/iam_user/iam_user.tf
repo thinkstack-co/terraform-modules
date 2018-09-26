55ef6667239b95affb0a5201aae3511603f7fa6f
Zachary Hill <zhill@thinkstack.co>
2018-09-26 16:15:11 -0400

    added iam_user module

---
+++ modules/aws/iam_user/iam_user.tf
@@ -0,0 +1,6 @@
+resource "aws_iam_user" "this" {
+  force_destroy        = "${var.force_destroy}"
+  name                 = "${var.name}"
+  path                 = "${var.path}"
+  permissions_boundary = "${var.permissions_boundary}"
+}
---
+++ modules/aws/iam_user/outputs.tf
@@ -0,0 +1,7 @@
+output "arn" {
+  value = "${aws_iam_user.this.arn}"
+}
+
+output "unique_id" {
+  value = "${aws_iam_user.this.unique_id}"
+}
