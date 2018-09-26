d910775c4019287991ae89ab85c15d487da99d77
Zachary Hill <zhill@thinkstack.co>
2018-09-26 12:06:35 -0400

    iam_policy module

---
+++ modules/aws/iam_policy/outputs.tf
@@ -0,0 +1,7 @@
+output "arn" {
+  value = "${aws_iam_policy.this.arn}"
+}
+
+output "id" {
+  value = "${aws_iam_policy.this.id}"
+}
---
+++ modules/aws/iam_policy/variables.tf
@@ -0,0 +1,20 @@
+variable "description" {
+  type        = "string"
+  description = "(Optional, Forces new resource) Description of the IAM policy."
+}
+
+variable "name" {
+  type        = "string"
+  description = "(Optional, Forces new resource) The name of the policy. If omitted, Terraform will assign a random, unique name."
+}
+
+variable "path" {
+  type        = "string"
+  description = "(Optional, default '/') Path in which to create the policy. See IAM Identifiers for more information."
+  default     = "/"
+}
+
+variable "policy" {
+  type        = "string"
+  description = "(Required) The policy document. This is a JSON formatted string. The heredoc syntax, file function, or the aws_iam_policy_document data source are all helpful here."
+}
