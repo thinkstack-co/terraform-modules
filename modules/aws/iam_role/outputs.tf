6fc7d1f2314ca12551b8691051afcce7f315fb19
Zachary Hill <zhill@thinkstack.co>
2018-09-26 11:32:30 -0400

    outputs update to the iam_role module

--- modules/aws/iam_role/outputs.tf
+++ modules/aws/iam_role/outputs.tf
@@ -2,6 +2,10 @@ output "arn" {
   value = "${aws_iam_role.this.arn}"
 }
 
-output "id" {
-  value = "${aws_iam_role.this.id}"
+output "name" {
+  value = "${aws_iam_role.this.name}"
+}
+
+output "unique_id" {
+  value = "${aws_iam_role.this.unique_id}"
 }
