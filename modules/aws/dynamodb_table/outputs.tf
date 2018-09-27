5ceefa526d187d464845f29f316559de1499ec1b
Zachary Hill <zhill@thinkstack.co>
2018-09-27 07:33:15 -0400

    add dynamodb_table module

---
+++ modules/aws/dynamodb_table/outputs.tf
@@ -0,0 +1,15 @@
+output "arn" {
+  value = "${aws_dynamodb_table.this.arn}"
+}
+
+output "id" {
+  value = "${aws_dynamodb_table.this.id}"
+}
+
+output "stream_arn" {
+  value = "${aws_dynamodb_table.this.stream_arn}"
+}
+
+output "stream_label" {
+  value = "${aws_dynamodb_table.this.stream_label}"
+}
