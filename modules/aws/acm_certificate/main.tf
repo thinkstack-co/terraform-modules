7d801abf5e4a8a58aa9c7fc6ac1fc0f793202029
Zachary Hill <zhill@thinkstack.co>
2018-12-06 13:28:25 -0500

    adding aws_acm_certificate module

---
+++ modules/aws/acm_certificate/main.tf
@@ -0,0 +1,10 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name               = "${var.domain_name}"
+  validation_method         = "${var.validation_method}"
+  subject_alternative_names = "${var.subject_alternative_names}"
+  tags                      = "${var.tags}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ modules/aws/acm_certificate/variables.tf
@@ -0,0 +1,22 @@
+variable "domain_name" {
+  type = "string"
+  description = "(Required) A domain name for which the certificate should be issued"
+}
+
+variable "validation_method" {
+  type = "string"
+  description = "(Required) Which method to use for validation. DNS or EMAIL are valid, NONE can be used for certificates that were imported into ACM and then into Terraform."
+  default = "DNS"
+}
+
+variable "subject_alternative_names" {
+  type = "string"
+  description = "(Optional) A list of domains that should be SANs in the issued certificate"
+  default = ""
+}
+
+variable "tags" {
+  type = "map"
+  description = "(Optional) A mapping of tags to assign to the resource."
+  default = {}
+}
