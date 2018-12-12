9cee34fabbf48435a4677e9b50d6067c1619fc68
Zachary Hill <zhill@thinkstack.co>
2018-12-12 16:51:56 -0500

    new module ssm-service-role

---
+++ modules/aws/ssm_role/main.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "this" {
+  name               = "ssm-service-role"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "this_attach" {
+  role       = "${aws_iam_role.this.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
