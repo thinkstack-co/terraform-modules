21527b46037a924a2a9134823b381f82e76fb378
Zachary Hill <zhill@thinkstack.co>
2018-12-12 17:22:06 -0500

    removing leading space in the JSON policy

--- modules/aws/ssm_role/main.tf
+++ modules/aws/ssm_role/main.tf
@@ -1,19 +1,19 @@
 resource "aws_iam_role" "this" {
   name               = "ssm-service-role"
   assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
 EOF
 }
 
