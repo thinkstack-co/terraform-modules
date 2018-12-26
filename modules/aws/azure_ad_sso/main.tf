f0681a9d7aced9a33f9a6099899882f8f20e64a7
Zachary Hill <zhill@thinkstack.co>
2018-12-26 14:59:38 -0500

    resolved the identifier to point to the correct policy

--- modules/aws/azure_ad_sso/main.tf
+++ modules/aws/azure_ad_sso/main.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "thinkstack_azure_ad_assume_policy" {
 
     principals {
       type        = "Federated"
-      identifiers = ["${aws_iam_saml_provider.thinkstack_azure_ad_assume_policy.arn}"]
+      identifiers = ["${aws_iam_saml_provider.this.arn}"]
     }
 
     condition {
