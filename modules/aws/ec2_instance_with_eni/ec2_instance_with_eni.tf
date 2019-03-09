2e183d71a948af21c4386b3dcd72d62988266f6f
Zachary Hill <zhill@thinkstack.co>
2019-03-09 10:08:03 -0500

    added cloudwatch alarms to all possible ec2 modules

--- modules/aws/ec2_instance_with_eni/ec2_instance_with_eni.tf
+++ modules/aws/ec2_instance_with_eni/ec2_instance_with_eni.tf
@@ -57,3 +57,61 @@ resource "aws_instance" "ec2" {
     ignore_changes  = ["volume_tags"]
   }
 }
+
+###################################################
+# CloudWatch Alarms
+###################################################
+
+#####################
+# Status Check Failed Instance Metric
+#####################
+
+resource "aws_cloudwatch_metric_alarm" "instance" {
+  actions_enabled           = true
+  alarm_actions             = []
+  alarm_description         = "EC2 instance StatusCheckFailed_Instance alarm"
+  alarm_name                = "${format("%s-instance-alarm", element(aws_instance.ec2.*.id, count.index))}"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  count                     = "${var.count}"
+  datapoints_to_alarm       = 2
+  dimensions                = {
+    InstanceId = "${element(aws_instance.ec2.*.id, count.index)}"
+  }
+  evaluation_periods        = "2"
+  insufficient_data_actions = []
+  metric_name               = "StatusCheckFailed_Instance"
+  namespace                 = "AWS/EC2"
+  ok_actions                = []
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  treat_missing_data        = "missing"
+  #unit                      = "${var.unit}"
+}
+
+#####################
+# Status Check Failed System Metric
+#####################
+
+resource "aws_cloudwatch_metric_alarm" "system" {
+  actions_enabled           = true
+  alarm_actions             = ["arn:aws:automate:${var.region}:ec2:recover"]
+  alarm_description         = "EC2 instance StatusCheckFailed_System alarm"
+  alarm_name                = "${format("%s-system-alarm", element(aws_instance.ec2.*.id, count.index))}"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  count                     = "${var.count}"
+  datapoints_to_alarm       = 2
+  dimensions                = {
+    InstanceId = "${element(aws_instance.ec2.*.id, count.index)}"
+  }
+  evaluation_periods        = "2"
+  insufficient_data_actions = []
+  metric_name               = "StatusCheckFailed_System"
+  namespace                 = "AWS/EC2"
+  ok_actions                = []
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  treat_missing_data        = "missing"
+  #unit                      = "${var.unit}"
+}
