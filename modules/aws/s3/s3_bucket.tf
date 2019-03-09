885e4a77cf7f48815be75323d4a8f20f76bab5cb
Zachary Hill <zhill@thinkstack.co>
2019-03-09 09:29:04 -0500

    added s3 service side encryption enforcement

--- modules/aws/s3/s3_bucket.tf
+++ modules/aws/s3/s3_bucket.tf
@@ -13,7 +13,14 @@ resource "aws_s3_bucket" "s3_bucket" {
         prevent_destroy = true
     }
 
-    tags {
-        terraform    = "yes"
-    }
+    server_side_encryption_configuration {
+        rule {
+          apply_server_side_encryption_by_default {
+            kms_master_key_id = "${var.kms_master_key_id}"
+            sse_algorithm     = "${var.sse_algorithm}"
+          }
+        }
+      }
+
+    tags            = "${var.tags}"
 }
