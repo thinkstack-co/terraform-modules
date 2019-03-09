cc4c74fc7d201399a46d554cd5a5cc3f14d3c97e
Zachary Hill <zhill@thinkstack.co>
2019-03-09 09:59:34 -0500

    updated syntax error of ec2 to ec2_instance

--- modules/aws/ec2_domain_controller/dc.tf
+++ modules/aws/ec2_domain_controller/dc.tf
@@ -55,7 +55,7 @@ resource "aws_cloudwatch_metric_alarm" "instance" {
   actions_enabled           = true
   alarm_actions             = []
   alarm_description         = "EC2 instance StatusCheckFailed_Instance alarm"
-  alarm_name                = "${format("%s-instance-alarm", element(aws_instance.ec2.*.id, count.index))}"
+  alarm_name                = "${format("%s-instance-alarm", element(aws_instance.ec2_instance.*.id, count.index))}"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   count                     = "${var.count}"
   datapoints_to_alarm       = 2
@@ -82,7 +82,7 @@ resource "aws_cloudwatch_metric_alarm" "system" {
   actions_enabled           = true
   alarm_actions             = ["arn:aws:automate:${var.region}:ec2:recover"]
   alarm_description         = "EC2 instance StatusCheckFailed_System alarm"
-  alarm_name                = "${format("%s-system-alarm", element(aws_instance.ec2.*.id, count.index))}"
+  alarm_name                = "${format("%s-system-alarm", element(aws_instance.ec2_instance.*.id, count.index))}"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   count                     = "${var.count}"
   datapoints_to_alarm       = 2
