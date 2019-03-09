1fd87d525bb3971f7f826be1b0163bc4adbc91f2
Zachary Hill <zhill@thinkstack.co>
2019-03-09 09:57:26 -0500

    renamed ec2 to ec2_instance for continuity so Terraform doesn't rebuild the domain controllers

--- modules/aws/ec2_domain_controller/outputs.tf
+++ modules/aws/ec2_domain_controller/outputs.tf
@@ -1,25 +1,25 @@
 output "ec2_instance_id" {
-  value = ["${aws_instance.ec2.*.id}"]
+  value = ["${aws_instance.ec2_instance.*.id}"]
 }
 
 output "ec2_instance_priv_ip" {
-    value = ["${aws_instance.ec2.*.private_ip}"]
+    value = ["${aws_instance.ec2_instance.*.private_ip}"]
 }
 
 output "ec2_instance_pub_ip" {
-    value = ["${aws_instance.ec2.*.public_ip}"]
+    value = ["${aws_instance.ec2_instance.*.public_ip}"]
 }
 
 output "ec2_instance_network_id" {
-    value = ["${aws_instance.ec2.*.network_interface_id}"]
+    value = ["${aws_instance.ec2_instance.*.network_interface_id}"]
 }
 
 output "ec2_instance_subnet_id" {
-    value = ["${aws_instance.ec2.*.subnet_id}"]
+    value = ["${aws_instance.ec2_instance.*.subnet_id}"]
 }
 
 output "ec2_instance_security_groups" {
-    value = ["${aws_instance.ec2.*.security_groups}"]
+    value = ["${aws_instance.ec2_instance.*.security_groups}"]
 }
 
 output "dhcp_options_id" {
