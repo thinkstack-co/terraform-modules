4132f8ce46df1672e2449e3ea01c4771440efb9f
Zachary Hill <zhill@thinkstack.co>
2019-04-16 15:15:01 -0400

    commented out the ids which are unused

--- modules/aws/vpn/outputs.tf
+++ modules/aws/vpn/outputs.tf
@@ -18,14 +18,15 @@ output "customer_gateway_type" {
     value = "${aws_customer_gateway.customer_gateway.*.type}"
 }
 
-output "vpn_connection_id" {
+/*output "vpn_connection_id" {
     value = "${aws_vpn_connection.vpn_connection.*.id}"
-}
+}*/
 
-output "vpn_connection_tunnel1_address" {
+/*output "vpn_connection_tunnel1_address" {
     value = "${aws_vpn_connection.vpn_connection.*.tunnel1_address}"
 }
 
 output "vpn_connection_tunnel2_address" {
     value = "${aws_vpn_connection.vpn_connection.*.tunnel2_address}"
 }
+*/
