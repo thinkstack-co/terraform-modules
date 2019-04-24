740b73786f2b24f59323fe4f257e61cda6da7254
Zachary Hill <zhill@thinkstack.co>
2019-04-24 14:03:41 -0400

    not going to have multiples in here

--- modules/aws/vpn_route/vpn_route.tf
+++ modules/aws/vpn_route/vpn_route.tf
@@ -1,5 +1,5 @@
 resource "aws_vpn_connection_route" "vpn_route" {
-  count                   = "${length(var.vpn_connection_id)}"
+  # count                   = "${length(var.vpn_connection_id)}"
   destination_cidr_block  = "${var.vpn_route_cidr_block}"
-  vpn_connection_id       = "${element(var.vpn_connection_id, count.index)}"
+  vpn_connection_id       = "${var.vpn_connection_id}"
 }
