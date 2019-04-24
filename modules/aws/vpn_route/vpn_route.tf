f84a8733169d5c11ef96cf269d41388c0620401b
Zachary Hill <zhill@thinkstack.co>
2019-04-24 12:34:34 -0400

    expanding the vpn routes to utilize additional vpn connections

--- modules/aws/vpn_route/vpn_route.tf
+++ modules/aws/vpn_route/vpn_route.tf
@@ -1,4 +1,5 @@
 resource "aws_vpn_connection_route" "vpn_route" {
+  count                   = "${length(var.vpn_connection_id)}"
   destination_cidr_block  = "${var.vpn_route_cidr_block}"
-  vpn_connection_id       = "${var.vpn_connection_id}"
+  vpn_connection_id       = "${element(var.vpn_connection_id, count.index)}"
 }
