1a3d343a7ee8513bc3df432cff433b4484c5b6c3
Zachary Hill <zhill@thinkstack.co>
2019-05-16 20:02:52 -0400

    resolving the name list issue for multiple ip addresses

--- modules/aws/vpn/vpn.tf
+++ modules/aws/vpn/vpn.tf
@@ -9,14 +9,14 @@ resource "aws_customer_gateway" "customer_gateway" {
   count            = "${length(var.ip_address)}"
   ip_address       = "${var.ip_address[count.index]}"
   type             = "${var.vpn_type}"
-  tags             = "${merge(var.tags, map("Name", format("%s_customer_gateway", var.customer_gw_name[count.index])))}"
+  tags             = "${merge(var.tags, map("Name", format("%s_customer_gateway_%s", var.customer_gw_name, count.index + 1)))}"
 }
 
 resource "aws_vpn_connection" "vpn_connection" {
   count                 = "${length(var.ip_address)}"
   customer_gateway_id   = "${element(aws_customer_gateway.customer_gateway.*.id, count.index)}"
   static_routes_only    = "${var.static_routes_only}"
-  tags                  = "${merge(var.tags, map("Name", format("%s", var.customer_gw_name[count.index])))}"
+  tags                  = "${merge(var.tags, map("Name", format("%s_vpn_connection", var.name)))}"
   type                  = "${var.vpn_type}"
   vpn_gateway_id        = "${aws_vpn_gateway.vpn_gateway.id}"
 }
