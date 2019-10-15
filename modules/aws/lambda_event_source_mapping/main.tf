85a36f683fdf536a9c0ef3c8a549743f91871691
Zachary Hill <zhill@thinkstack.co>
2019-10-15 18:49:43 -0400

    removing out the unused variables

--- modules/aws/lambda_event_source_mapping/main.tf
+++ modules/aws/lambda_event_source_mapping/main.tf
@@ -3,6 +3,6 @@ resource "aws_lambda_event_source_mapping" "this" {
   enabled                     = "${var.enabled}"
   event_source_arn            = "${var.event_source_arn}"
   function_name               = "${var.function_name}"
-  starting_position           = "${var.starting_position}"
-  starting_position_timestamp = "${var.starting_position_timestamp}"
+  # starting_position           = "${var.starting_position}"
+  # starting_position_timestamp = "${var.starting_position_timestamp}"
 }
--- modules/aws/lambda_event_source_mapping/variables.tf
+++ modules/aws/lambda_event_source_mapping/variables.tf
@@ -22,10 +22,10 @@ variable "function_name" {
 
 variable "starting_position" {
   description = "Optional) The position in the stream where AWS Lambda should start reading. Must be one of AT_TIMESTAMP (Kinesis only), LATEST or TRIM_HORIZON if getting events from Kinesis or DynamoDB. Must not be provided if getting events from SQS. More information about these positions can be found in the AWS DynamoDB Streams API Reference and AWS Kinesis API Reference."
-  default     = []
+  default     = ""
 }
 
 variable "starting_position_timestamp" {
   description = "(Optional) A timestamp in RFC3339 format of the data record which to start reading when using starting_position set to AT_TIMESTAMP. If a record with this exact timestamp does not exist, the next later record is chosen. If the timestamp is older than the current trim horizon, the oldest available record is chosen."
-  default     = []
+  default     = ""
 }
