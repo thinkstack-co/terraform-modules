7cc3bb54c9fdf29f389335896a4824d7b6cf7007
Zachary Hill <zhill@thinkstack.co>
2019-10-31 12:20:26 -0400

    updated the example to include first class expressions

--- modules/aws/vpc/examples/main.tf
+++ modules/aws/vpc/examples/main.tf
@@ -11,7 +11,7 @@ module "vpc" {
     public_subnets_list         = ["10.11.201.0/24", "10.11.202.0/24", "10.11.203.0/24"]
     workspaces_subnets_list     = ["10.11.21.0/24", "10.11.22.0/24", "10.11.23.0/24"]
     enable_firewall             = true
-    fw_network_interface_id     = "${module.aws_ec2_fortigate_fw.private_network_interface_id}"
+    fw_network_interface_id     = module.aws_ec2_fortigate_fw.private_network_interface_id
     tags                    = {
         terraform   = "true"
         created_by  = "Zachary Hill"
