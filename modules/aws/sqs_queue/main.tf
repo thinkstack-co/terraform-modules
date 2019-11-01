ad0ad2c7b14197a0b2745959fd97f38947fbe32d
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:22:45 -0700

    updated sqs_queue for first class expressions

--- modules/aws/sqs_queue/main.tf
+++ modules/aws/sqs_queue/main.tf
@@ -3,10 +3,10 @@ terraform {
 }
 
 resource "aws_sqs_queue" "queue" {
-  delay_seconds              = "${var.delay_seconds}"
-  fifo_queue                 = "${var.fifo_queue}"
-  message_retention_seconds  = "${var.message_retention_seconds}"
-  name                       = "${var.name}" 
-  tags                       = "${var.tags}"
-  visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
+  delay_seconds              = var.delay_seconds
+  fifo_queue                 = var.fifo_queue
+  message_retention_seconds  = var.message_retention_seconds
+  name                       = var.name 
+  tags                       = var.tags
+  visibility_timeout_seconds = var.visibility_timeout_seconds
 }
