23df6448b9d96a5fdee9698341e9c2908ff6c242
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:20:59 -0700

    updated s3 module for first class expressions

--- modules/aws/s3/main.tf
+++ modules/aws/s3/main.tf
@@ -3,14 +3,14 @@ terraform {
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-    acl             = "${var.acl}"
-    bucket_prefix   = "${var.bucket_prefix}"
-    policy          = "${var.policy}"
-    region          = "${var.region}"
+    acl             = var.acl
+    bucket_prefix   = var.bucket_prefix
+    policy          = var.policy
+    region          = var.region
 
     versioning {
-        enabled     = "${var.versioning}"
-        mfa_delete  = "${var.mfa_delete}"
+        enabled     = var.versioning
+        mfa_delete  = var.mfa_delete
     }
 
     lifecycle {
@@ -20,11 +20,11 @@ resource "aws_s3_bucket" "s3_bucket" {
     server_side_encryption_configuration {
         rule {
           apply_server_side_encryption_by_default {
-            kms_master_key_id = "${var.kms_master_key_id}"
-            sse_algorithm     = "${var.sse_algorithm}"
+            kms_master_key_id = var.kms_master_key_id
+            sse_algorithm     = var.sse_algorithm
           }
         }
       }
 
-    tags            = "${var.tags}"
+    tags            = var.tags
 }
