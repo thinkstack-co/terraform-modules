d12705ac4ca36b4b102b1cb82c64dae75978f31d
Zachary Hill <zhill@thinkstack.co>
2019-11-01 07:35:35 -0700

    updated cloudwatch module to use first class expressions

--- modules/aws/cloudwatch/main.tf
+++ modules/aws/cloudwatch/main.tf
@@ -3,13 +3,13 @@ terraform {
 }
 
 resource "aws_cloudwatch_event_rule" "event_rule" {
-    name                    = "${var.name}"
-    description             = "${var.description}"
-    schedule_expression     = "${var.schedule_expression}"
-    is_enabled              = "${var.is_enabled}"
+    name                    = var.name
+    description             = var.description
+    schedule_expression     = var.schedule_expression
+    is_enabled              = var.is_enabled
 }
 
 resource "aws_cloudwatch_event_target" "event_target" {
-    rule    = "${aws_cloudwatch_event_rule.event_rule.name}"
-    arn     = "${var.event_target_arn}"
+    rule    = aws_cloudwatch_event_rule.event_rule.name
+    arn     = var.event_target_arn
 }
