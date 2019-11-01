7f7a0866cf8c891abdd46ec1fc854d24793e52cf
Zachary Hill <zhill@thinkstack.co>
2019-11-01 07:43:56 -0700

    ec2_domain_controller to use first class expressions

--- modules/aws/ec2_domain_controller/main.tf
+++ modules/aws/ec2_domain_controller/main.tf
@@ -3,32 +3,32 @@ terraform {
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami                                  = "${var.ami}"
+    ami                                  = var.ami
     # This is redundant with the subnet_id option set. The subnet_id already defines an availability zone
-    # availability_zone                    = "${element(var.availability_zone, count.index)}"
-    count                                = "${var.count}"
-    disable_api_termination              = "${var.disable_api_termination}"
-    ebs_optimized                        = "${var.ebs_optimized}"
-    iam_instance_profile                 = "${var.iam_instance_profile}"
-    instance_initiated_shutdown_behavior = "${var.instance_initiated_shutdown_behavior}"
-    instance_type                        = "${var.instance_type}"
-    key_name                             = "${var.key_name}"
-    monitoring                           = "${var.monitoring}"
-    placement_group                      = "${var.placement_group}"
-    private_ip                           = "${element(var.private_ip, count.index)}"
+    # availability_zone                    = element(var.availability_zone, count.index)
+    count                                = var.count
+    disable_api_termination              = var.disable_api_termination
+    ebs_optimized                        = var.ebs_optimized
+    iam_instance_profile                 = var.iam_instance_profile
+    instance_initiated_shutdown_behavior = var.instance_initiated_shutdown_behavior
+    instance_type                        = var.instance_type
+    key_name                             = var.key_name
+    monitoring                           = var.monitoring
+    placement_group                      = var.placement_group
+    private_ip                           = element(var.private_ip, count.index)
     root_block_device                    = {
-        delete_on_termination = "${var.root_delete_on_termination}"
-        # iops                = "${var.root_iops}"
-        volume_size           = "${var.root_volume_size}"
-        volume_type           = "${var.root_volume_type}"
+        delete_on_termination = var.root_delete_on_termination
+        # iops                = var.root_iops
+        volume_size           = var.root_volume_size
+        volume_type           = var.root_volume_type
     }
-    source_dest_check      = "${var.source_dest_check}"
-    subnet_id              = "${element(var.subnet_id, count.index)}"
-    tenancy                = "${var.tenancy}"
-    tags                   = "${merge(var.tags, map("Name", format("%s%01d", var.name, count.index + 1)))}"
-    user_data              = "${var.user_data}"
-    volume_tags            = "${merge(var.tags, map("Name", format("%s%01d", var.name, count.index + 1)))}"
-    vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
+    source_dest_check      = var.source_dest_check
+    subnet_id              = element(var.subnet_id, count.index)
+    tenancy                = var.tenancy
+    tags                   = merge(var.tags, map("Name", format("%s%01d", var.name, count.index + 1)))
+    user_data              = var.user_data
+    volume_tags            = merge(var.tags, map("Name", format("%s%01d", var.name, count.index + 1)))
+    vpc_security_group_ids = [var.vpc_security_group_ids]
 
     lifecycle {
         ignore_changes  = ["user_data"]
@@ -36,14 +36,14 @@ resource "aws_instance" "ec2_instance" {
 }
 
 resource "aws_vpc_dhcp_options" "dc_dns" {
-    domain_name_servers = ["${aws_instance.ec2_instance.*.private_ip}"]
-    domain_name         = "${var.domain_name}"
-    tags                = "${merge(var.tags, map("Name", format("%s-dhcp-options", var.name)))}"
+    domain_name_servers = [aws_instance.ec2_instance.*.private_ip]
+    domain_name         = var.domain_name
+    tags                = merge(var.tags, map("Name", format("%s-dhcp-options", var.name)))
 }
 
 resource "aws_vpc_dhcp_options_association" "dc_dns" {
-    dhcp_options_id = "${aws_vpc_dhcp_options.dc_dns.id}"
-    vpc_id          = "${var.vpc_id}"
+    dhcp_options_id = aws_vpc_dhcp_options.dc_dns.id
+    vpc_id          = var.vpc_id
 }
 
 
@@ -59,12 +59,12 @@ resource "aws_cloudwatch_metric_alarm" "instance" {
   actions_enabled           = true
   alarm_actions             = []
   alarm_description         = "EC2 instance StatusCheckFailed_Instance alarm"
-  alarm_name                = "${format("%s-instance-alarm", element(aws_instance.ec2_instance.*.id, count.index))}"
+  alarm_name                = format("%s-instance-alarm", element(aws_instance.ec2_instance.*.id, count.index))
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  count                     = "${var.count}"
+  count                     = var.count
   datapoints_to_alarm       = 2
   dimensions                = {
-    InstanceId = "${element(aws_instance.ec2_instance.*.id, count.index)}"
+    InstanceId = element(aws_instance.ec2_instance.*.id, count.index)
   }
   evaluation_periods        = "2"
   insufficient_data_actions = []
@@ -75,7 +75,7 @@ resource "aws_cloudwatch_metric_alarm" "instance" {
   statistic                 = "Maximum"
   threshold                 = "1"
   treat_missing_data        = "missing"
-  #unit                      = "${var.unit}"
+  #unit                      = var.unit
 }
 
 #####################
@@ -86,12 +86,12 @@ resource "aws_cloudwatch_metric_alarm" "system" {
   actions_enabled           = true
   alarm_actions             = ["arn:aws:automate:${var.region}:ec2:recover"]
   alarm_description         = "EC2 instance StatusCheckFailed_System alarm"
-  alarm_name                = "${format("%s-system-alarm", element(aws_instance.ec2_instance.*.id, count.index))}"
+  alarm_name                = format("%s-system-alarm", element(aws_instance.ec2_instance.*.id, count.index))
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  count                     = "${var.count}"
+  count                     = var.count
   datapoints_to_alarm       = 2
   dimensions                = {
-    InstanceId = "${element(aws_instance.ec2_instance.*.id, count.index)}"
+    InstanceId = element(aws_instance.ec2_instance.*.id, count.index)
   }
   evaluation_periods        = "2"
   insufficient_data_actions = []
@@ -102,5 +102,5 @@ resource "aws_cloudwatch_metric_alarm" "system" {
   statistic                 = "Maximum"
   threshold                 = "1"
   treat_missing_data        = "missing"
-  #unit                      = "${var.unit}"
+  #unit                      = var.unit
 }
