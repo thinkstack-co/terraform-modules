090480fe2cea877d7efe141849fb8bd8393985b2
Zachary Hill <zhill@thinkstack.co>
2019-11-01 07:37:03 -0700

    updated db_subnet_group to utilize first class expressions

--- modules/aws/db_subnet_group/main.tf
+++ modules/aws/db_subnet_group/main.tf
@@ -3,8 +3,8 @@ terraform {
 }
 
 resource "aws_db_subnet_group" "group" {
-  description = "${var.description}"
-  name        = "${var.name}"
-  subnet_ids  = "${var.subnet_ids}"
-  tags        = "${var.tags}"
+  description = var.description
+  name        = var.name
+  subnet_ids  = var.subnet_ids
+  tags        = var.tags
 }
