7d0d65b9bea9cedbc8be761a1eaed69d64ca9a97
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:15:26 -0700

    iam_saml_provider module updated to utilize first class expressions

--- modules/aws/iam_saml_provider/main.tf
+++ modules/aws/iam_saml_provider/main.tf
@@ -3,6 +3,6 @@ terraform {
 }
 
 resource "aws_iam_saml_provider" "this" {
-  name                   = "${var.name}"
-  saml_metadata_document = "${var.saml_metadata_document}"
+  name                   = var.name
+  saml_metadata_document = var.saml_metadata_document
 }
