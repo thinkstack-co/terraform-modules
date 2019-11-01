b7f1c291552df0a718db72d8dfe0ecc7bc75a83d
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:14:33 -0700

    updated iam_role to utilize first class expressions

--- modules/aws/iam_role/main.tf
+++ modules/aws/iam_role/main.tf
@@ -3,10 +3,10 @@ terraform {
 }
 
 data "aws_iam_role" "this" {
-  assume_role_policy    = "${var.assume_role_policy}"
-  description           = "${var.description}"
-  force_detach_policies = "${var.force_detach_policies}"
-  max_session_duration  = "${var.max_session_duration}"
-  name                  = "${var.name}"
-  permissions_boundary  = "${var.permissions_boundary}"
+  assume_role_policy    = var.assume_role_policy
+  description           = var.description
+  force_detach_policies = var.force_detach_policies
+  max_session_duration  = var.max_session_duration
+  name                  = var.name
+  permissions_boundary  = var.permissions_boundary
 }
