c7ee898ba743d3a481e97cc16175d9da92eac04c
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:18:02 -0700

    updated lambda_event_source_mapping module for first class expressions

--- modules/aws/lambda_event_source_mapping/main.tf
+++ modules/aws/lambda_event_source_mapping/main.tf
@@ -3,10 +3,10 @@ terraform {
 }
 
 resource "aws_lambda_event_source_mapping" "this" {
-  batch_size                  = "${var.batch_size}"
-  enabled                     = "${var.enabled}"
-  event_source_arn            = "${var.event_source_arn}"
-  function_name               = "${var.function_name}"
-  # starting_position           = "${var.starting_position}"
-  # starting_position_timestamp = "${var.starting_position_timestamp}"
+  batch_size                  = var.batch_size
+  enabled                     = var.enabled
+  event_source_arn            = var.event_source_arn
+  function_name               = var.function_name
+  # starting_position           = var.starting_position
+  # starting_position_timestamp = var.starting_position_timestamp
 }
