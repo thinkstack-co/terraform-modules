04f7f760fdf3c8992bde735f6ed1f6e7ef9b1591
Zachary Hill <zhill@thinkstack.co>
2019-11-01 07:36:38 -0700

    updated db_parameter_group to utilize first class expressions

--- modules/aws/db_parameter_group/main.tf
+++ modules/aws/db_parameter_group/main.tf
@@ -3,9 +3,9 @@ terraform {
 }
 
 resource "aws_db_parameter_group" "group" {
-  description = "${var.description}"
-  family      = "${var.family}"
-  name        = "${var.name}"
-  parameter   = "${var.parameter}"
-  tags        = "${var.tags}"
+  description = var.description
+  family      = var.family
+  name        = var.name
+  parameter   = var.parameter
+  tags        = var.tags
 }
