e4d8e4ec37bf926b2bce59902f86ecc999644542
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:17:05 -0700

    kms module updated with first class expressions

--- modules/aws/kms/main.tf
+++ modules/aws/kms/main.tf
@@ -3,16 +3,16 @@ terraform {
 }
 
 resource "aws_kms_key" "key" {
-    description             = "${var.description}"
-    deletion_window_in_days = "${var.deletion_window_in_days}"
-    enable_key_rotation     = "${var.enable_key_rotation}"
-    key_usage               = "${var.key_usage}"
-    is_enabled              = "${var.is_enabled}"
-    policy                  = "${var.policy}"
-    tags                    = "${var.tags}"
+    description             = var.description
+    deletion_window_in_days = var.deletion_window_in_days
+    enable_key_rotation     = var.enable_key_rotation
+    key_usage               = var.key_usage
+    is_enabled              = var.is_enabled
+    policy                  = var.policy
+    tags                    = var.tags
 }
 
 resource "aws_kms_alias" "alias" {
-  name          = "${var.name}"
-  target_key_id = "${aws_kms_key.key.key_id}"
+  name          = var.name
+  target_key_id = aws_kms_key.key.key_id
 }
