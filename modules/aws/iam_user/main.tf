534f3b77cc9b4985b1cb7fc8fa54f960c6a7062f
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:15:48 -0700

    updated iam_user to utilize first class expressions

--- modules/aws/iam_user/main.tf
+++ modules/aws/iam_user/main.tf
@@ -3,8 +3,8 @@ terraform {
 }
 
 resource "aws_iam_user" "this" {
-  force_destroy        = "${var.force_destroy}"
-  name                 = "${var.name}"
-  path                 = "${var.path}"
-  permissions_boundary = "${var.permissions_boundary}"
+  force_destroy        = var.force_destroy
+  name                 = var.name
+  path                 = var.path
+  permissions_boundary = var.permissions_boundary
 }
