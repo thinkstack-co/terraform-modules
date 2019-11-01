964e570fa49f54396c0d9b9426f1551df86a4399
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:23:54 -0700

    updated vpc_peering_connection for first class expressions

--- modules/aws/vpc_peering_connection/main.tf
+++ modules/aws/vpc_peering_connection/main.tf
@@ -3,18 +3,18 @@ terraform {
 }
 
 resource "aws_vpc_peering_connection" "this" {
-  auto_accept   = "${var.auto_accept}"
-  peer_owner_id = "${var.peer_owner_id}"
-  peer_region   = "${var.peer_region}"
-  peer_vpc_id   = "${var.peer_vpc_id}"
-  tags          = "${var.tags}"
-  vpc_id        = "${var.vpc_id}"
+  auto_accept   = var.auto_accept
+  peer_owner_id = var.peer_owner_id
+  peer_region   = var.peer_region
+  peer_vpc_id   = var.peer_vpc_id
+  tags          = var.tags
+  vpc_id        = var.vpc_id
 
   accepter {
-    allow_remote_vpc_dns_resolution = "${var.allow_remote_vpc_dns_resolution}"
+    allow_remote_vpc_dns_resolution = var.allow_remote_vpc_dns_resolution
   }
 
   requester {
-    allow_remote_vpc_dns_resolution = "${var.allow_remote_vpc_dns_resolution}"
+    allow_remote_vpc_dns_resolution = var.allow_remote_vpc_dns_resolution
   }
 }
