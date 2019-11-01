4797841a9e4e3a50eb9474a2283c03f5d9a2582c
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:22:25 -0700

    updated sns_email for first class expressions

--- modules/aws/sns_email/main.tf
+++ modules/aws/sns_email/main.tf
@@ -3,12 +3,12 @@ terraform {
 }
 
 resource "aws_sns_topic" "topic" {
-  display_name = "${var.display_name}"
-  name         = "${var.name}"
+  display_name = var.display_name
+  name         = var.name
 }
 
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  topic_arn = "${aws_sns_topic.topic.id}"
-  protocol  = "${var.protocol}"
+  topic_arn = aws_sns_topic.topic.id
+  protocol  = var.protocol
   endpoint  = "arn:aws:sqs:us-west-2:432981146916:terraform-queue-too"
 }
