ab74effcf9d5f4bc2ff77bd83ebe8cdd9e770538
Zachary Hill <zhill@thinkstack.co>
2019-11-01 08:20:06 -0700

    updated rds_cluster_parameter_group for first class expressions

--- modules/aws/rds_cluster_parameter_group/main.tf
+++ modules/aws/rds_cluster_parameter_group/main.tf
@@ -3,9 +3,9 @@ terraform {
 }
 
 resource "aws_rds_cluster_parameter_group" "group" {
-  description = "${var.description}"
-  family      = "${var.family}"
-  name        = "${var.name}"
-  parameter   = "${var.parameter}"
-  tags        = "${var.tags}"
+  description = var.description
+  family      = var.family
+  name        = var.name
+  parameter   = var.parameter
+  tags        = var.tags
 }
