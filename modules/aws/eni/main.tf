9f62716272101756fecbef5af1d7ce059b961c25
Zachary Hill <zhill@thinkstack.co>
2019-11-01 07:53:34 -0700

    updated eni module with first class expressions

--- modules/aws/eni/main.tf
+++ modules/aws/eni/main.tf
@@ -3,17 +3,17 @@ terraform {
 }
 
 resource "aws_network_interface" "eni" {
-  attachment          = "${var.attachment}"
-  private_ips         = "${var.private_ips}"
-  private_ips_count   = "${var.private_ips_count}"
-  security_groups     = "${var.security_groups}"
-  source_dest_check   = "${var.source_dest_check}"
-  subnet_id           = "${var.subnet_id}"
-  tags                = "${var.tags}"
+  attachment          = var.attachment
+  private_ips         = var.private_ips
+  private_ips_count   = var.private_ips_count
+  security_groups     = var.security_groups
+  source_dest_check   = var.source_dest_check
+  subnet_id           = var.subnet_id
+  tags                = var.tags
 }
 
 resource "aws_network_interface_attachment" "eni_attach" {
-  device_index         = "${var.device_index}"
-  instance_id          = "${var.instance_id}"
-  network_interface_id = "${aws_network_interface.eni.id}"
+  device_index         = var.device_index
+  instance_id          = var.instance_id
+  network_interface_id = aws_network_interface.eni.id
 }
