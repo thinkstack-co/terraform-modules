2ee00b3967a99700beb1d99f3005ee10f1824677
Zachary Hill <zhill@thinkstack.co>
2019-11-01 07:50:42 -0700

    updated elastic_beanstalk_application module to utilize first class expressions

--- modules/aws/elastic_beanstalk_application/main.tf
+++ modules/aws/elastic_beanstalk_application/main.tf
@@ -3,13 +3,13 @@ terraform {
 }
 
 resource "aws_elastic_beanstalk_application" "this" {
-  name        = "${var.name}"
-  description = "${var.description}"
+  name        = var.name
+  description = var.description
 
   appversion_lifecycle {
-    service_role          = "${var.service_role}"
-    max_age_in_days       = "${var.max_age_in_days}"
-    max_count             = "${var.max_count}"
-    delete_source_from_s3 = "${var.delete_source_from_s3}"
+    service_role          = var.service_role
+    max_age_in_days       = var.max_age_in_days
+    max_count             = var.max_count
+    delete_source_from_s3 = var.delete_source_from_s3
   }
 }
