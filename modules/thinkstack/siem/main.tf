4dae6199b8894349b1f675f3e8084ffdd909de98
Zachary Hill <zhill@thinkstack.co>
2019-11-17 09:10:15 -0500

    adding siem modules

---
+++ modules/thinkstack/siem/main.tf
@@ -0,0 +1,366 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+###########################
+# VPC
+###########################
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+  instance_tenancy     = var.instance_tenancy
+  tags                 = merge(var.tags, map("Name", format("%s", var.name)))
+}
+
+###########################
+# VPC - Subnets
+###########################
+
+resource "aws_subnet" "private_subnets" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnets_list[count.index]
+  availability_zone = list(format("%sa", var.region), format("%sb", var.region))
+  count             = length(var.private_subnets_list)
+  tags              = merge(var.tags, map("Name", format("%s-subnet-private-%s", var.name, element(var.azs, count.index))))
+}
+
+resource "aws_subnet" "public_subnets" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnets_list[count.index]
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  count                   = length(var.public_subnets_list)
+  tags                    = merge(var.tags, map("Name", format("%s-subnet-public-%s", var.name, element(var.azs, count.index))))
+}
+
+###########################
+# VPC - Gateways
+###########################
+
+resource "aws_internet_gateway" "igw" {
+  tags   = merge(var.tags, map("Name", format("%s-igw", var.name)))
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_nat_gateway" "natgw" {
+  depends_on    = ["aws_internet_gateway.igw"]
+
+  allocation_id = element(aws_eip.nateip.*.id, (var.single_nat_gateway ? 0 : count.index))
+  count         = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.azs)) : 0
+  subnet_id     = element(aws_subnet.public_subnets.*.id, (var.single_nat_gateway ? 0 : count.index))
+}
+
+###########################
+# VPC - Routes
+###########################
+
+resource "aws_route_table" "public_route_table" {
+  propagating_vgws = var.public_propagating_vgws
+  tags             = merge(var.tags, map("Name", format("%s-rt-public", var.name)))
+  vpc_id           = aws_vpc.vpc.id
+}
+
+resource "aws_route" "public_default_route" {
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+  route_table_id         = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  count            = length(var.azs)
+  propagating_vgws = var.private_propagating_vgws
+  tags             = merge(var.tags, map("Name", format("%s-rt-private-%s", var.name, element(var.azs, count.index))))
+  vpc_id           = aws_vpc.vpc.id
+}
+
+resource "aws_route" "private_default_route_natgw" {
+  count                  = var.enable_firewall ? 0 : length(var.azs)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.natgw.*.id, count.index)
+  route_table_id         = element(aws_route_table.private_route_table.*.id, count.index)
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets_list)
+  route_table_id = element(aws_route_table.private_route_table.*.id, count.index)
+  subnet_id      = element(aws_subnet.private_subnets.*.id, count.index)
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets_list)
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
+}
+
+###########################
+# VPC - Peering
+###########################
+
+resource "aws_vpc_peering_connection" "peer" {
+  count         = var.enable_vpc_peering
+  auto_accept   = var.auto_accept
+  peer_owner_id = var.peer_owner_id
+  peer_region   = var.peer_region
+  peer_vpc_id   = var.peer_vpc_id
+  tags          = var.tags
+  vpc_id        = var.vpc_id
+
+  accepter {
+    allow_remote_vpc_dns_resolution = var.allow_remote_vpc_dns_resolution
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = var.allow_remote_vpc_dns_resolution
+  }
+}
+
+
+###########################
+# VPC - VPN
+###########################
+
+resource "aws_vpn_gateway" "vpn_gateway" {
+  vpc_id            = aws_vpc.vpc.id
+  tags              = merge(var.tags, map("Name", format("%s_vpn_gw", var.name)))
+}
+
+resource "aws_customer_gateway" "customer_gateway" {
+  bgp_asn          = var.bgp_asn
+  count            = length(var.vpn_peer_ip_address)
+  ip_address       = var.vpn_peer_ip_address[count.index]
+  type             = var.vpn_type
+  tags             = merge(var.tags, map("Name", format("%s_customer_gw", var.customer_gw_name[count.index])))
+}
+
+resource "aws_vpn_connection" "vpn_connection" {
+  count                 = length(var.vpn_peer_ip_address)
+  customer_gateway_id   = element(aws_customer_gateway.customer_gateway.*.id, count.index)
+  static_routes_only    = var.static_routes_only
+  tags                  = merge(var.tags, map("Name", format("%s_vpn_connection", var.name)))
+  type                  = var.vpn_type
+  vpn_gateway_id        = aws_vpn_gateway.vpn_gateway.id
+}
+
+resource "aws_vpn_connection_route" "vpn_route" {
+  count                  = length(vpn_route_cidr_blocks)
+  destination_cidr_block = var.vpn_route_cidr_blocks
+  vpn_connection_id      = aws_vpn_connection.vpn_connection.id
+}
+
+###########################
+# EC2 - Keypair
+###########################
+
+resource "aws_key_pair" "deployer_key" {
+    key_name_prefix =   var.key_name_prefix
+    public_key      =   var.public_key
+}
+
+###########################
+# EC2 - Instance
+###########################
+
+resource "aws_instance" "ec2" {
+  ami                                  = var.ami
+  associate_public_ip_address          = var.associate_public_ip_address
+  availability_zone                    = var.availability_zone
+  count                                = var.count
+  disable_api_termination              = var.disable_api_termination
+  ebs_optimized                        = var.ebs_optimized
+  iam_instance_profile                 = var.iam_instance_profile
+  instance_initiated_shutdown_behavior = var.instance_initiated_shutdown_behavior
+  instance_type                        = var.instance_type
+  ipv6_address_count                   = var.ipv6_address_count
+  ipv6_addresses                       = var.ipv6_addresses
+  key_name                             = aws_key_pair.deployer_key.id
+  monitoring                           = var.monitoring
+  placement_group                      = var.placement_group
+  private_ip                           = var.private_ip
+  root_block_device                    = {
+    delete_on_termination = var.root_delete_on_termination
+    volume_type           = var.root_volume_type
+    volume_size           = var.root_volume_size
+  }
+  source_dest_check      = var.source_dest_check
+  subnet_id              = aws_subnet.private_subnets.id[count.index]
+  tags                   = merge(var.tags, map("Name", format("%s%d", var.name, count.index + 1)))
+  tenancy                = var.tenancy
+  user_data              = var.user_data
+  volume_tags            = merge(var.tags, map("Name", format("%s%d", var.name, count.index + 1)))
+  vpc_security_group_ids = [var.vpc_security_group_ids]
+}
+
+###################################################
+# EC2 - CloudWatch Alarms
+###################################################
+
+#####################
+# Status Check Failed Instance Metric
+#####################
+
+resource "aws_cloudwatch_metric_alarm" "instance" {
+  actions_enabled           = true
+  alarm_actions             = []
+  alarm_description         = "EC2 instance StatusCheckFailed_Instance alarm"
+  alarm_name                = format("%s-instance-alarm", element(aws_instance.ec2.*.id, count.index))
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  count                     = var.count
+  datapoints_to_alarm       = 2
+  dimensions                = {
+    InstanceId = element(aws_instance.ec2.*.id, count.index)
+  }
+  evaluation_periods        = "2"
+  insufficient_data_actions = []
+  metric_name               = "StatusCheckFailed_Instance"
+  namespace                 = "AWS/EC2"
+  ok_actions                = []
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  treat_missing_data        = "missing"
+}
+
+#####################
+# Status Check Failed System Metric
+#####################
+
+resource "aws_cloudwatch_metric_alarm" "system" {
+  actions_enabled           = true
+  alarm_actions             = ["arn:aws:automate:${var.region}:ec2:recover"]
+  alarm_description         = "EC2 instance StatusCheckFailed_System alarm"
+  alarm_name                = format("%s-system-alarm", element(aws_instance.ec2.*.id, count.index))
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  count                     = var.count
+  datapoints_to_alarm       = 2
+  dimensions                = {
+    InstanceId = element(aws_instance.ec2.*.id, count.index)
+  }
+  evaluation_periods        = "2"
+  insufficient_data_actions = []
+  metric_name               = "StatusCheckFailed_System"
+  namespace                 = "AWS/EC2"
+  ok_actions                = []
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  treat_missing_data        = "missing"
+}
+
+###########################
+# EC2 - Security Group
+###########################
+
+resource "aws_security_group" "sg" {
+    description = var.security_group_description
+    name        = var.security_group_name
+    tags        = merge(var.tags, map("Name", format("%s", var.security_group_name)))
+    vpc_id      = aws_vpc.vpc.id
+}
+
+
+###########################
+# EC2 - Security Group Rules
+###########################
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+    type                = "egress"
+    from_port           = 0
+    to_port             = 0
+    protocol            = "-1"
+    cidr_blocks         = ["0.0.0.0/0"]
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "allow_icmp_inbound" {
+    type                = "ingress"
+    from_port           = -1
+    to_port             = -1
+    protocol            = "icmp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "tcp_22_inbound" {
+    type                = "ingress"
+    from_port           = 22
+    to_port             = 22
+    protocol            = "tcp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "udp_161_inbound" {
+    type                = "ingress"
+    from_port           = 161
+    to_port             = 161
+    protocol            = "udp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "udp_162_inbound" {
+    type                = "ingress"
+    from_port           = 162
+    to_port             = 162
+    protocol            = "udp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "tcp_135_inbound" {
+    type                = "ingress"
+    from_port           = 135
+    to_port             = 135
+    protocol            = "tcp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "tcp_443_inbound" {
+    type                = "ingress"
+    from_port           = 443
+    to_port             = 443
+    protocol            = "tcp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "udp_514_inbound" {
+    type                = "ingress"
+    from_port           = 514
+    to_port             = 514
+    protocol            = "udp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "udp_5480_inbound" {
+    type                = "ingress"
+    from_port           = 5480
+    to_port             = 5480
+    protocol            = "udp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "tcp_5480_inbound" {
+    type                = "ingress"
+    from_port           = 5480
+    to_port             = 5480
+    protocol            = "tcp"
+    cidr_blocks         = var.cidr_blocks
+
+    security_group_id   = aws_security_group.sg.id
+}
+
---
+++ modules/thinkstack/siem/variables.tf
@@ -0,0 +1,238 @@
+variable "ami" {
+  description = "ID of AMI to use for the instance"
+}
+
+variable "associate_public_ip_address" {
+  description = "If true, the EC2 instance will have associated public IP address"
+  default     = false
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region which will be utilized by this VPC"
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "bgp_asn" {
+    description = "BGP ASN used for dynamic routing between the customer gateway and AWS gateway"
+    default     = 65001
+}
+
+variable "count" {
+  description = "Number of instances to launch"
+  default     = 1
+}
+
+variable "customer_gw_name" {
+    type = "list"
+    description = "(Required) List of names to use for the customer gateways. The order of names will be associated with the same IP address peering order"
+}
+
+variable "created_by" {
+  type = "string"
+  description = "(Required) the full name of the person who is created the resource in terraform"
+}
+
+variable "disable_api_termination" {
+  description = "If true, enables EC2 Instance Termination Protection"
+  default     = false
+}
+
+variable "ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized"
+  default     = false
+}
+
+variable "enable_dns_hostnames" {
+  description = "should be true if you want to use private DNS within the VPC"
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "should be true if you want to use private DNS within the VPC"
+  default     = true
+}
+
+variable "enable_firewall" {
+  description = "should be true if you are using a firewall to NAT traffic for the private subnets"
+  default     = false
+}
+
+variable "enable_nat_gateway" {
+  description = "should be true if you want to provision NAT Gateways for each of your private networks"
+  default     = true
+}
+
+variable "iam_instance_profile" {
+  description = "The IAM Instance Profile to launch the instance with. Specified as the name of the Instance Profile."
+  default     = ""
+}
+
+variable "instance_initiated_shutdown_behavior" {
+  # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/terminating-instances.html#Using_ChangingInstanceInitiatedShutdownBehavior
+  description = "Shutdown behavior for the instance"
+  default     = ""
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  default     = "t3a.medium"
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  default     = "default"
+}
+
+variable "ipv6_address_count" {
+  description = "A number of IPv6 addresses to associate with the primary network interface. Amazon EC2 chooses the IPv6 addresses from the range of your subnet."
+  default     = 0
+}
+
+variable "ipv6_addresses" {
+  description = "Specify one or more IPv6 addresses from the range of the subnet to associate with the primary network interface"
+  default     = []
+}
+
+variable "key_name_prefix" {
+    description = "SSL key pair name prefix, used to generate unique keypair name for EC2 instance deployments"
+    default = "siem_keypair"
+}
+
+variable "map_public_ip_on_launch" {
+  description = "should be false if you do not want to auto-assign public IP on launch"
+  default     = false
+}
+
+variable "monitoring" {
+  description = "If true, the launched EC2 instance will have detailed monitoring enabled"
+  default     = false
+}
+
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  default     = "siem"
+}
+
+variable "placement_group" {
+  description = "The Placement Group to start the instance in"
+  default     = ""
+}
+
+variable "private_ip" {
+  description = "Private IP address to associate with the instance in a VPC"
+  default     = "10.77.1.12"
+}
+
+variable "private_propagating_vgws" {
+  description = "A list of VGWs the private route table should propagate."
+  default     = []
+}
+
+variable "private_subnets_list" {
+  description = "A list of private subnets inside the VPC."
+  default     = ["10.77.1.64/26", "10.77.1.192/26"]
+}
+
+variable "public_key" {
+    description = "(Required) Public rsa key"
+}
+
+variable "public_propagating_vgws" {
+  description = "A list of VGWs the public route table should propagate."
+  default     = []
+}
+
+variable "public_subnets_list" {
+  description = "A list of public subnets inside the VPC."
+  default     = ["10.77.1.0/26", "10.77.1.128/26"]
+}
+
+variable "root_delete_on_termination" {
+  type        = "string"
+  description = "(Optional) Whether the volume should be destroyed on instance termination (Default: true)"
+  default     = true
+}
+
+variable "root_volume_size" {
+  type        = "string"
+  description = "(Optional) The size of the volume in gigabytes."
+  default     = "100"
+}
+
+variable "root_volume_type" {
+  type        = "string"
+  description = "(Optional) The type of volume. Can be standard, gp2, or io1. (Default: standard)"
+  default     = "gp2"
+}
+
+variable "security_group_description" {
+    description = "Description of the security group"
+    default     = "SIEM Collector Security Group"
+}
+
+variable "security_group_name" {
+    description = "Name of the security group used for SIEM"
+    default     = "siem_collector_sg"
+}
+
+variable "single_nat_gateway" {
+  description = "should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  default     = false
+}
+
+variable "source_dest_check" {
+  description = "Controls if traffic is routed to the instance when the destination address does not match the instance. Used for NAT or VPNs."
+  default     = true
+}
+
+variable "static_routes_only" {
+    description = "Flag to determine whether or not dynamic or static routing is enabled"
+    default     = false
+}
+
+variable "region" {
+  type        = "string"
+  description = "(Required) AWS region in which the VPC and all rersources will be created in"
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  default     = {
+    created_by  = var.created_by
+    terraform   = "true"
+    environment = "prod"
+    project     = "core_infrastructure"
+    team        = "security_team"
+    used_by     = "organization"
+  }
+}
+
+variable "tenancy" {
+  description = "The tenancy of the instance (if the instance is running in a VPC). Available values: default, dedicated, host."
+  default     = "default"
+}
+
+variable "user_data" {
+  description = "The user data to provide when launching the instance"
+  default     = ""
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.77.1.0/24"
+}
+
+variable "vpn_peer_ip_address" {
+    type        = "list"
+    description = "(Required) List of customer gateway external IP addresses which will be utilized to create VPN connections with"
+}
+
+variable "vpn_route_cidr_blocks" {
+    type        = "list"
+    description = "(Required) CIDR block of the VPN subnets"
+}
+
+variable "vpn_type" {
+    description = "Type of VPN tunnel. Currently only supports ipsec.1"
+    default     = "ipsec.1"
+}
