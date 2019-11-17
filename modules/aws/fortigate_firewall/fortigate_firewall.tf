2a39556c5fce9c2e276f27992cfcb4a777d2d6ba
Zachary Hill <zhill@thinkstack.co>
2019-11-17 09:13:46 -0500

    updated fortigate firewall module to use first class expressions

--- modules/aws/fortigate_firewall/fortigate_firewall.tf
+++ modules/aws/fortigate_firewall/fortigate_firewall.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "fortigate_fw_sg" {
-    name    = "${var.sg_name}"
+    name    = var.sg_name
     description = "Security group applied to all fortigate firewalls"
-    vpc_id = "${var.vpc_id}"
+    vpc_id = var.vpc_id
 
     ingress {
         from_port   = 0
@@ -17,12 +17,12 @@ resource "aws_security_group" "fortigate_fw_sg" {
     cidr_blocks     = ["0.0.0.0/0"]
     }
 
-    tags            = "${merge(var.tags, map("Name", format("%s", var.sg_name)))}"
+    tags            = merge(var.tags, map("Name", format("%s", var.sg_name)))
 }
 
 resource "aws_eip" "external_ip" {
   vpc   = true
-  count = "${var.count}"
+  count = var.count
 
   lifecycle {
     prevent_destroy = true
@@ -30,19 +30,19 @@ resource "aws_eip" "external_ip" {
 }
 
 resource "aws_eip_association" "fw_external_ip" {
-    count                   = "${var.count}"
-    allocation_id           = "${element(aws_eip.external_ip.*.id, count.index)}"
-    network_interface_id    = "${element(aws_network_interface.fw_public_nic.*.id, count.index)}"
+    count                   = var.count
+    allocation_id           = element(aws_eip.external_ip.*.id, count.index)
+    network_interface_id    = element(aws_network_interface.fw_public_nic.*.id, count.index)
 }
 
 resource "aws_network_interface" "fw_public_nic" {
-    count               = "${var.count}"
-    description         = "${var.public_nic_description}"
-    private_ips         = "${var.wan_private_ips}"
-    security_groups     = ["${aws_security_group.fortigate_fw_sg.id}"]
-    source_dest_check   = "${var.source_dest_check}"
-    subnet_id           = "${element(var.public_subnet_id, count.index)}"
-    tags                = "${merge(var.tags, map("Name", format("%s%d_public", var.instance_name_prefix, count.index + 1)))}"
+    count               = var.count
+    description         = var.public_nic_description
+    private_ips         = var.wan_private_ips
+    security_groups     = [aws_security_group.fortigate_fw_sg.id]
+    source_dest_check   = var.source_dest_check
+    subnet_id           = element(var.public_subnet_id, count.index)
+    tags                = merge(var.tags, map("Name", format("%s%d_public", var.instance_name_prefix, count.index + 1)))
 
     lifecycle {
       ignore_changes  = ["subnet_id"]
@@ -50,16 +50,16 @@ resource "aws_network_interface" "fw_public_nic" {
 }
 
 resource "aws_network_interface" "fw_private_nic" {
-    count               = "${var.count}"
-    description         = "${var.private_nic_description}"
-    private_ips         = ["${element(var.lan_private_ips, count.index)}"]
-    security_groups     = ["${aws_security_group.fortigate_fw_sg.id}"]
-    source_dest_check   = "${var.source_dest_check}"
-    subnet_id           = "${element(var.private_subnet_id, count.index)}"
-    tags                = "${merge(var.tags, map("Name", format("%s%d_private", var.instance_name_prefix, count.index + 1)))}"
+    count               = var.count
+    description         = var.private_nic_description
+    private_ips         = [element(var.lan_private_ips, count.index)]
+    security_groups     = [aws_security_group.fortigate_fw_sg.id]
+    source_dest_check   = var.source_dest_check
+    subnet_id           = element(var.private_subnet_id, count.index)
+    tags                = merge(var.tags, map("Name", format("%s%d_private", var.instance_name_prefix, count.index + 1)))
 
     attachment {
-        instance        = "${element(aws_instance.ec2_instance.*.id, count.index)}"
+        instance        = element(aws_instance.ec2_instance.*.id, count.index)
         device_index    = 1
     }
 
@@ -69,16 +69,16 @@ resource "aws_network_interface" "fw_private_nic" {
 }
 
 resource "aws_network_interface" "fw_dmz_nic" {
-    count               = "${var.enable_dmz ? var.count : 0}"
-    description         = "${var.dmz_nic_description}"
-    private_ips         = ["${element(var.dmz_private_ips, count.index)}"]
-    security_groups     = ["${aws_security_group.fortigate_fw_sg.id}"]
-    source_dest_check   = "${var.source_dest_check}"
-    subnet_id           = "${element(var.dmz_subnet_id, count.index)}"
-    tags                = "${merge(var.tags, map("Name", format("%s%d_dmz", var.instance_name_prefix, count.index + 1)))}"
+    count               = var.enable_dmz ? var.count : 0
+    description         = var.dmz_nic_description
+    private_ips         = [element(var.dmz_private_ips, count.index)]
+    security_groups     = [aws_security_group.fortigate_fw_sg.id]
+    source_dest_check   = var.source_dest_check
+    subnet_id           = element(var.dmz_subnet_id, count.index)
+    tags                = merge(var.tags, map("Name", format("%s%d_dmz", var.instance_name_prefix, count.index + 1)))
 
     attachment {
-        instance        = "${element(aws_instance.ec2_instance.*.id, count.index)}"
+        instance        = element(aws_instance.ec2_instance.*.id, count.index)
         device_index    = 2
     }
 
@@ -88,29 +88,29 @@ resource "aws_network_interface" "fw_dmz_nic" {
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami           = "${var.ami_id}"
-    count         = "${var.count}"
-    ebs_optimized = "${var.ebs_optimized}"
-    instance_type = "${var.instance_type}"
-    key_name      = "${var.key_name}"
-    monitoring    = "${var.monitoring}"
-    volume_tags   = "${merge(var.tags, map("Name", format("%s%d", var.instance_name_prefix, count.index + 1)))}"
-    tags          = "${merge(var.tags, map("Name", format("%s%d", var.instance_name_prefix, count.index + 1)))}"
+    ami           = var.ami_id
+    count         = var.count
+    ebs_optimized = var.ebs_optimized
+    instance_type = var.instance_type
+    key_name      = var.key_name
+    monitoring    = var.monitoring
+    volume_tags   = merge(var.tags, map("Name", format("%s%d", var.instance_name_prefix, count.index + 1)))
+    tags          = merge(var.tags, map("Name", format("%s%d", var.instance_name_prefix, count.index + 1)))
 
     network_interface {
-        network_interface_id = "${element(aws_network_interface.fw_public_nic.*.id, count.index)}"
+        network_interface_id = element(aws_network_interface.fw_public_nic.*.id, count.index)
         device_index         = 0
     }
 
     root_block_device {
-        volume_type = "${var.root_volume_type}"
-        volume_size = "${var.root_volume_size}"
+        volume_type = var.root_volume_type
+        volume_size = var.root_volume_size
     }
     ebs_block_device {
-        device_name = "${var.ebs_device_name}"
-        volume_type = "${var.ebs_volume_type}"
-        volume_size = "${var.ebs_volume_size}"
-        encrypted   = "${var.ebs_volume_encrypted}"
+        device_name = var.ebs_device_name
+        volume_type = var.ebs_volume_type
+        volume_size = var.ebs_volume_size
+        encrypted   = var.ebs_volume_encrypted
     }
 }
 
@@ -126,12 +126,12 @@ resource "aws_cloudwatch_metric_alarm" "instance" {
   actions_enabled           = true
   alarm_actions             = []
   alarm_description         = "EC2 instance StatusCheckFailed_Instance alarm"
-  alarm_name                = "${format("%s-instance-alarm", element(aws_instance.ec2_instance.*.id, count.index))}"
+  alarm_name                = format("%s-instance-alarm", element(aws_instance.ec2_instance.*.id, count.index))
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  count                     = "${var.count}"
+  count                     = var.count
   datapoints_to_alarm       = 2
   dimensions                = {
-    InstanceId = "${element(aws_instance.ec2_instance.*.id, count.index)}"
+    InstanceId = element(aws_instance.ec2_instance.*.id, count.index)
   }
   evaluation_periods        = "2"
   insufficient_data_actions = []
@@ -142,7 +142,7 @@ resource "aws_cloudwatch_metric_alarm" "instance" {
   statistic                 = "Maximum"
   threshold                 = "1"
   treat_missing_data        = "missing"
-  #unit                      = "${var.unit}"
+  #unit                      = var.unit
 }
 
 #####################
@@ -153,12 +153,12 @@ resource "aws_cloudwatch_metric_alarm" "system" {
   actions_enabled           = true
   alarm_actions             = ["arn:aws:automate:${var.region}:ec2:recover"]
   alarm_description         = "EC2 instance StatusCheckFailed_System alarm"
-  alarm_name                = "${format("%s-system-alarm", element(aws_instance.ec2_instance.*.id, count.index))}"
+  alarm_name                = format("%s-system-alarm", element(aws_instance.ec2_instance.*.id, count.index))
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  count                     = "${var.count}"
+  count                     = var.count
   datapoints_to_alarm       = 2
   dimensions                = {
-    InstanceId = "${element(aws_instance.ec2_instance.*.id, count.index)}"
+    InstanceId = element(aws_instance.ec2_instance.*.id, count.index)
   }
   evaluation_periods        = "2"
   insufficient_data_actions = []
@@ -169,5 +169,5 @@ resource "aws_cloudwatch_metric_alarm" "system" {
   statistic                 = "Maximum"
   threshold                 = "1"
   treat_missing_data        = "missing"
-  #unit                      = "${var.unit}"
+  #unit                      = var.unit
 }
