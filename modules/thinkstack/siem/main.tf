145f306d785ff019be4ff81679bd31838b768a29
Zachary Hill <zhill@thinkstack.co>
2019-11-19 14:36:38 -0500

    utilizing count index for the peer_vpc_ids

--- modules/thinkstack/siem/main.tf
+++ modules/thinkstack/siem/main.tf
@@ -108,7 +108,7 @@ resource "aws_vpc_peering_connection" "peer" {
   auto_accept   = var.auto_accept
   peer_owner_id = var.peer_owner_id
   peer_region   = var.peer_region
-  peer_vpc_id   = var.peer_vpc_ids
+  peer_vpc_id   = var.peer_vpc_ids[count.index]
   tags          = var.tags
   vpc_id        = aws_vpc.vpc.id
 
