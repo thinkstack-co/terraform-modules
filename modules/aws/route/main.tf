b5384cae6286f0bd56cfd5229015e5b8bee684c6
Zachary Hill <zhill@thinkstack.co>
2019-12-06 19:28:11 -0500

    trying for loop

--- modules/aws/route/main.tf
+++ modules/aws/route/main.tf
@@ -12,6 +12,10 @@ resource "aws_route" "route" {
   # instance_id                 = var.instance_id
   nat_gateway_id              = var.nat_gateway_id
   network_interface_id        = length(var.network_interface_id) > 0 ? element(concat(var.network_interface_id, list("")), count.index) : ""
-  route_table_id              = element(var.route_table_id, count.index)
+  route_table_id              = [
+    for route_table_id in var.route_table_id:
+    route_table_id[*]
+  ]
+  # route_table_id              = element(var.route_table_id, count.index)
   vpc_peering_connection_id   = var.vpc_peering_connection_id
 }
