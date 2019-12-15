908de44b395e6a42d64813efc2227db94f939ab3
Zachary Hill <zhill@thinkstack.co>
2019-12-15 18:04:50 -0500

    testing removing the list

--- modules/aws/ec2_instance_imported/main.tf
+++ modules/aws/ec2_instance_imported/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "ec2" {
   tenancy                = var.tenancy
   user_data              = var.user_data
   volume_tags            = merge(var.tags, map("Name", format("%s%d", var.name, count.index + 1)))
-  vpc_security_group_ids = [var.vpc_security_group_ids]
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   lifecycle {
     ignore_changes  = ["user_data"]
