76cfa6f36725d4d6f892bcf0c9bbbe07abba56e1
Zachary Hill <zhill@thinkstack.co>
2019-12-15 17:59:10 -0500

    removed parameter variable

--- modules/aws/db_parameter_group/main.tf
+++ modules/aws/db_parameter_group/main.tf
@@ -6,6 +6,5 @@ resource "aws_db_parameter_group" "group" {
   description = var.description
   family      = var.family
   name        = var.name
-  parameter   = var.parameter
   tags        = var.tags
 }
