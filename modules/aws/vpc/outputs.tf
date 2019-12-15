c2b3c76330e47602cb89da1d0e9cf2c8bb47c524
Zachary Hill <zhill@thinkstack.co>
2019-12-15 18:50:56 -0500

    two of the outputs

--- modules/aws/vpc/outputs.tf
+++ modules/aws/vpc/outputs.tf
@@ -31,11 +31,11 @@ output "workspaces_subnet_ids" {
 }
 
 output "vpc_id" {
-  value = aws_vpc.vpc[*].id
+  value = aws_vpc.vpc.id
 }
 
 output "vpc_cidr_block" {
-  value = aws_vpc.vpc[*].cidr_block
+  value = aws_vpc.vpc.cidr_block
 }
 
 output "public_route_table_ids" {
