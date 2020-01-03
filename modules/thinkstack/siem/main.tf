fbd3e4424b55d352c50a7eb8e831391d07a1283a
Zachary Hill <zhill@thinkstack.co>
2020-01-03 14:01:20 -0500

    adding role policy framework

--- modules/thinkstack/siem/main.tf
+++ modules/thinkstack/siem/main.tf
@@ -431,16 +431,22 @@ resource "aws_iam_role_policy_attachment" "this_attach" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
 
+##################
+# IAM role profile
+##################
+
 resource "aws_iam_instance_profile" "this" {
   name = var.iam_role_name
   role = aws_iam_role.this.name
 }
 
-
-
 ##################
 # IAM role policy
 ##################
+
+/*resource "type" "name" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}*/
 
 ##################
 # SSM activation
