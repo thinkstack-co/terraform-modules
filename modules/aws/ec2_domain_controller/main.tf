ec5d062421083c143fded3551daf8938155a0b83
Zachary Hill <zhill@thinkstack.co>
2020-03-18 10:21:35 -0400

    syntax changes for v0.12 terraform in ec2_domain_controller module

--- modules/aws/ec2_domain_controller/main.tf
+++ modules/aws/ec2_domain_controller/main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "ec2_instance" {
     vpc_security_group_ids = var.vpc_security_group_ids
 
     lifecycle {
-        ignore_changes  = ["user_data"]
+        ignore_changes  = [user_data]
     }
 }
 
