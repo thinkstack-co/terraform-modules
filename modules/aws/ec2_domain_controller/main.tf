5005ce19f934a2e4990b76ba81cc410836f92036
Zachary Hill <zhill@thinkstack.co>
2020-03-18 10:36:15 -0400

    syntax fix for root_block_device

--- modules/aws/ec2_domain_controller/main.tf
+++ modules/aws/ec2_domain_controller/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "ec2_instance" {
     monitoring                           = var.monitoring
     placement_group                      = var.placement_group
     private_ip                           = element(var.private_ip, count.index)
-    root_block_device                    = {
+    root_block_device {
         delete_on_termination = var.root_delete_on_termination
         encrypted             = var.encrypted
         # iops                = var.root_iops
