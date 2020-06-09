f1bd33042876c5ab92801928743d6d0644440b74
jjones-ts <jjones@thinkstack.co>
2020-06-09 15:19:13 -0400

    removed sse code

--- modules/aws/s3_website/main.tf
+++ modules/aws/s3_website/main.tf
@@ -22,14 +22,5 @@ resource "aws_s3_bucket" "s3_bucket" {
         prevent_destroy = true
     }
 
-    server_side_encryption_configuration {
-        rule {
-          apply_server_side_encryption_by_default {
-            kms_master_key_id = var.kms_master_key_id
-            sse_algorithm     = var.sse_algorithm
-          }
-        }
-      }
-
     tags            = var.tags
 }
