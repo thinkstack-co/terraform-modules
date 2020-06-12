30299f2180ba4f7ac81953de1890901673aba063
jjones-ts <jjones@thinkstack.co>
2020-06-12 09:46:38 -0400

    fixed syntax for tf 11

--- modules/aws/s3_legacy/main.tf
+++ modules/aws/s3_legacy/main.tf
@@ -1,21 +1,17 @@
-terraform {
-  required_version = ">= 0.12.0"
-}
-
 resource "aws_s3_bucket" "s3_bucket" {
-    acl             = var.acl
-    bucket          = var.bucket
-    policy          = var.policy
-    region          = var.region
+    acl             = "${var.acl}"
+    bucket          = "${var.bucket}"
+    policy          = "${var.policy}"
+    region          = "${var.region}"
 
     /*logging {
-        target_bucket = var.target_bucket
-        target_prefix = var.target_prefix
+        target_bucket = "${var.target_bucket}"
+        target_prefix = "${var.target_prefix}"
     }*/
 
     versioning {
-        enabled     = var.versioning
-        mfa_delete  = var.mfa_delete
+        enabled     = "${var.versioning}"
+        mfa_delete  = "${var.mfa_delete}"
     }
 
     lifecycle {
@@ -25,11 +21,11 @@ resource "aws_s3_bucket" "s3_bucket" {
     server_side_encryption_configuration {
         rule {
           apply_server_side_encryption_by_default {
-            kms_master_key_id = var.kms_master_key_id
-            sse_algorithm     = var.sse_algorithm
+            kms_master_key_id = "${var.kms_master_key_id}"
+            sse_algorithm     = "${var.sse_algorithm}"
           }
         }
       }
 
-    tags            = var.tags
+    tags            = "${var.tags}"
 }
