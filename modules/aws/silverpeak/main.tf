6914b2eb1f4374d945aab7cb8febd69a1cf3d097
Thomas Gallagher <63355740+tgallagher91@users.noreply.github.com>
2020-11-11 15:17:21 -0500

    replaced count.index with number.index

--- modules/aws/silverpeak/main.tf
+++ modules/aws/silverpeak/main.tf
@@ -38,26 +38,26 @@ resource "aws_network_interface" "wan0_nic" {
     private_ips         = var.wan0_private_ips
     security_groups     = [aws_security_group.silverpeak_sg.id]
     source_dest_check   = var.source_dest_check
-    subnet_id           = element(var.dmz_subnet_id, count.index)
-    tags                = merge(var.tags, map("Name", format("%s%d_wan0", var.name, count.index + 1)))
+    subnet_id           = element(var.dmz_subnet_id, number.index)
+    tags                = merge(var.tags, map("Name", format("%s%d_wan0", var.name, number.index + 1)))
 
     attachment {
-        instance        = element(aws_instance.ec2.*.id, count.index)
+        instance        = element(aws_instance.ec2.*.id, number.index)
         device_index    = 1
     }
 }
 
 resource "aws_network_interface" "lan0_nic" {
-    number               = var.number
+    number              = var.number
     description         = var.lan0_description
     private_ips         = var.lan0_private_ips
     security_groups     = [aws_security_group.silverpeak_sg.id]
     source_dest_check   = var.source_dest_check
-    subnet_id           = element(var.private_subnet_id, count.index)
-    tags                = merge(var.tags, map("Name", format("%s%d_lan0", var.name, count.index + 1)))
+    subnet_id           = element(var.private_subnet_id, number.index)
+    tags                = merge(var.tags, map("Name", format("%s%d_lan0", var.name, number.index + 1)))
 
     attachment {
-        instance        = element(aws_instance.ec2.*.id, count.index)
+        instance        = element(aws_instance.ec2.*.id, number.index)
         device_index    = 2
     }
 }
@@ -68,8 +68,8 @@ resource "aws_network_interface" "mgmt0_nic" {
     private_ips         = var.mgmt0_private_ips
     security_groups     = [aws_security_group.silverpeak_sg.id]
     source_dest_check   = var.source_dest_check
-    subnet_id           = element(var.mgmt_subnet_id, count.index)
-    tags                = merge(var.tags, map("Name", format("%s%d_mgmt0", var.name, count.index + 1)))
+    subnet_id           = element(var.mgmt_subnet_id, number.index)
+    tags                = merge(var.tags, map("Name", format("%s%d_mgmt0", var.name, number.index + 1)))
 }
 
 #######################
@@ -101,8 +101,8 @@ resource "aws_instance" "ec2" {
     volume_size           = var.root_volume_size
   }
   
-  tags                   = merge(var.tags, map("Name", format("%s%d", var.name, count.index + 1)))
+  tags                   = merge(var.tags, map("Name", format("%s%d", var.name, number.index + 1)))
   tenancy                = var.tenancy
   user_data              = var.user_data
-  volume_tags            = merge(var.tags, map("Name", format("%s%d", var.name, count.index + 1)))
+  volume_tags            = merge(var.tags, map("Name", format("%s%d", var.name, number.index + 1)))
 }
