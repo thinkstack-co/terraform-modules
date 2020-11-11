2b36a057d611b5423309ee2646b58e12a0f4291a
Thomas Gallagher <63355740+tgallagher91@users.noreply.github.com>
2020-11-11 16:03:38 -0500

    removed .id from aws_network_interface

--- modules/aws/silverpeak/main.tf
+++ modules/aws/silverpeak/main.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "ec2" {
   monitoring                           = var.monitoring
 
   network_interface {
-        network_interface_id = aws_network_interface.mgmt0_nic.id[count.index]
+        network_interface_id = aws_network_interface.mgmt0_nic[count.index]
     }
 
   placement_group                      = var.placement_group
