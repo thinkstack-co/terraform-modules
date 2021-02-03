2da3ac90630a40b4aa1fc6adcf2565a2ca7a5d7e
Thomas Gallagher <63355740+tgallagher91@users.noreply.github.com>
2021-02-03 15:55:03 -0500

    added s3 back to service_name

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -206,7 +206,7 @@ data "aws_vpc_endpoint_service" "s3" {
 resource "aws_vpc_endpoint" "ep" {
   count        = var.enable_s3_endpoint ? 1 : 0
   vpc_id       = aws_vpc.vpc.id
-  service_name = data.aws_vpc_endpoint_service.service_name
+  service_name = data.aws_vpc_endpoint_service.s3.service_name
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
