9f1ca9ce9bf0957945ae4aeb198c83d3be3185a4
Thomas Gallagher <63355740+tgallagher91@users.noreply.github.com>
2021-02-03 21:22:56 -0500

    added full s3 endpoint to name

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -193,7 +193,7 @@ resource "aws_route" "workspaces_default_route_fw" {
 data "aws_vpc_endpoint_service" "s3" {
   service = "s3"
   filter {
-    name   = "s3"
+    name   = "s3.us-east-1.amazonaws.com"
     values = ["${aws_vpc.vpc.id}"]
   }
 }
