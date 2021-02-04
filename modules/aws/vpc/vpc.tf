9859901b7b3a00c148315295d7e030c5b8cff1fc
jakeasarus <jjones@thinkstack.co>
2021-02-04 09:19:39 -0500

    removed args

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -193,9 +193,7 @@ resource "aws_route" "workspaces_default_route_fw" {
   route_table_id         = "${element(aws_route_table.workspaces_route_table.*.id, count.index)}"
 }
 
-data "aws_region" "current" {
-  name = "${provider.aws.region}"
-}
+data "aws_region" "current" {}
 data "aws_vpc_endpoint_service" "s3" {
   service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
  }
