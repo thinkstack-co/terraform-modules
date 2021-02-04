ad0870ded7eca41162c27713c347e7690bda002d
Thomas Gallagher <63355740+tgallagher91@users.noreply.github.com>
2021-02-03 22:40:35 -0500

    changed the service name

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -191,7 +191,7 @@ resource "aws_route" "workspaces_default_route_fw" {
 }
 
 data "aws_vpc_endpoint_service" "s3" {
-  service      = "s3"
+  service      = "com.amazonaws.us-east-1.s3"
 #   filter {
 #     name   = "service-name"
 #     values = ["com.amazonaws.us-east-1.s3"]
