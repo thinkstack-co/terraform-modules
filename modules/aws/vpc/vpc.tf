f48fb99732969a5c9a7e99bf6cde77efb7790d0c
jakeasarus <jjones@thinkstack.co>
2021-02-04 09:42:38 -0500

    updated data

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -190,9 +190,11 @@ resource "aws_route" "workspaces_default_route_fw" {
   route_table_id         = "${element(aws_route_table.workspaces_route_table.*.id, count.index)}"
 }
 
-data "aws_region" "current" {}
+# data "aws_region" "current" {
+#   name = ${provider.aws.region}
+# }
 data "aws_vpc_endpoint_service" "s3" {
-  service_name = "com.amazonaws.${provider.aws.region}.s3"
+  service_name = "com.amazonaws.${module.provider.aws.region}.s3"
  }
 
 resource "aws_vpc_endpoint" "ep" {
