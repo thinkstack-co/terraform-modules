b88843bf4262a98dfbd002d8f82bc1537af151b9
jakeasarus <jjones@thinkstack.co>
2021-02-04 09:38:45 -0500

    updated args

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -192,7 +192,7 @@ resource "aws_route" "workspaces_default_route_fw" {
 
 data "aws_region" "current" {}
 data "aws_vpc_endpoint_service" "s3" {
-  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
+  service_name = "com.amazonaws.${provider.aws.region}.s3"
  }
 
 resource "aws_vpc_endpoint" "ep" {
