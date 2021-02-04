08a35a306dd73fc3e5d91950e87e2bb1a0074d9a
Thomas Gallagher <63355740+tgallagher91@users.noreply.github.com>
2021-02-03 22:39:51 -0500

    removed filter

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -192,11 +192,11 @@ resource "aws_route" "workspaces_default_route_fw" {
 
 data "aws_vpc_endpoint_service" "s3" {
   service      = "s3"
-  filter {
-    name   = "service-name"
-    values = ["com.amazonaws.us-east-1.s3"]
-  }
-}
+#   filter {
+#     name   = "service-name"
+#     values = ["com.amazonaws.us-east-1.s3"]
+#   }
+ }
 
 resource "aws_vpc_endpoint" "ep" {
   count        = "${var.enable_s3_endpoint}"
