94a7cf2268e19352845818f74c21f41f66444a7e
Thomas Gallagher <63355740+tgallagher91@users.noreply.github.com>
2021-02-03 23:16:13 -0500

    changed name

--- modules/aws/vpc/vpc.tf
+++ modules/aws/vpc/vpc.tf
@@ -193,7 +193,7 @@ resource "aws_route" "workspaces_default_route_fw" {
 data "aws_vpc_endpoint_service" "s3" {
   service      = "${s3}"
    filter {
-     name   = "${vpc_id}"
+     name   = "${aws_vpc.vpc.id}"
      values = ["${aws_vpc.vpc.id}"]
    }
  }
