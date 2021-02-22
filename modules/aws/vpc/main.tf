c3020911ef54579627b2d504ac87053aacae1809
jakeasarus <jjones@thinkstack.co>
2021-02-22 15:21:10 -0500

    added element back in

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -210,7 +210,7 @@ locals {
 resource "aws_vpc_endpoint" "s3" {
   count        = var.enable_s3_endpoint ? 1 : 0
   vpc_id       = aws_vpc.vpc.id
-  service_name = local.service_name
+  service_name = element(local.service_name)
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
