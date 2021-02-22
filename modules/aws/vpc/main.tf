440ea4843b72c010deb8501f381dd305297e54a6
jakeasarus <jjones@thinkstack.co>
2021-02-22 15:17:34 -0500

    updated resources referencing old "ep" resource that was replaced with "s3" name.

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -215,13 +215,13 @@ resource "aws_vpc_endpoint" "s3" {
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
   count           = var.enable_s3_endpoint ? length(var.private_subnets_list) : 0
-  vpc_endpoint_id = aws_vpc_endpoint.ep[count.index]
+  vpc_endpoint_id = aws_vpc_endpoint.s3[count.index]
   route_table_id  = element(aws_route_table.private_route_table.*.id, count.index)
 }
 
 resource "aws_vpc_endpoint_route_table_association" "public_s3" {
   count           = var.enable_s3_endpoint ? length(var.public_subnets_list) : 0
-  vpc_endpoint_id = aws_vpc_endpoint.ep[count.index]
+  vpc_endpoint_id = aws_vpc_endpoint.s3[count.index]
   route_table_id  = aws_route_table.public_route_table.id
 }
 
