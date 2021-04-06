6528bc735eee0f2853b4706d9aee18413a7d80f5
Zachary Hill <zhill@thinkstack.co>
2021-04-06 16:53:19 -0400

    variable alignment

--- modules/aws/route/variables.tf
+++ modules/aws/route/variables.tf
@@ -1,71 +1,71 @@
 variable "carrier_gateway_id" {
-  type = string
+  type        = string
   description = "(Optional) Identifier of a carrier gateway. This attribute can only be used when the VPC contains a subnet which is associated with a Wavelength Zone."
-  default = null
+  default     = null
 }
 
 variable "destination_cidr_block" {
-  type = string
+  type        = string
   description = " (Optional) The destination CIDR block."
 }
 
 variable "destination_ipv6_cidr_block" {
-  type = string
+  type        = string
   description = "(Optional) The destination IPv6 CIDR block."
-  default = null
+  default     = null
 }
 
 variable "egress_only_gateway_id" {
-  type = string
+  type        = string
   description = "(Optional) An ID of a VPC Egress Only Internet Gateway."
-  default = null
+  default     = null
 }
 
 variable "gateway_id" {
-  type = string
+  type        = string
   description = "(Optional) An ID of a VPC internet gateway or a virtual private gateway."
-  default = null
+  default     = null
 }
 
 variable "instance_id" {
-  type = string
+  type        = string
   description = "(Optional) An ID of an EC2 instance."
-  default = null
+  default     = null
 }
 
 variable "local_gateway_id" {
-  type = string
+  type        = string
   description = "(Optional) Identifier of a Outpost local gateway."
-  default = null
+  default     = null
 }
 
 variable "nat_gateway_id" {
-  type = string
+  type        = string
   description = "(Optional) An ID of a VPC NAT gateway."
-  default = null
+  default     = null
 }
 
 variable "network_interface_id" {
-  type = list
+  type        = string
   description = "(Optional) An ID of a network interface."
-  default = null
+  default     = null
 }
 
 variable "transit_gateway_id" {
-  type = string
+  type        = string
   description = "(Optional) Identifier of an EC2 Transit Gateway."
-  default = null
+  default     = null
 }
 
 variable "route_table_id" {
-  type = list
+  type        = list
   description = "(Required) The ID of the routing table."
 }
 
 variable "vpc_endpoint_id" {
-  type = string
+  type        = string
   description = "(Optional) Identifier of a VPC Endpoint."
-  default = null
+  default     = null
 }
 
 variable "vpc_peering_connection_id" {
