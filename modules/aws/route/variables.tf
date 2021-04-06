da32487f265bf6b9c6398f444ca6e213e20f0651
Zachary Hill <zhill@thinkstack.co>
2021-04-06 16:40:04 -0400

    added all of the new gateway ids for the aws provider to the aws_route module and subsequent variables

--- modules/aws/route/variables.tf
+++ modules/aws/route/variables.tf
@@ -1,3 +1,9 @@
+variable "carrier_gateway_id" {
+  type = string
+  description = "(Optional) Identifier of a carrier gateway. This attribute can only be used when the VPC contains a subnet which is associated with a Wavelength Zone."
+  default = null
+}
+
 variable "destination_cidr_block" {
   type = string
   description = " (Optional) The destination CIDR block."
@@ -12,25 +18,31 @@ variable "destination_ipv6_cidr_block" {
 variable "egress_only_gateway_id" {
   type = string
   description = "(Optional) An ID of a VPC Egress Only Internet Gateway."
-  default = ""
+  default = null
 }
 
 variable "gateway_id" {
   type = string
   description = "(Optional) An ID of a VPC internet gateway or a virtual private gateway."
-  default = ""
+  default = null
 }
 
 variable "instance_id" {
   type = string
   description = "(Optional) An ID of an EC2 instance."
-  default = ""
+  default = null
+}
+
+variable "local_gateway_id" {
+  type = string
+  description = "(Optional) Identifier of a Outpost local gateway."
+  default = null
 }
 
 variable "nat_gateway_id" {
   type = string
   description = "(Optional) An ID of a VPC NAT gateway."
-  default = ""
+  default = null
 }
 
 variable "network_interface_id" {
@@ -39,13 +51,25 @@ variable "network_interface_id" {
   default = []
 }
 
+variable "transit_gateway_id" {
+  type = string
+  description = "(Optional) Identifier of an EC2 Transit Gateway."
+  default = null
+}
+
 variable "route_table_id" {
   type = list
   description = "(Required) The ID of the routing table."
 }
 
+variable "vpc_endpoint_id" {
+  type = string
+  description = "(Optional) Identifier of a VPC Endpoint."
+  default = null
+}
+
 variable "vpc_peering_connection_id" {
   type = string
   description = "(Optional) An ID of a VPC peering connection."
-  default = ""
+  default = null
 }
