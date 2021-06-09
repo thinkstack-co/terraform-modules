e2b0d7d0f749e050f1b7de01fe7a06cb12f645e1
jakeasarus <jjones@thinkstack.co>
2021-06-09 10:31:19 -0400

    fixed typo

--- modules/aws/transit_gateway/main.tf
+++ modules/aws/transit_gateway/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ec2_transit_gateway" "transit_gateway" {  
     description = var.description
     amazon_side_asn = var.amazon_side_asn
-    auto_accept_shared_attachments = var.audo_accept_shared_attachments
+    auto_accept_shared_attachments = var.auto_accept_shared_attachments
     default_route_table_association = var.default_route_table_association
     default_route_table_propagation  = var.default_route_table_propagation
     dns_support  = var.dns_support
