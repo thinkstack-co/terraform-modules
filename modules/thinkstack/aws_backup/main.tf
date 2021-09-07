4694f062a6b61306ce667c89f04b6d7d6618660b
Zachary Hill <zhill@thinkstack.co>
2021-09-07 11:01:35 -0400

    added kms key and all vault outputs

--- modules/thinkstack/aws_backup/main.tf
+++ modules/thinkstack/aws_backup/main.tf
@@ -13,21 +13,42 @@ terraform {
 # KMS Encryption Key
 ###############################################################
 
+# Production region key
 resource "aws_kms_key" "key" {
-    bypass_policy_lockout_safety_check = var.key_bypass_policy_lockout_safety_check
-    customer_master_key_spec           = var.key_customer_master_key_spec
-    description                        = var.key_description
-    deletion_window_in_days            = var.key_deletion_window_in_days
-    enable_key_rotation                = var.key_enable_key_rotation
-    key_usage                          = var.key_usage
-    is_enabled                         = var.key_is_enabled
-    policy                             = var.key_policy
-    tags                               = var.tags
+  bypass_policy_lockout_safety_check = var.key_bypass_policy_lockout_safety_check
+  customer_master_key_spec           = var.key_customer_master_key_spec
+  description                        = var.key_description
+  deletion_window_in_days            = var.key_deletion_window_in_days
+  enable_key_rotation                = var.key_enable_key_rotation
+  key_usage                          = var.key_usage
+  is_enabled                         = var.key_is_enabled
+  policy                             = var.key_policy
+  tags                               = var.tags
 }
 
 resource "aws_kms_alias" "alias" {
-  name          = var.key_name
+  name          = var.prod_key_name
   target_key_id = aws_kms_key.key.key_id
+}
+
+# Disaster recovery region key
+resource "aws_kms_key" "dr_key" {
+  provider                           = aws.aws_dr_region
+  bypass_policy_lockout_safety_check = var.key_bypass_policy_lockout_safety_check
+  customer_master_key_spec           = var.key_customer_master_key_spec
+  description                        = var.key_description
+  deletion_window_in_days            = var.key_deletion_window_in_days
+  enable_key_rotation                = var.key_enable_key_rotation
+  key_usage                          = var.key_usage
+  is_enabled                         = var.key_is_enabled
+  policy                             = var.key_policy
+  tags                               = var.tags
+}
+
+resource "aws_kms_alias" "dr_alias" {
+  provider      = aws.aws_dr_region
+  name          = var.key_name
+  target_key_id = aws_kms_key.dr_key.key_id
 }
 
 ###############################################################
@@ -92,7 +113,7 @@ resource "aws_backup_vault" "vault_prod_monthly" {
 resource "aws_backup_vault" "vault_disaster_recovery" {
   provider    = aws.aws_dr_region
   name        = var.vault_disaster_recovery_name
-  kms_key_arn = aws_kms_key.key.arn
+  kms_key_arn = aws_kms_key.dr_key.arn
   tags        = var.tags
 }
 
