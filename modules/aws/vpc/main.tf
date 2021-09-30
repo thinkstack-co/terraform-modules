14240074af97cba909969b4f9dcc44a1c9f178e9
TheJalpa <30740021+TheJalpa@users.noreply.github.com>
2021-09-30 16:28:22 -0700

    trying without map

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -7,8 +7,10 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
   instance_tenancy     = var.instance_tenancy
+  #tags                 = merge(var.tags, map("Name", format("%s", var.name))) old
   #tags                 = merge(var.tags, "Name" = format("%s", var.name))
-  tags = ["${concat(var.tags, "Name" = format("%s", var.name))}"]
+  #tags = ["${concat(var.tags, "Name" = format("%s", var.name))}"]
+  tags = merge({"Name" = format("%s", var.name)},var.tags,)
   }
 
 resource "aws_subnet" "private_subnets" {
