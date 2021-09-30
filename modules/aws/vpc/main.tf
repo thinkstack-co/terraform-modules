cf00d71117be7701732aea3e159a5520486cd57f
TheJalpa <30740021+TheJalpa@users.noreply.github.com>
2021-09-30 16:24:18 -0700

    trying with concat

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = var.enable_dns_support
   instance_tenancy     = var.instance_tenancy
   #tags                 = merge(var.tags, "Name" = format("%s", var.name))
-  tags = ["${concat(var.tags, list(tomap("key", "Name","value", var.name)))}"]
+  tags = ["${concat(var.tags, "Name" = format("%s", var.name))}"]
   }
 
 resource "aws_subnet" "private_subnets" {
