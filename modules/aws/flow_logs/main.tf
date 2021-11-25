bca15b3849b44a28bb63f7dd3335fa555a07578c
Zachary Hill <zhill@thinkstack.co>
2021-11-25 09:52:27 -0500

    renamed the role

--- modules/aws/flow_logs/main.tf
+++ modules/aws/flow_logs/main.tf
@@ -67,7 +67,7 @@ resource "aws_iam_policy" "policy" {
 # IAM Role
 ###########################
 
-data "aws_iam_role" "this" {
+data "aws_iam_role" "role" {
   assume_role_policy    = var.assume_role_policy
   description           = var.description
   force_detach_policies = var.force_detach_policies
