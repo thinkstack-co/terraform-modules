d857e8c8d7d3f0e24c5a94c209a41933acc95e9a
TheJalpa <30740021+TheJalpa@users.noreply.github.com>
2021-12-01 11:04:20 -0800

    updating tags as they somehow got reverted

--- modules/aws/ec2_instance/main.tf
+++ modules/aws/ec2_instance/main.tf
@@ -30,14 +30,14 @@ resource "aws_instance" "ec2" {
   root_block_device {
     delete_on_termination = var.root_delete_on_termination
     encrypted             = var.encrypted
-    tags                  = merge(var.tags, map("Name", format("%s%d", var.name, count.index + 1)))
+     tags                  = merge(var.tags, ({ "Name" = format("%s%d", var.name, count.index + 1) }))
     volume_type           = var.root_volume_type
     volume_size           = var.root_volume_size
   }
 
   source_dest_check      = var.source_dest_check
   subnet_id              = var.subnet_id
-  tags                   = merge(var.tags, map("Name", format("%s%d", var.name, count.index + 1)))
+   tags                  = merge(var.tags, ({ "Name" = format("%s%d", var.name, count.index + 1) }))
   tenancy                = var.tenancy
   user_data              = var.user_data
   vpc_security_group_ids = var.vpc_security_group_ids
