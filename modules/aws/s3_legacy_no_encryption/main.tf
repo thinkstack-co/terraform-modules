2f645d962e02f2f5b1ef662c8e5f347e41c375c1
jakeasarus <jjones@thinkstack.co>
2021-12-14 16:20:09 -0500

    added another module for s3 legacy without encryption

---
+++ modules/aws/s3_legacy_no_encryption/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  acl    = var.acl
+  bucket = var.bucket
+  policy = var.policy
+
+  /*logging {
+        target_bucket = var.target_bucket
+        target_prefix = var.target_prefix
+    }*/
+
+  versioning {
+    enabled    = var.versioning
+    mfa_delete = var.mfa_delete
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = var.tags
+}
