5df7a7935343986d7ec14245b43a9d1c74c2d421
Zachary Hill <zhill@thinkstack.co>
2021-12-19 17:31:37 -0500

    updated the datasource

--- modules/aws/flow_logs/main.tf
+++ modules/aws/flow_logs/main.tf
@@ -32,7 +32,7 @@ resource "aws_kms_key" "key" {
             "Sid" = "Enable IAM User Permissions",
             "Effect" = "Allow",
             "Principal" = {
-                "AWS" = "arn:aws:iam::${account_id}:root"
+                "AWS" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
             },
             "Action" = "kms:*",
             "Resource" = "*"
