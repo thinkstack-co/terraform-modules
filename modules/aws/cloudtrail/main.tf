7eff8a94d53ecd0d56cd9cd42a116fba23ef662e
Zachary Hill <zhill@thinkstack.co>
2021-12-23 15:36:47 -0500

    major overhaul of the cloudtrail module to include better file structure and best practices from AWS

--- modules/aws/cloudtrail/main.tf
+++ modules/aws/cloudtrail/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.13.0"
 }
 
 ###########################
@@ -96,69 +96,47 @@ resource "aws_kms_key" "cloudtrail" {
 # S3 Bucket
 ###########################
 resource "aws_s3_bucket" "cloudtrail_s3_bucket" {
-  acl           = var.acl
-  bucket_prefix = var.bucket_prefix
+  acl           = "private"
+  bucket_prefix = var.s3_bucket_prefix
+  tags          = var.tags
 
   versioning {
-    enabled    = var.enabled
-    mfa_delete = var.mfa_delete
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = var.kms_master_key_id
-        sse_algorithm     = var.sse_algorithm
-      }
-    }
-  }
-
-  tags = {
-    terraform = "true"
+    enabled    = var.s3_versioning_enabled
+    mfa_delete = var.s3_mfa_delete
   }
 }
 
 resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
   bucket = aws_s3_bucket.cloudtrail_s3_bucket.id
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
+  policy = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = [
         {
-            "Sid": "AWSCloudTrailAclCheck",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
+            "Sid" = "AWSCloudTrailAclCheck",
+            "Effect" = "Allow",
+            "Principal" = {
+                "Service" = "cloudtrail.amazonaws.com"
             },
-            "Action": "s3:GetBucketAcl",
-            "Resource": "${aws_s3_bucket.cloudtrail_s3_bucket.arn}"
+            "Action" = "s3:GetBucketAcl",
+            "Resource" = "${aws_s3_bucket.cloudtrail_s3_bucket.arn}"
         },
         {
-            "Sid": "AWSCloudTrailAclCheck",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
+            "Sid" = "AWSCloudTrailWrite",
+            "Effect" = "Allow",
+            "Principal" = {
+                "Service" = "cloudtrail.amazonaws.com"
             },
-            "Action": "s3:GetBucketAcl",
-            "Resource": "${aws_s3_bucket.cloudtrail_s3_bucket.arn}"
-        },
-        {
-            "Sid": "AWSCloudTrailWrite",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:PutObject",
-            "Resource": "${aws_s3_bucket.cloudtrail_s3_bucket.arn}/*",
-            "Condition": {
-                "StringEquals": {
-                    "s3:x-amz-acl": "bucket-owner-full-control"
+            "Action" = "s3:PutObject",
+            "Resource" = "${aws_s3_bucket.cloudtrail_s3_bucket.arn}/*",
+            "Condition" = {
+                "StringEquals" = {
+                    "AWS:SourceArn" = "${aws_cloudtrail.cloudtrail.arn}",
+                    "s3:x-amz-acl" = "bucket-owner-full-control"
                 }
             }
         }
     ]
-}
-POLICY
+  })
 }
 
 ###########################
@@ -166,11 +144,14 @@ POLICY
 ###########################
 
 resource "aws_cloudtrail" "cloudtrail" {
-    enable_log_file_validation      =   var.enable_log_file_validation
-    include_global_service_events   =   var.include_global_service_events
-    is_multi_region_trail           =   var.is_multi_region_trail
+    enable_log_file_validation      =   var.cloudtrail_enable_log_file_validation
+    include_global_service_events   =   var.cloudtrail_include_global_service_events
+    is_multi_region_trail           =   var.cloudtrail_is_multi_region_trail
     kms_key_id                      =   aws_kms_key.cloudtrail.arn
-    name                            =   var.name
+    name                            =   var.cloudtrail_name
     s3_bucket_name                  =   aws_s3_bucket.cloudtrail_s3_bucket.id
-    s3_key_prefix                   =   var.s3_key_prefix
+    s3_key_prefix                   =   var.cloudtrail_s3_key_prefix
+    insight_selector {
+      insight_type = var.cloudtrail_insight_type
+    }
 }
--- modules/aws/cloudtrail/variables.tf
+++ modules/aws/cloudtrail/variables.tf
@@ -44,68 +44,76 @@ variable "key_name_prefix" {
   type        = string
 }
 
-
 ###########################
-# 
+# S3 Bucket
 ###########################
 
-variable "bucket_prefix" {
+variable "s3_bucket_prefix" {
   description = "(Optional, Forces new resource) Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+  default     = "cloudtrail-"
+  type        = string
 }
 
-variable "acl" {
-  description = "(Optional) The canned ACL to apply. Defaults to 'private'."
-  default     = "private"
-}
-
-variable "enabled" {
+variable "s3_versioning_enabled" {
   description = "(Optional) Enable versioning. Once you version-enable a bucket, it can never return to an unversioned state. You can, however, suspend versioning on that bucket."
-  default     = true
+  default     = false
 }
 
-variable "kms_master_key_id" {
-  type        = string
-  description = "(optional) The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms."
-  default     = ""
-}
-
-variable "sse_algorithm" {
-  type        = string
-  description = "(required) The server-side encryption algorithm to use. Valid values are AES256 and aws:kms"
-  default     = "aws:kms"
-}
-
-variable "mfa_delete" {
+variable "s3_mfa_delete" {
   description = "(Optional) Enable MFA delete for either Change the versioning state of your bucket or Permanently delete an object version. Default is false."
   default     = true
 }
 
-variable "name" {
-  description = "Name of the trail"
+###########################
+# Cloudtrail
+###########################
+
+variable "cloudtrail_enable_log_file_validation" {
+  description = "(Optional) Whether log file integrity validation is enabled. Defaults to false."
+  default     = true
+  type        = bool
+}
+
+variable "cloudtrail_include_global_service_events" {
+  description = "(Optional) Whether the trail is publishing events from global services such as IAM to the log files. Defaults to true."
+  default     = true
+  type        = bool
+}
+
+variable "cloudtrail_is_multi_region_trail" {
+  description = "(Optional) Whether the trail is created in the current region or in all regions. Defaults to false."
+  default     = true
+  type        = bool
+}
+
+variable "cloudtrail_name" {
+  description = "(Required) Name of the trail."
   default     = "cloudtrail"
+  type        = string
 }
 
-variable "s3_key_prefix" {
-  description = "S3 key prefix to be applied to all logs"
-  default     = "cloudtrail-logs"
+variable "cloudtrail_s3_key_prefix" {
+  description = "(Optional) S3 key prefix that follows the name of the bucket you have designated for log file delivery."
+  default     = null
+  type        = string
 }
 
-variable "include_global_service_events" {
-  description = "Specifies whether the trail is publishing events from global services such as IAM to the log files"
-  default     = true
+variable "cloudtrail_insight_type" {
+  type        = list(string)
+  description = "(Optional) Type of insights to log on a trail. The valid value is ApiCallRateInsight"
+  default     = ["ApiCallRateInsight", "ApiErrorRateInsight"]
 }
 
-variable "is_multi_region_trail" {
-  description = "Determines whether or not the cloudtrail is created for all regions"
-  default     = true
-}
+###########################
+# General Use Variables
+###########################
 
-variable "kms_key_id" {
-  description = "KMS key used to encrypt the cloudtrail logs"
-  default     = ""
-}
-
-variable "enable_log_file_validation" {
-  description = "Enabled log file validation to all logs sent to S3"
-  default     = true
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the object."
+  default     = {
+    terraform   = "true"
+    created_by  = "ThinkStack"
+    environment = "prod"
+    priority    = "high"
+  }
 }
