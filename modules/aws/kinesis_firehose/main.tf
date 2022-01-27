c9896c0f34dfcaa26a8f080677714f520506c787
Zachary Hill <zhill@thinkstack.co>
2022-01-26 19:16:10 -0500

    removed incomplete IAM role and policy from example code

--- modules/aws/kinesis_firehose/main.tf
+++ modules/aws/kinesis_firehose/main.tf
@@ -50,50 +50,6 @@ resource "aws_s3_bucket" "firehose_bucket" {
   }
 }
 
-#################################
-# IAM
-#################################
-
-resource "aws_iam_role" "firehose_role" {
-  name = "firehose_test_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "firehose.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role" "lambda_iam" {
-  name = "lambda_iam"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
 ###########################
 # IAM Policy
 ###########################
