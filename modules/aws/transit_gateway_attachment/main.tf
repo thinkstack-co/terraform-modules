f70266fce72b54c174d03cd994ba4407719636f6
Zachary Hill <zhill@thinkstack.co>
2022-03-13 10:58:50 -0400

    updates to the attachment module to include all supported arguments

--- modules/aws/transit_gateway_attachment/main.tf
+++ modules/aws/transit_gateway_attachment/main.tf
@@ -1,6 +1,9 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "transit_gateway_vpc_attachment" {
-  subnet_ids         = var.subnet_ids
-  transit_gateway_id = var.transit_gateway_id
-  vpc_id             = var.vpc_id
-  tags  = var.tags
+  appliance_mode_support = var.appliance_mode_support
+  dns_support            = var.dns_support
+  ipv6_support           = var.ipv6_support
+  subnet_ids             = var.subnet_ids
+  tags                   = merge(tomap({Name = var.name}),var.tags)
+  transit_gateway_id     = var.transit_gateway_id
+  vpc_id                 = var.vpc_id
 }
--- modules/aws/transit_gateway_attachment/variables.tf
+++ modules/aws/transit_gateway_attachment/variables.tf
@@ -1,53 +1,47 @@
-variable "appliance_mdoe_support" {
+variable "appliance_mode_support" {
   description = "(Optional) Whether Appliance Mode support is enabled. If enabled, a traffic flow between a source and destination uses the same Availability Zone for the VPC attachment for the lifetime of that flow."
-  default = "disable"
-}
-
-variable "description" {
-  description = "(Optional) Description of the EC2 Transit Gateway Attachment."
-  default     = "Transit gateway attachment to allow access across VPCs or accounts."
+  default     = "disable"
+  type        = string
 }
 
 variable "dns_support" {
-  description = "(Optional) Whether DNS support is enabled."
-  default = "enable"
+  description = "(Optional) Whether DNS support is enabled. Valid values: disable, enable. Default value: enable."
+  default     = "enable"
+  type        = string
 }
 
 variable "ipv6_support" {
-  description = "(Optional) Whether IPv6 support is enabled."
-  default = "disable"
+  description = "(Optional) Whether IPv6 support is enabled. Valid values: disable, enable. Default value: disable."
+  default     = "disable"
+  type        = string
+}
+
+variable "name" {
+  description = "(Required) The name of the transit gateway attachment"
+  type        = string
 }
 
 variable "subnet_ids" {
   description = "(Required) Identifiers of EC2 Subnets."
-  type = list
+  type        = list
 }
 
 variable "tags" {
-  description = "(Optional) Map of tags for the EC2 Transit Gateway."
-  default     = {
+  description   = "(Optional) Map of tags for the EC2 Transit Gateway."
+  default       = {
     terraform   = "true"
     environment = "prod"
     project     = "core_infrastructure"
   }
-}
-
-variable "transit_gateway_default_route_table_association" {
-  description = "(Optional) Boolean whether the VPC Attachment should be associated with the EC2 Transit Gateway association default route table. This cannot be configured or perform drift detection with Resource Access Manager shared EC2 Transit Gateways."
-  type = bool
-  default = true
-}
-
-variable "transit_gateway_default_route_table_propagation" {
-  description = "(Optional) Boolean whether the VPC Attachment should be associated with the EC2 Transit Gateway association default route table. This cannot be configured or perform drift detection with Resource Access Manager shared EC2 Transit Gateways."
-  type = bool
-  default = true
+  type          = map
 }
 
 variable "transit_gateway_id" {
-  description = "(Optional) Boolean whether the VPC Attachment should propagate routes with the EC2 Transit Gateway propagation default route table. This cannot be configured or perform drift detection with Resource Access Manager shared EC2 Transit Gateways."
+  description = "(Required) Identifier of EC2 Transit Gateway."
+  type        = string
 }
 
 variable "vpc_id" {
-  description = "(Required) Identifier of EC2 VPC."
+  description = "(Required) Identifier of the VPC."
+  type        = string
 }
