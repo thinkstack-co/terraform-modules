3efa415b88d15d985f3cb917845f06369dc5f7d8
Zachary Hill <zhill@thinkstack.co>
2022-04-26 15:31:27 -0400

    main.tf built with all possible arguments

---
+++ modules/terraform/workspace/main.tf
@@ -0,0 +1,25 @@
+resource "tfe_workspace" "this" {
+  agent_pool_id             = var.agent_pool_id
+  allow_destroy_plan        = var.allow_destroy_plan
+  auto_apply                = var.auto_apply
+  description               = var.description
+  execution_mode            = var.execution_mode
+  file_triggers_enabled     = var.file_triggers_enabled
+  global_remote_state       = var.global_remote_state
+  name                      = var.name
+  organization              = var.organization
+  queue_all_runs            = var.queue_all_runs
+  remote_state_consumer_ids = var.remote_state_consumer_ids
+  speculative_enabled       = var.speculative_enabled
+  ssh_key_id                = var.ssh_key_id
+  terraform_version         = var.terraform_version
+  trigger_prefixes          = var.trigger_prefixes
+  tag_names                 = var.tag_names
+  working_directory         = var.working_directory
+  vcs_repo {
+    identifier              = var.identifier
+    branch                  = var.branch
+    ingress_submodules      = var.ingress_submodules
+    oauth_token_id          = var.oauth_token_id
+  }
+}
