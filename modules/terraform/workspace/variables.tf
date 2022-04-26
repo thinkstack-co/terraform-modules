1589a1d49486434f35aff1e1797242398711bb4b
Zachary Hill <zhill@thinkstack.co>
2022-04-26 16:50:24 -0400

    modified file_triggers_enabled default to false

--- modules/terraform/workspace/variables.tf
+++ modules/terraform/workspace/variables.tf
@@ -31,7 +31,7 @@ variable "execution_mode" {
 variable "file_triggers_enabled" {
   description = "(Optional) Whether to filter runs based on the changed files in a VCS push. Defaults to true. If enabled, the working directory and trigger prefixes describe a set of paths which must contain changes for a VCS push to trigger a run. If disabled, any push will trigger a run."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "global_remote_state" {
