cf90eec99c558bbb8a37790ccdeef664c2f54e6e
Zachary Hill <zhill@thinkstack.co>
2022-06-24 12:58:11 -0400

    removed unnecessary outputs

--- modules/aws/vpn/outputs.tf
+++ modules/aws/vpn/outputs.tf
@@ -6,41 +6,6 @@ output "customer_gateway_id" {
   value = aws_customer_gateway.customer_gateway[*].id
 }
 
-output "customer_gateway_bgp_asn" {
-  value = aws_customer_gateway.customer_gateway[*].bgp_asn
-}
-
-output "customer_gateway_ip_address" {
-  value = aws_customer_gateway.customer_gateway[*].ip_address
-}
-
 output "customer_gateway_type" {
   value = aws_customer_gateway.customer_gateway[*].type
 }
-
-/* # Used if enable_transit_gateway_attachment == true
-output "vpn_connection_id" {
-  value = aws_vpn_connection.vpn_connection_transit_gateway_attachment[*].id
-}
-
-output "vpn_connection_tunnel1_address" {
-  value = aws_vpn_connection.vpn_connection_transit_gateway_attachment[*].tunnel1_address
-}
-
-output "vpn_connection_tunnel2_address" {
-  value = aws_vpn_connection.vpn_connection_transit_gateway_attachment[*].tunnel2_address
-}
-
-# Used if enable_transit_gateway_attachment == false
-output "vpn_connection_id" {
-  value = aws_vpn_connection.vpn_connection_vpn_gateway_attachment[*].id
-}
-
-output "vpn_connection_tunnel1_address" {
-  value = aws_vpn_connection.vpn_connection_vpn_gateway_attachment[*].tunnel1_address
-}
-
-output "vpn_connection_tunnel2_address" {
-  value = aws_vpn_connection.vpn_connection_vpn_gateway_attachment[*].tunnel2_address
-}
- */
