58824de69760d6ae221ec71da00e57657ad5db48
Zachary Hill <zhill@thinkstack.co>
2022-06-24 11:19:25 -0400

    created new resource and bool for enable_transit_gateway_attachment which will use either a transit_gateway_id OR a vpn_gateway_id

--- modules/aws/vpn/main.tf
+++ modules/aws/vpn/main.tf
@@ -3,21 +3,35 @@ terraform {
 }
 
 resource "aws_vpn_gateway" "vpn_gateway" {
-  vpc_id = var.vpc_id
-  # availability_zone = var.availability_zone
-  tags = merge(var.tags, ({ "Name" = format("%s_vpn_gw", var.name) }))
+  count             = var.enable_transit_gateway_attachment ? 1 : 0
+  vpc_id            = var.vpc_id
+  availability_zone = var.availability_zone
+  tags              = merge(var.tags, ({ "Name" = format("%s_vpn_gw", var.name) }))
+  amazon_side_asn   = var.amazon_side_asn
 }
 
 resource "aws_customer_gateway" "customer_gateway" {
-  bgp_asn    = var.bgp_asn
-  count      = length(var.ip_address)
-  ip_address = var.ip_address[count.index]
-  type       = var.vpn_type
-  tags       = merge(var.tags, ({ "Name" = format("%s_customer_gw", var.customer_gw_name[count.index]) }))
+  bgp_asn         = var.bgp_asn
+  count           = length(var.ip_address)
+  certificate_arn = var.certificate_arn
+  ip_address      = var.ip_address[count.index]
+  type            = var.vpn_type
+  tags            = merge(var.tags, ({ "Name" = format("%s_customer_gw", var.customer_gw_name[count.index]) }))
 }
 
-resource "aws_vpn_connection" "vpn_connection" {
-  count               = length(var.ip_address)
+# Used if enable_transit_gateway_attachment == true
+resource "aws_vpn_connection" "vpn_connection_transit_gateway_attachment" {
+  count               = var.enable_transit_gateway_attachment ? length(var.ip_address) : 0
+  customer_gateway_id = element(aws_customer_gateway.customer_gateway.*.id, count.index)
+  static_routes_only  = var.static_routes_only
+  tags                = merge(var.tags, ({ "Name" = format("%s_vpn_connection", var.name) }))
+  type                = var.vpn_type
+  transit_gateway_id  = var.transit_gateway_id
+}
+
+# Used if enable_transit_gateway_attachment == false
+resource "aws_vpn_connection" "vpn_connection_vpn_gateway_attachment" {
+  count               = var.enable_transit_gateway_attachment ? 0 : length(var.ip_address)
   customer_gateway_id = element(aws_customer_gateway.customer_gateway.*.id, count.index)
   static_routes_only  = var.static_routes_only
   tags                = merge(var.tags, ({ "Name" = format("%s_vpn_connection", var.name) }))
--- modules/aws/vpn/variables.tf
+++ modules/aws/vpn/variables.tf
@@ -1,10 +1,18 @@
 variable "vpc_id" {
-  description = "VPC ID"
+  type        = string
+  description = "(Required) The VPC ID to create in."
 }
 
 variable "availability_zone" {
-  description = "Availability zone in which to apply the VPN"
-  default     = ""
+  type        = string
+  description = "(Optional) The Availability Zone for the virtual private gateway."
+  default     = null
+}
+
+variable "amazon_side_asn" {
+  type        = number
+  description = "(Optional) The Autonomous System Number (ASN) for the Amazon side of the gateway. If you don't specify an ASN, the virtual private gateway is created with the default ASN."
+  default     = null
 }
 
 variable "customer_gw_name" {
@@ -13,31 +21,51 @@ variable "customer_gw_name" {
 }
 
 variable "tags" {
-  description = "Tags assigned to all created resources"
+  description = "(Optional) A map of tags to assign to the resource. If configured with a provider default_tags configuration block present, tags with matching keys will overwrite those defined at the provider-level."
   default     = {}
 }
 
 variable "name" {
-  description = "Name to be used on all the resources as identifier"
-  default     = "vpn_terraform"
+  type        = string
+  description = "(Required) Name to be used on all the resources as identifier"
 }
 
 variable "bgp_asn" {
-  description = "BGP ASN used for dynamic routing between the customer gateway and AWS gateway"
+  type        = number
+  description = "(Required) The gateway's Border Gateway Protocol (BGP) Autonomous System Number (ASN)."
   default     = 65000
 }
 
+variable "certificate_arn" {
+  type = string
+  description = "(Optional) The Amazon Resource Name (ARN) for the customer gateway certificate."
+}
+
 variable "ip_address" {
-  type        = list
-  description = "Customer gateway external IP address"
+  type        = list(string)
+  description = "(Required) The IPv4 address for the customer gateway device's outside interface."
 }
 
 variable "vpn_type" {
-  description = "Type of VPN tunnel. Currently only supports ipsec.1"
+  type        = string
+  description = "(Required) The type of customer gateway. The only type AWS supports at this time is ipsec.1"
   default     = "ipsec.1"
 }
 
 variable "static_routes_only" {
-  description = "Flag to determine whether or not dynamic or static routing is enabled"
+  type        = bool
+  description = "(Optional, Default true) Whether the VPN connection uses static routes exclusively. Static routes must be used for devices that don't support BGP."
   default     = true
+}
+
+variable "transit_gateway_id" {
+  type        = string
+  description = "(Optional) The ID of the EC2 Transit Gateway."
+  default     = null
+}
+
+variable "enable_transit_gateway_attachment" {
+  type        = bool
+  description = "(Required) A boolean flag to enable/disable the use of a transit gateway attachment rather than VPN gateway attachment. Defaults false."
+  default     = false
 }
