1fe1a60c36d2a2b1bf4ba7c5bde46813eadf034c
Zachary Hill <zhill@thinkstack.co>
2022-06-24 11:37:55 -0400

    added new outputs with the correct resource name values and duplicated for enable_transit_gateway_attachment bool

--- modules/aws/vpn/outputs.tf
+++ modules/aws/vpn/outputs.tf
@@ -18,15 +18,28 @@ output "customer_gateway_type" {
   value = aws_customer_gateway.customer_gateway[*].type
 }
 
+# Used if enable_transit_gateway_attachment == true
 output "vpn_connection_id" {
-  value = aws_vpn_connection.vpn_connection[*].id
+  value = aws_vpn_connection.vpn_connection_transit_gateway_attachment[*].id
 }
 
 output "vpn_connection_tunnel1_address" {
-  value = aws_vpn_connection.vpn_connection[*].tunnel1_address
+  value = aws_vpn_connection.vpn_connection_transit_gateway_attachment[*].tunnel1_address
 }
 
 output "vpn_connection_tunnel2_address" {
-  value = aws_vpn_connection.vpn_connection[*].tunnel2_address
+  value = aws_vpn_connection.vpn_connection_transit_gateway_attachment[*].tunnel2_address
 }
 
+# Used if enable_transit_gateway_attachment == false
+output "vpn_connection_id" {
+  value = aws_vpn_connection.vpn_connection_vpn_gateway_attachment[*].id
+}
+
+output "vpn_connection_tunnel1_address" {
+  value = aws_vpn_connection.vpn_connection_vpn_gateway_attachment[*].tunnel1_address
+}
+
+output "vpn_connection_tunnel2_address" {
+  value = aws_vpn_connection.vpn_connection_vpn_gateway_attachment[*].tunnel2_address
+}
