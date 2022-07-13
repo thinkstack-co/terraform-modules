7359b48d0942384e3461b1c0dece63deebcd23e5
jakeasarus <jjones@thinkstack.co>
2022-07-13 15:26:02 -0400

    lots of stuff

---
+++ modules/aws/fsx/main.tf
@@ -0,0 +1,116 @@
+###########################
+# FSx Instance
+###########################
+
+resource "aws_fsx_windows_file_system" "fsx" {
+  kms_key_id                        = aws_kms_key.key.key_id
+  storage_capacity                  = var.storage_capacity
+  subnet_ids                        = var.subnet_ids
+  throughput_capacity               = var.throughput_capacity
+  backup_id                         = var.backup_id
+  active_directory_id               = var.active_directory_id
+  aliases                           = var.aliases
+  automatic_backup_retention_days   = var.automatic_backup_retention_days
+  copy_tags_to_backups              = var.copy_tags_to_backups
+  daily_automatic_backup_start_time = var.daily_automatic_backup_start_time
+  #replace below var with sg module in this code.
+  security_group_ids                = var.security_group_ids
+  skip_final_backup                 = var.skip_final_backup
+  tags                              = var.tags
+  weekly_maintenance_start_time     = var.weekly_maintenance_start_time
+  deployment_type                   = var.deployment_type
+  preferred_subnet_id               = var.preferred_subnet_id
+  audit_log_configuration {
+    audit_log_destination             = aws_cloudwatch_event_rule.event_rule
+    file_access_audit_log_level       = var.file_access_audit_log_level
+    file_share_access_audit_log_level = var.file_access_audit_log_level
+  }
+  storage_type = var.storage_type
+  # Active Directory Settings
+  self_managed_active_directory {
+    dns_ips                                = var.dns_ips
+    domain_name                            = var.domain_name
+    password                               = var.fsx_password #should be stored in TF cloud workspace
+    username                               = var.fsx_username #should be stored in TF cloud workspace
+    file_system_administrators_group       = var.file_system_administrators_group
+    organizational_unit_distinguished_name = var.organizational_unit_distinguished_name
+  }
+}
+
+###########################
+# KMS Key
+###########################
+resource "aws_kms_key" "key" {
+  description             = var.description
+  deletion_window_in_days = var.deletion_window_in_days
+  enable_key_rotation     = var.enable_key_rotation
+  key_usage               = var.key_usage
+  is_enabled              = var.is_enabled
+  policy                  = var.policy
+  tags                    = var.tags
+}
+
+resource "aws_kms_alias" "alias" {
+  name          = var.name
+  target_key_id = aws_kms_key.key.key_id
+}
+
+###########################
+# CloudWatch Log Group
+###########################
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  count             = (var.enable_audit_logs == true ? 1 : 0)
+  kms_key_id        = aws_kms_key.key[0].arn
+  name_prefix       = var.cloudwatch_name_prefix
+  retention_in_days = var.cloudwatch_retention_in_days
+  tags              = var.tags
+}
+
+###########################
+# IAM Policy
+###########################
+resource "aws_iam_policy" "policy" {
+  count       = (var.enable_audit_logs == true ? 1 : 0)
+  description = var.iam_policy_description
+  name_prefix = var.iam_policy_name_prefix
+  path        = var.iam_policy_path
+  tags        = var.tags
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      Resource = [
+        "${aws_cloudwatch_log_group.log_group[0].arn}:*"
+      ]
+    }]
+  })
+}
+
+###########################
+# IAM Role
+###########################
+
+resource "aws_iam_role" "role" {
+  count                 = (var.enable_audit_logs == true ? 1 : 0)
+  assume_role_policy    = var.iam_role_assume_role_policy
+  description           = var.iam_role_description
+  force_detach_policies = var.iam_role_force_detach_policies
+  max_session_duration  = var.iam_role_max_session_duration
+  name_prefix           = var.iam_role_name_prefix
+  permissions_boundary  = var.iam_role_permissions_boundary
+}
+
+resource "aws_iam_role_policy_attachment" "role_attach" {
+  count      = (var.enable_audit_logs == true ? 1 : 0)
+  role       = aws_iam_role.role[0].name
+  policy_arn = aws_iam_policy.policy[0].arn
+}
+
---
+++ modules/aws/fsx/variables.tf
@@ -0,0 +1,190 @@
+###########################
+# FSx Instance
+###########################
+
+variable "storage_capacity" {
+    description = "(Optional) Storage capacity (GiB) of the file system. Minimum of 32 and maximum of 65536. If the storage type is set to HDD the minimum value is 2000. Required when not creating filesystem for a backup."
+    default     = "2000"
+    type        = string
+}
+
+variable "subnet_ids" {
+    description = "(Required) A list of IDs for the subnets that the file system will be accessible from. To specify more than a single subnet set deployment_type to MULTI_AZ_1."
+}
+
+variable "throughput_capacity" {
+    description = "(Required) Throughput (megabytes per second) of the file system in power of 2 increments. Minimum of 8 and maximum of 2048."
+    default = "50"
+}
+
+variable "backup_id" {
+    description = "(Optional) The ID of the source backup to create the filesystem from."
+}
+
+variable "active_directory_id" {
+    description = "(Optional) The ID for an existing Microsoft Active Directory instance that the file system should join when it's created. Cannot be specified with self_managed_active_directory."
+}
+
+variable "aliases" {
+    description = "(Optional) An array DNS alias names that you want to associate with the Amazon FSx file system. For more information, see Working with DNS Aliases."
+}
+
+variable "automatic_backup_retention_days" {
+    description = "(Optional) The number of days to retain automatic backups. Minimum of 0 and maximum of 90. Defaults to 7. Set to 0 to disable."
+    default = "7"
+}
+
+variable "copy_tags_to_backups" {
+    description = "(Optional) A boolean flag indicating whether tags on the file system should be copied to backups. Defaults to false."
+    default = false
+}
+
+variable "daily_automatic_backup_start_time" {
+    description = "(Optional) The preferred time (in HH:MM format) to take daily automatic backups, in the UTC time zone."
+    default = "23:59"
+}
+
+#replace with an sg module in code
+variable "security_group_ids" {
+    description = "(Optional) A list of IDs for the security groups that apply to the specified network interfaces created for file system access. These security groups will apply to all network interfaces."
+}
+
+variable "skip_final_backup" {
+    description = "(Optional) When enabled, will skip the default final backup taken when the file system is deleted. This configuration must be applied separately before attempting to delete the resource to have the desired behavior. Defaults to false."
+    default = false
+}
+
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the object."
+  default     = {
+    terraform   = "true"
+    created_by  = "ThinkStack"
+    environment = "prod"
+  }
+}
+
+variable "weekly_maintenance_start_time" {
+    description = "(Optional) The preferred start time (in d:HH:MM format) to perform weekly maintenance, in the UTC time zone."
+    default    = "1:01:00"
+}
+
+variable "deployment_type" {
+    description = "(Optional) Specifies the file system deployment type, valid values are MULTI_AZ_1, SINGLE_AZ_1 and SINGLE_AZ_2. Default value is SINGLE_AZ_1."
+    default = "SINGLE_AZ_1"
+}
+
+variable "preferred_subnet_id" {
+    description = "(Optional) Specifies the subnet in which you want the preferred file server to be located. Required for when deployment type is MULTI_AZ_1."
+}
+
+variable "storage_type" {
+    description = "(Optional) Specifies the storage type, Valid values are SSD and HDD. HDD is supported on SINGLE_AZ_2 and MULTI_AZ_1 Windows file system deployment types. Default value is SSD."
+
+}
+
+###########################
+# Self Managed AD Config
+###########################
+
+variable "dns_ips" {
+    description = "(Required) A list of up to two IP addresses of DNS servers or domain controllers in the self-managed AD directory. The IP addresses need to be either in the same VPC CIDR range as the file system or in the private IP version 4 (IPv4) address ranges as specified in RFC 1918."
+    default = ["10.11.1.100", "10.11.2.100"]
+}
+
+variable "domain_name" {
+    description = "(Required) The fully qualified domain name of the self-managed AD directory. For example, corp.example.com."
+}
+
+variable "fsx_password" {
+    description = "(Required) The password for the service account on your self-managed AD domain that Amazon FSx will use to join to your AD domain."
+}
+
+variable "fsx_username" {
+    description = "(Required) The user name for the service account on your self-managed AD domain that Amazon FSx will use to join to your AD domain."
+}
+
+variable "file_system_administrators_group" {
+    description = "(Optional) The name of the domain group whose members are granted administrative privileges for the file system. Administrative privileges include taking ownership of files and folders, and setting audit controls (audit ACLs) on files and folders. The group that you specify must already exist in your domain. Defaults to Domain Admins."
+    default = "Domain Admins"
+}
+
+variable "organizational_unit_distinguished_name" {
+    description = "(Optional) The fully qualified distinguished name of the organizational unit within your self-managed AD directory that the Windows File Server instance will join. For example, OU=FSx,DC=yourdomain,DC=corp,DC=com. Only accepts OU as the direct parent of the file system. If none is provided, the FSx file system is created in the default location of your self-managed AD directory. To learn more, see RFC 2253."
+}
+
+###########################
+# KMS Key
+###########################
+
+variable "description" {
+    description = "description"
+    default = "KMS key to be used with FSx instance."
+}
+
+variable "deletion_window_in_days" {
+    description = ""
+}
+
+variable "enable_key_rotation" {
+    description = ""
+    default = 
+}
+
+variable "key_usage" {
+    description = ""
+}
+
+variable "is_enabled" {
+    description = ""
+}
+
+variable "policy" {
+    description = ""
+}
+
+###########################
+# Audit Log Config
+###########################
+
+#Global
+variable "enable_audit_logs" {
+    description = "Determines count for cloudwatch log group, IAM policy, and IAM role. Defaults to true and enters a count of 1 to create resources."
+    default = true
+}
+
+#FSx Configuration block variables.
+variable "audit_log_destination" {
+    description = "(Optional) The Amazon Resource Name (ARN) for the destination of the audit logs. The destination can be any Amazon CloudWatch Logs log group ARN or Amazon Kinesis Data Firehose delivery stream ARN. Can be specified when file_access_audit_log_level and file_share_access_audit_log_level are not set to DISABLED. The name of the Amazon CloudWatch Logs log group must begin with the /aws/fsx prefix. The name of the Amazon Kinesis Data Firehouse delivery stream must begin with the aws-fsx prefix. If you do not provide a destination in audit_log_destionation, Amazon FSx will create and use a log stream in the CloudWatch Logs /aws/fsx/windows log group."  
+}
+
+variable "file_access_audit_log_level" {
+    description = "(Optional) Sets which attempt type is logged by Amazon FSx for file and folder accesses. Valid values are SUCCESS_ONLY, FAILURE_ONLY, SUCCESS_AND_FAILURE, and DISABLED. Default value is DISABLED."
+}
+
+variable "file_share_access_audit_log_level" {
+    description = "(Optional) Sets which attempt type is logged by Amazon FSx for file share accesses. Valid values are SUCCESS_ONLY, FAILURE_ONLY, SUCCESS_AND_FAILURE, and DISABLED. Default value is DISABLED." 
+}
+
+#Cloudwatch Log Group
+
+variable "cloudwatch_name_prefix" {
+    description = ""  
+}
+
+variable "cloudwatch_retention_in_days" {
+    description = ""
+}
+
+
+#IAM
+variable "iam_policy_description" {
+    description = ""
+}
+
+variable "iam_policy_name_prefix" {
+    description = ""
+}
+
+variable "iam_policy_path" {
+    description = ""
+}
