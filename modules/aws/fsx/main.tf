3b49d1683457eb541562055207f560c8be8a72f6
jakeasarus <jjones@thinkstack.co>
2022-07-20 13:41:38 -0400

    added key policy

--- modules/aws/fsx/main.tf
+++ modules/aws/fsx/main.tf
@@ -46,7 +46,40 @@ resource "aws_kms_key" "key" {
   enable_key_rotation     = var.enable_key_rotation
   key_usage               = var.key_usage
   is_enabled              = var.is_enabled
-  policy                  = var.policy
+  policy                  = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = [
+        {
+            "Sid" = "Enable IAM User Permissions",
+            "Effect" = "Allow",
+            "Principal" = {
+                "AWS" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+            },
+            "Action" = "kms:*",
+            "Resource" = "*"
+        },
+        {
+            "Effect" = "Allow",
+            "Principal" = {
+                "Service" = "logs.${data.aws_region.current.name}.amazonaws.com"
+            },
+            "Action" = [
+                "kms:Encrypt*",
+                "kms:Decrypt*",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:Describe*"
+            ],
+            "Resource" = "*",
+            "Condition" = {
+                "ArnEquals" = {
+                    "kms:EncryptionContext:aws:logs:arn": "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"
+                }
+            }
+        }
+    ]
+})
+
   tags                    = var.tags
 }
 
