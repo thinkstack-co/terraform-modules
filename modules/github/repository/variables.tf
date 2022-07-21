96c442a37d7b6de0deab2358472d6795edfb11af
Zachary Hill <zhill@thinkstack.co>
2022-07-21 15:26:40 -0400

    added all variables framework

---
+++ modules/github/repository/variables.tf
@@ -0,0 +1,149 @@
+variable "template" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "allow_auto_merge" {
+  description = "(Optional) Set to true to allow auto-merging pull requests on the repository."
+  type        = bool
+  default     = false
+}
+
+variable "allow_merge_commit" {
+  description = "(Optional) Set to false to disable merge commits on the repository."
+  type        = bool
+  default     = 
+}
+
+variable "allow_rebase_merge" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "allow_squash_merge" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "archived" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "archive_on_destroy" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "auto_init" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "delete_branch_on_merge" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "description" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "gitignore_template" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "has_downloads" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "has_issues" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "has_projects" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "has_wiki" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "homepage_url" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "license_template" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "is_template" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "name" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "private" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "topics" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "visibility" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "vulnerability_alerts" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "template_owner" {
+  description = ""
+  type        = 
+  default     = 
+}
+
+variable "template_repository" {
+  description = ""
+  type        = 
+  default     = 
+}
