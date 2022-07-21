f799c442f418e6e1d7442ba3efc19b05fb57d324
jakeasarus <jjones@thinkstack.co>
2022-07-21 08:29:10 -0400

    cleaned up code

--- modules/aws/fsx/main.tf
+++ modules/aws/fsx/main.tf
@@ -4,6 +4,7 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
+
 ###########################
 # FSx Instance
 ###########################
@@ -14,12 +15,10 @@ resource "aws_fsx_windows_file_system" "fsx" {
   subnet_ids                        = var.subnet_ids
   throughput_capacity               = var.throughput_capacity
   backup_id                         = var.backup_id
-  #active_directory_id               = var.active_directory_id
   aliases                           = var.aliases
   automatic_backup_retention_days   = var.automatic_backup_retention_days
   copy_tags_to_backups              = var.copy_tags_to_backups
   daily_automatic_backup_start_time = var.daily_automatic_backup_start_time
-  #replace below var with sg module in this code.
   security_group_ids                = var.security_group_ids
   skip_final_backup                 = var.skip_final_backup
   tags                              = var.tags
@@ -105,51 +104,3 @@ resource "aws_cloudwatch_log_group" "log_group" {
   retention_in_days = var.cloudwatch_retention_in_days
   tags              = var.tags
 }
-
-# ###########################
-# # IAM Policy
-# ###########################
-# resource "aws_iam_policy" "policy" {
-#   count       = (var.enable_audit_logs == true ? 1 : 0)
-#   description = var.iam_policy_description
-#   name_prefix = var.iam_policy_name_prefix
-#   path        = var.iam_policy_path
-#   tags        = var.tags
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Effect = "Allow",
-#       Action = [
-#         "logs:CreateLogGroup",
-#         "logs:CreateLogStream",
-#         "logs:PutLogEvents",
-#         "logs:DescribeLogGroups",
-#         "logs:DescribeLogStreams"
-#       ],
-#       Resource = [
-#         "${aws_cloudwatch_log_group.log_group[0].arn}:*"
-#       ]
-#     }]
-#   })
-# }
-
-# ###########################
-# # IAM Role
-# ###########################
-
-# resource "aws_iam_role" "role" {
-#   count                 = (var.enable_audit_logs == true ? 1 : 0)
-#   assume_role_policy    = var.iam_role_assume_role_policy
-#   description           = var.iam_role_description
-#   force_detach_policies = var.iam_role_force_detach_policies
-#   max_session_duration  = var.iam_role_max_session_duration
-#   name_prefix           = var.iam_role_name_prefix
-#   permissions_boundary  = var.iam_role_permissions_boundary
-# }
-
-# resource "aws_iam_role_policy_attachment" "role_attach" {
-#   count      = (var.enable_audit_logs == true ? 1 : 0)
-#   role       = aws_iam_role.role[0].name
-#   policy_arn = aws_iam_policy.policy[0].arn
-# }
-
