22dee5ef3fb6ac57d265066dd4e91b13f8254291
Zachary Hill <zhill@thinkstack.co>
2022-07-29 17:30:06 -0400

    added iam policy and role

--- modules/aws/transfer_family/main.tf
+++ modules/aws/transfer_family/main.tf
@@ -1,4 +1,60 @@
-resource "aws_transfer_server" "example" {
+terraform {
+  required_version = ">= 0.15.0"
+}
+
+###########################
+# Data Sources
+###########################
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+###########################
+# IAM Policy
+###########################
+resource "aws_iam_policy" "cloudwatch_logging_policy" {
+  description = var.iam_policy_description
+  name_prefix = var.iam_policy_name_prefix
+  path        = var.iam_policy_path
+  tags        = var.tags
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+        Effect = "Allow",
+        Action = [
+            "logs:DescribeLogStreams",
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+        ],
+        Resource = [
+            "*"
+        ]
+        }]
+    })
+}
+
+###########################
+# IAM Role
+###########################
+
+resource "aws_iam_role" "cloudwatch_logging_role" {
+  assume_role_policy    = var.iam_role_assume_role_policy
+  description           = var.iam_role_description
+  force_detach_policies = var.iam_role_force_detach_policies
+  max_session_duration  = var.iam_role_max_session_duration
+  name_prefix           = var.iam_role_name_prefix
+  permissions_boundary  = var.iam_role_permissions_boundary
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logging_role_attach" {
+  role       = aws_iam_role.cloudwatch_logging_role.name
+  policy_arn = aws_iam_policy.cloudwatch_logging_policy.arn
+}
+
+##################################
+# Transfer Family Server
+##################################
+resource "aws_transfer_server" "this" {
   certificate                      = var.certificate
   domain                           = var.domain
   protocols                        = var.protocols
---
+++ modules/aws/transfer_family/outputs.tf
@@ -0,0 +1,11 @@
+output "arn" {
+    value = aws_transfer_server.this.arn
+}
+
+output "id" {
+    value = aws_transfer_server.this.id
+}
+
+output "endpoint" {
+    value = aws_transfer_server.this.endpoint
+}
