ecf45244bf37ea90929539809fd178657cdb657f
Zachary Hill <zhill@thinkstack.co>
2022-07-31 22:37:04 -0400

    updated the script filetype to .sh

--- modules/thinkstack/siem/main.tf
+++ modules/thinkstack/siem/main.tf
@@ -208,7 +208,7 @@ resource "aws_instance" "ec2" {
   subnet_id              = aws_subnet.private_subnets[count.index].id
   tags                   = merge(var.tags, ({ "Name" = format("%s%d", var.name, count.index + 1) }))
   tenancy                = var.tenancy
-  user_data              = templatefile("${path.module}/snypr_centos_script.txt")
+  user_data              = templatefile("${path.module}/snypr_centos_script.sh")
   volume_tags            = merge(var.tags, ({ "Name" = format("%s%d", var.name, count.index + 1) }))
   vpc_security_group_ids = [aws_security_group.sg.id]
 
