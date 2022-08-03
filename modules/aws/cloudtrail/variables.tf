e0923c27b82bdd7bcfd4ba40fcd95fba636f9f2d
Zachary Hill <zhill@thinkstack.co>
2022-08-03 13:50:48 -0400

    Added the bucket key

--- modules/aws/cloudtrail/variables.tf
+++ modules/aws/cloudtrail/variables.tf
@@ -17,6 +17,12 @@ variable "versioning_status" {
   default     = "Enabled"
 }
 
+variable "bucket_key_enabled" {
+  type        = bool
+  description = "(Optional) Whether or not to use Amazon S3 Bucket Keys for SSE-KMS."
+  default     = true
+}
+
 variable "kms_master_key_id" {
   type        = string
   description = "(Optional) The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms."
