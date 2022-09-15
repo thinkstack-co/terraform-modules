7507666b6c5e48074a5cc19d3813ff95ec906f31
jakeasarus <jjones@thinkstack.co>
2022-09-15 15:01:08 -0400

    updated module to include sg for vpc dev_vpc_endpoints

---
+++ modules/aws/vpc/security_groups/ssm_vpc_endpoint_sg/variables.tf
@@ -0,0 +1,29 @@
+variable "cidr_blocks" {
+  description = "security group allowed cidr blocks"
+  type        = list
+}
+
+variable "description" {
+  type = string
+  description = "(Optional, Forces new resource) The security group description. Defaults to 'Managed by Terraform'. Cannot be blank. NOTE: This field maps to the AWS GroupDescription attribute, for which there is no Update API. If you'd like to classify your security groups in a way that can be updated, use tags."
+}
+
+variable "name" {
+  description = "(Optional, Forces new resource) The name of the security group. If omitted, Terraform will assign a random, unique name"
+}
+
+variable "security_groups" {
+  description = "(Optional) List of security group Group Names if using EC2-Classic, or Group IDs if using a VPC."
+  default     = []
+}
+
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the resource."
+  default     = {
+    terraform = "true"
+  }
+}
+
+variable "vpc_id" {
+  description = "The VPC id to add the security group"
+}
