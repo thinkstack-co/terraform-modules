24a0f1f7ef7b9114278db8149067f801f645a46f
Zachary Hill <zhill@thinkstack.co>
2022-10-04 13:03:57 -0600

    updated the security groups attached to the correct nics

--- modules/aws/cato_sdwan/main.tf
+++ modules/aws/cato_sdwan/main.tf
@@ -79,7 +79,7 @@ resource "aws_network_interface" "mgmt_nic" {
   count             = var.number
   description       = var.mgmt_nic_description
   private_ips       = var.mgmt_ips
-  security_groups   = [aws_security_group.cato_sdwan_sg.id]
+  security_groups   = [aws_security_group.cato_wan_mgmt_sg.id]
   subnet_id         = element(var.mgmt_subnet_id, count.index)
   tags              = merge(var.tags, ({ "Name" = format("%s%d_mgmt", var.instance_name_prefix, count.index + 1) }))
 }
@@ -88,7 +88,7 @@ resource "aws_network_interface" "public_nic" {
   count             = var.number
   description       = var.public_nic_description
   private_ips       = [element(var.public_ips, count.index)]
-  security_groups   = [aws_security_group.cato_sdwan_sg.id]
+  security_groups   = [aws_security_group.cato_wan_mgmt_sg.id]
   subnet_id         = element(var.public_subnet_id, count.index)
   tags              = merge(var.tags, ({ "Name" = format("%s%d_public", var.instance_name_prefix, count.index + 1) }))
 
@@ -102,7 +102,7 @@ resource "aws_network_interface" "private_nic" {
   count             = var.number
   description       = var.private_nic_description
   private_ips       = [element(var.private_ips, count.index)]
-  security_groups   = [aws_security_group.cato_sdwan_sg.id]
+  security_groups   = [aws_security_group.cato_lan_sg.id]
   source_dest_check = var.source_dest_check
   subnet_id         = element(var.private_subnet_id, count.index)
   tags              = merge(var.tags, ({ "Name" = format("%s%d_private", var.instance_name_prefix, count.index + 1) }))
