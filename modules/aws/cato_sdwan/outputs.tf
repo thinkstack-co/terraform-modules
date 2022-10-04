7cc664fb1231166b141c2a4b401f487f8d5a8ffd
Zachary Hill <zhill@thinkstack.co>
2022-10-04 13:01:44 -0600

    updated the outputs to reflect the correct resource names

--- modules/aws/cato_sdwan/outputs.tf
+++ modules/aws/cato_sdwan/outputs.tf
@@ -1,31 +1,23 @@
-output "security_group_id" {
-  value = aws_security_group.cato_sdwan_sg.id
-}
-
 output "eip_id" {
-  value = aws_eip.external_ip[*].id
-}
-
-output "eip_private_ip" {
-  value = aws_eip.external_ip[*].private_ip
+  value = aws_eip.wan_external_ip[*].id
 }
 
 output "eip_public_ip" {
-  value = aws_eip.external_ip[*].public_ip
+  value = aws_eip.wan_external_ip[*].public_ip
 }
 
 output "ec2_instance_id" {
   value = aws_instance.ec2_instance[*].id
 }
 
-output "network_interface_id" {
-  value = aws_network_interface.fw_public_nic[*].id
+output "public_network_interface_id" {
+  value = aws_network_interface.public_nic[*].id
 }
 
 output "private_network_interface_id" {
-  value = aws_network_interface.fw_private_nic[*].id
+  value = aws_network_interface.private_nic[*].id
 }
 
-output "dmz_network_interface_id" {
-  value = aws_network_interface.fw_dmz_nic[*].id
+output "mgmt_network_interface_id" {
+  value = aws_network_interface.mgmt_nic[*].id
 }
