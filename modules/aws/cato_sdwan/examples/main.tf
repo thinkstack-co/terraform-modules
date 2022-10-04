4ee70b585e7db8fcf792546823a89b5c2836d77e
Zachary Hill <zhill@thinkstack.co>
2022-10-04 12:45:33 -0600

    updated the example file

--- modules/aws/cato_sdwan/examples/main.tf
+++ modules/aws/cato_sdwan/examples/main.tf
@@ -1,24 +1,21 @@
-module "aws_ec2_fortigate_fw" {
-  source = "github.com/thinkstack-co/terraform-modules//modules/aws/cato_sdwan"
+module "aws_prod_cato" {
+  source            = "github.com/thinkstack-co/terraform-modules//modules/aws/ec2_instance"
 
-  vpc_id               = module.vpc.vpc_id
-  number_of_instances  = 2
-  public_subnet_id     = module.vpc.public_subnet_ids
-  private_subnet_id    = module.vpc.private_subnet_ids
-  ami_id               = "ami-ffffffff"
-  instance_type        = "m3.medium"
-  key_name             = module.keypair.key_name
-  instance_name_prefix = "aws_fw"
+  ami               = "ami-023xxdfb5fdsxfx1e"
+  availability_zone = module.vpc.availability_zone[0]
+  key_name          = module.keypair.key_name
+  instance_type     = "c5.large"
+  mgmt_subnet_id    = module.vpc.mgmt_subnet_ids
+  public_subnet_id  = module.vpc.public_subnet_ids
+  private_subnet_id = module.vpc.private_subnet_ids
+  vpc_id            = module.vpc.vpc_id
 
-  tags = {
-    terraform         = "true"
-    created_by        = "terraform"
-    environment       = "prod"
-    project           = "core_infrastructure"
-    role              = "fortigate_firewall"
-    backup            = "true"
-    hourly_retention  = "7"
-    daily_retention   = "14"
-    monthly_retention = "60"
+  tags              = {
+    terraform   = "true"
+    created_by  = "Zachary Hill"
+    environment = "prod"
+    project     = "aws_poc"
+    backup      = "true"
+    role        = "cato_sdwan"
   }
 }
