dd723fcf94382526bb6f25b00fca6ab7ebf65ca2
jakeasarus <jjones@thinkstack.co>
2022-11-21 13:39:12 -0500

    testing logs

--- modules/aws/s3/main.tf
+++ modules/aws/s3/main.tf
@@ -7,11 +7,6 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket_prefix = var.bucket_prefix
   policy        = var.policy
 
-  /*logging {
-        target_bucket = var.target_bucket
-        target_prefix = var.target_prefix
-    }*/
-
   versioning {
     enabled    = var.versioning
     mfa_delete = var.mfa_delete
@@ -30,4 +25,12 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 
   tags = var.tags
+
+resource "aws_s3_bucket_logging" "logging" {
+  bucket = aws_s3_bucket.s3_bucket
+
+  target_bucket = var.log_bucket
+  target_prefix = "log/"
+}
+
 }
