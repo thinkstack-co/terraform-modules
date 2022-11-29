dd3e3d07ea958cdef5547289b0b576d839b4d230
Ayo Alabi <ayoalabi@Ayos-MBP.attlocal.net>
2022-11-29 08:49:41 -0600

    added S3 write events in cloudtrail

--- modules/aws/cloudtrail/main.tf
+++ modules/aws/cloudtrail/main.tf
@@ -10,6 +10,15 @@ resource "aws_cloudtrail" "cloudtrail" {
   name                          = var.name
   s3_bucket_name                = aws_s3_bucket.cloudtrail_s3_bucket.id
   s3_key_prefix                 = var.s3_key_prefix
+  
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+
+  data_resource {
+    type   = "AWS::S3::Object"
+    values = ["arn:aws:s3"]
+  }
 }
 
 resource "aws_s3_bucket" "cloudtrail_s3_bucket" {
