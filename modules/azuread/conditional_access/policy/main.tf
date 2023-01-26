8ad68f3456c03a55bd8449a0de923f0848d94edb
Zachary Hill <zhill@thinkstack.co>
2023-01-26 13:21:37 -0500

    added all attributes for conditional access

--- modules/azuread/conditional_access/policy/main.tf
+++ modules/azuread/conditional_access/policy/main.tf
@@ -8,42 +8,50 @@ resource "azuread_conditional_access_policy" "example" {
     user_risk_levels    = var.user_risk_levels
 
     applications {
-      included_applications = var.included_applications
       excluded_applications = var.excluded_applications
+      included_applications = var.included_applications
+      included_user_actions = var.included_user_actions
     }
 
     devices {
       filter {
-        mode = "exclude"
-        rule = "device.operatingSystem eq \"Doors\""
+        mode = var.filter_mode
+        rule = var.filter_rule
       }
     }
 
     locations {
-      included_locations = var.included_locations
       excluded_locations = var.excluded_locations
+      included_locations = var.included_locations
     }
 
     platforms {
-      included_platforms = var.included_platforms
       excluded_platforms = var.excluded_platforms
+      included_platforms = var.included_platforms
     }
 
     users {
-      included_users = var.included_users
+      excluded_groups = var.excluded_groups
+      excluded_roles = var.excluded_roles
       excluded_users = var.excluded_users
+      included_groups = var.included_groups
+      included_roles = var.included_roles
+      included_users = var.included_users
     }
   }
 
   grant_controls {
-    operator          = var.operator
-    built_in_controls = var.built_in_controls
+    built_in_controls             = var.built_in_controls
+    custom_authentication_factors = var.custom_authentication_factors
+    operator                      = var.operator
+    terms_of_use                  = var.terms_of_use
   }
 
   session_controls {
     application_enforced_restrictions_enabled = var.application_enforced_restrictions_enabled
+    cloud_app_security_policy                 = var.cloud_app_security_policy
+    persistent_browser_mode                   = var.persistent_browser_mode
     sign_in_frequency                         = var.sign_in_frequency
     sign_in_frequency_period                  = var.sign_in_frequency_period
-    cloud_app_security_policy                 = var.cloud_app_security_policy
   }
 }
