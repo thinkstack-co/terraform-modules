59da01d85b0e5d6a19baf088c19f493940ea2708
Zachary Hill <zhill@thinkstack.co>
2023-01-26 14:15:07 -0500

    added conditions - users variables

--- modules/azuread/conditional_access/policy/variables.tf
+++ modules/azuread/conditional_access/policy/variables.tf
@@ -28,7 +28,7 @@ variable "client_app_types" {
 variable "sign_in_risk_levels" {
     type = list(string)
     description = "(Optional) A list of sign-in risk levels included in the policy. Possible values are: low, medium, high, hidden, none, unknownFutureValue."
-    default = ""
+    default = null
     validation {
       condition = contains(["low", "medium", "high", "hidden", "none", "unknownFutureValue"])
       error_message = "The sign_in_risk_levels value must be a list of strings and one or more of the following possible values: 'low', 'medium', 'high', 'hidden', 'none', 'unknownFutureValue'"
@@ -51,19 +51,19 @@ variable "user_risk_levels" {
 variable "excluded_applications" {
     type = list(string)
     description = "(Optional) A list of application IDs explicitly excluded from the policy. Can also be set to Office365."
-    default = []
+    default = null
 }
 
 variable "included_applications" {
     type = list(string)
     description = "(Optional) A list of application IDs the policy applies to, unless explicitly excluded (in excluded_applications). Can also be set to All, None or Office365. Cannot be specified with included_user_actions. One of included_applications or included_user_actions must be specified."
-    default = []
+    default = null
 }
 
 variable "included_user_actions" {
     type = list(string)
     description = "(Optional) A list of user actions to include. Supported values are urn:user:registerdevice and urn:user:registersecurityinfo. Cannot be specified with included_applications. One of included_applications or included_user_actions must be specified."
-    default = []
+    default = null
 }
 
 ################
@@ -73,7 +73,7 @@ variable "included_user_actions" {
 variable "device_filter_mode" {
     type = string
     description = "(Optional) Whether to include in, or exclude from, matching devices from the policy. Supported values are include or exclude."
-    default = ""
+    default = null
     validation {
         condition = contains(["include", "exclude"])
         error_message = "The device_filter_mode value must be a string and one of the following possible values: 'include' or 'exclude'"
@@ -83,7 +83,7 @@ variable "device_filter_mode" {
 variable "device_filter_rule" {
     type = string
     description = "(Required) Condition filter to match devices. For more information, see official documentation."
-    default = ""
+    default = null
 }
 
 ################
@@ -93,13 +93,13 @@ variable "device_filter_rule" {
 variable "excluded_locations" {
     type = list(string)
     description = "(Optional) A list of location IDs excluded from scope of policy. Can also be set to AllTrusted."
-    default = []
+    default = null
 }
 
 variable "included_locations" {
     type = list(string)
     description = "(Required) A list of location IDs in scope of policy unless explicitly excluded. Can also be set to All, or AllTrusted."
-    default = []
+    default = null
 }
 
 ################
@@ -123,3 +123,52 @@ variable "included_platforms" {
       error_message = "The included_platforms value must be a list of strings and one or more from the following possible values: 'all', 'android', 'iOS', 'linux', 'macOS', 'windows', 'windowsPhone', 'unknownFutureValue'"
     }
 }
+
+################
+# Conditions - Users
+################
+
+variable "excluded_groups" {
+    type = list(string)
+    description = "(Optional) A list of group IDs excluded from scope of policy."
+    default = null
+}
+
+variable "excluded_roles" {
+    type = list(string)
+    description = "(Optional) A list of role IDs excluded from scope of policy."
+    default = null
+}
+
+variable "excluded_users" {
+    type = list(string)
+    description = "(Optional) A list of user IDs excluded from scope of policy and/or GuestsOrExternalUsers."
+    default = null
+}
+
+variable "included_groups" {
+    type = list(string)
+    description = "(Optional) A list of group IDs in scope of policy unless explicitly excluded."
+    default = null
+}
+
+variable "included_roles" {
+    type = list(string)
+    description = "(Optional) A list of role IDs in scope of policy unless explicitly excluded."
+    default = null
+}
+
+variable "included_users" {
+    type = list(string)
+    description = "(Optional) A list of user IDs in scope of policy unless explicitly excluded, or None or All or GuestsOrExternalUsers."
+    default = null
+}
+
+################
+# Grant Controls
+################
+
+
+################
+# Session Controls
+################
