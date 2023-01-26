d29dbf5b0eec53b6a46046fe86a86413e1a1266a
Zachary Hill <zhill@thinkstack.co>
2023-01-26 13:46:29 -0500

    added conditions to the variables

--- modules/azuread/conditional_access/policy/variables.tf
+++ modules/azuread/conditional_access/policy/variables.tf
@@ -19,15 +19,27 @@ variable "state" {
 variable "client_app_types" {
     type = list(string)
     description = "(Required) A list of client application types included in the policy. Possible values are: all, browser, mobileAppsAndDesktopClients, exchangeActiveSync, easSupported and other."
+    validation {
+      condition = contains(["all", "browser", "mobileAppsAndDesktopClients", "exchangeActiveSync", "easSupported", "other"])
+      error_message = "The client_app_types value must be a list of strings and one or more of the following possibile values: 'all', 'browser', 'mobileAppsAndDesktopClients', 'exchangeActiveSync', 'easSupported', 'other'"
+    }
 }
 
 variable "sign_in_risk_levels" {
     type = list(string)
     description = "(Optional) A list of sign-in risk levels included in the policy. Possible values are: low, medium, high, hidden, none, unknownFutureValue."
     default = ""
+    validation {
+      condition = contains(["low", "medium", "high", "hidden", "none", "unknownFutureValue"])
+      error_message = "The sign_in_risk_levels value must be a list of strings and one or more of the following possible values: 'low', 'medium', 'high', 'hidden', 'none', 'unknownFutureValue'"
+    }
 }
 
 variable "user_risk_levels" {
     type = list(string)
     description = "(Optional) A list of user risk levels included in the policy. Possible values are: low, medium, high, hidden, none, unknownFutureValue."
+    validation {
+      condition = contains(["low", "medium", "high", "hidden", "none", "unknownFutureValue"])
+      error_message = "The user_risk_levels value must be a list of strings and one or more of the following possible values: 'low', 'medium', 'high', 'hidden', 'none', 'unknownFutureValue'"
+    }
 }
