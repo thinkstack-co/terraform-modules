7a336a007188e7a6852934ee8092d5c19e40c575
Zachary Hill <zhill@thinkstack.co>
2023-01-31 11:29:56 -0500

    added lifecycle ignore_changes = ami to all EC2 instance modules which could increment and need a new AMI to accomplish

--- modules/aws/ec2_domain_controller/main.tf
+++ modules/aws/ec2_domain_controller/main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids = var.vpc_security_group_ids
 
   lifecycle {
-    ignore_changes = [user_data, ami]
+    ignore_changes = [ami, user_data]
   }
 }
 
