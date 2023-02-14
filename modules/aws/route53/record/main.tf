bd15fa2a75890c427d37e4ade42afa1095ad33bc
Zachary Hill <zhill@thinkstack.co>
2023-02-14 13:32:25 -0500

    filling out the record with different types of routing policies

--- modules/aws/route53/record/main.tf
+++ modules/aws/route53/record/main.tf
@@ -1,7 +1,32 @@
 resource "aws_route53_record" "this" {
-  zone_id = var.zone_id
-  name    = var.name
-  type    = var.type
-  ttl     = var.ttl
-  records = var.records
+    zone_id                    = var.zone_id
+    name                       = var.name
+    type                       = var.type
+    ttl                        = var.ttl
+    records                    = var.records
+
+    set_identifier             = var.set_identifier
+    health_check_id            = var.health_check_id
+    alias {
+        name                   = var.alias_name
+        zone_id                = var.alias_zone_id
+        evaluate_target_health = var.alias_evaluate_target_health
+    }
+    weighted_routing_policy {
+        weight                 = var.weighted_routing_policy_weight
+    }
+    latency_routing_policy {
+        region                 = var.latency_routing_policy_region
+    }
+    geolocation_routing_policy {
+        continent              = var.geolocation_routing_policy_continent
+        country                = var.geolocation_routing_policy_country
+        subdivision            = var.geolocation_routing_policy_subdivision
+    }
+    failover_routing_policy {
+        type                   = var.failover_routing_policy_type
+    }
+    multivalue_answer_routing_policy {
+        enabled                = var.multivalue_answer_routing_policy_enabled
+    }
 }
--- modules/aws/route53/record/variables.tf
+++ modules/aws/route53/record/variables.tf
@@ -22,3 +22,63 @@ variable "records" {
     type = list(string)
     description = "(Required for non-alias records) A string list of records. To specify a single record value longer than 255 characters such as a TXT record for DKIM, add \"\" inside the Terraform configuration string."
 }
+
+variable "set_identifier" {
+    type = string
+    description = "(Optional) Unique identifier to differentiate records with routing policies from one another. Required if using failover, geolocation, latency, multivalue_answer, or weighted routing policies documented below."
+}
+
+variable "health_check_id" {
+    type = string
+    description = "(Optional) The health check the record should be associated with."
+}
+
+variable "alias_name" {
+    type = string
+    description = "(Required) DNS domain name for a CloudFront distribution, S3 bucket, ELB, or another resource record set in this hosted zone."
+}
+
+variable "alias_zone_id" {
+    type = string
+    description = "(Required) Hosted zone ID for a CloudFront distribution, S3 bucket, ELB, or Route 53 hosted zone. See resource_elb.zone_id for example."
+}
+
+variable "alias_evaluate_target_health" {
+    type = bool
+    description = "(Required) Set to true if you want Route 53 to determine whether to respond to DNS queries using this resource record set by checking the health of the resource record set. Some resources have special requirements, see related part of documentation."
+}
+
+variable "weighted_routing_policy_weight" {
+    type = number
+    description = "(Required) A numeric value indicating the relative weight of the record. See http://docs.aws.amazon.com/Route53/latest/DeveloperGuide/routing-policy.html#routing-policy-weighted."
+}
+
+variable "latency_routing_policy_region" {
+    type = string
+    description = "(Required) An AWS region from which to measure latency. See http://docs.aws.amazon.com/Route53/latest/DeveloperGuide/routing-policy.html#routing-policy-latency"
+}
+
+variable "geolocation_routing_policy_continent" {
+    type = string
+    description = "A two-letter continent code. See http://docs.aws.amazon.com/Route53/latest/APIReference/API_GetGeoLocation.html for code details. Either continent or country must be specified."
+}
+
+variable "geolocation_routing_policy_country" {
+    type = string
+    description = "A two-character country code or * to indicate a default resource record set."
+}
+
+variable "geolocation_routing_policy_subdivision" {
+    type = string
+    description = "(Optional) A subdivision code for a country."
+}
+
+variable "failover_routing_policy_type" {
+    type = string
+    description = "(Required) PRIMARY or SECONDARY. A PRIMARY record will be served if its healthcheck is passing, otherwise the SECONDARY will be served. See http://docs.aws.amazon.com/Route53/latest/DeveloperGuide/dns-failover-configuring-options.html#dns-failover-failover-rrsets"
+}
+
+variable "multivalue_answer_routing_policy_enabled" {
+    type = bool
+    description = "(Optional) Set to true to indicate a multivalue answer routing policy. Conflicts with any other routing policy."
+}
