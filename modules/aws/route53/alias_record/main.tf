6e05cc4b58fcf9214a5348afbdb3c3a29ad1632e
Zachary Hill <zhill@thinkstack.co>
2023-02-14 19:29:26 -0500

    removed routing patterns from alias_record for now

--- modules/aws/route53/alias_record/main.tf
+++ modules/aws/route53/alias_record/main.tf
@@ -10,18 +10,4 @@ resource "aws_route53_record" "this" {
         zone_id                      = var.alias_zone_id
         evaluate_target_health       = var.alias_evaluate_target_health
     }
-    weighted_routing_policy {
-        weight                       = var.weighted_routing_policy_weight
-    }
-    latency_routing_policy {
-        region                       = var.latency_routing_policy_region
-    }
-    geolocation_routing_policy {
-        continent                    = var.geolocation_routing_policy_continent
-        country                      = var.geolocation_routing_policy_country
-        subdivision                  = var.geolocation_routing_policy_subdivision
-    }
-    failover_routing_policy {
-        type                         = var.failover_routing_policy_type
-    }
 }
