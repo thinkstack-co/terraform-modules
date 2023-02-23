5abfe4d0a29732ab08a9d41b2269b5506ad76126
Zachary Hill <zhill@thinkstack.co>
2023-02-23 17:54:03 -0500

    unique situation with cloudwatch_arn requiring wildcard for logstreams

--- modules/aws/cloudtrail/main.tf
+++ modules/aws/cloudtrail/main.tf
@@ -153,7 +153,7 @@ resource "aws_cloudtrail" "cloudtrail" {
   name                          = var.name
   s3_bucket_name                = aws_s3_bucket.cloudtrail_s3_bucket.id
   s3_key_prefix                 = var.s3_key_prefix
-  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.cloudtrail.arn
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*" # CloudTrail requires the Log Stream wildcard
   cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail.arn
 }
 
