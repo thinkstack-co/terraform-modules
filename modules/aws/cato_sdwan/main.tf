7e9021aacb095ba83ac7ef976cda38ecd9382dfe
Zachary Hill <zhill@thinkstack.co>
2023-02-23 12:11:23 -0500

    updated the splat expression in cato_sdwan module to the latest syntax

--- modules/aws/cato_sdwan/main.tf
+++ modules/aws/cato_sdwan/main.tf
@@ -71,8 +71,8 @@ resource "aws_eip" "wan_external_ip" {
 
 resource "aws_eip_association" "wan_external_ip" {
   count                = var.number
-  allocation_id        = element(aws_eip.wan_external_ip.*.id, count.index)
-  network_interface_id = element(aws_network_interface.public_nic.*.id, count.index)
+  allocation_id        = element(aws_eip.wan_external_ip[*].id, count.index)
+  network_interface_id = element(aws_network_interface.public_nic[*].id, count.index)
 }
 
 ############################################
@@ -97,7 +97,7 @@ resource "aws_network_interface" "public_nic" {
   tags              = merge(var.tags, ({ "Name" = format("%s%d_public", var.instance_name_prefix, count.index + 1) }))
 
   attachment {
-    instance     = element(aws_instance.ec2_instance.*.id, count.index)
+    instance     = element(aws_instance.ec2_instance[*].id, count.index)
     device_index = 1
   }
 }
@@ -112,7 +112,7 @@ resource "aws_network_interface" "private_nic" {
   tags              = merge(var.tags, ({ "Name" = format("%s%d_private", var.instance_name_prefix, count.index + 1) }))
 
   attachment {
-    instance     = element(aws_instance.ec2_instance.*.id, count.index)
+    instance     = element(aws_instance.ec2_instance[*].id, count.index)
     device_index = 2
   }
 }
@@ -135,7 +135,7 @@ resource "aws_instance" "ec2_instance" {
   user_data            = var.user_data
 
   network_interface {
-    network_interface_id = element(aws_network_interface.mgmt_nic.*.id, count.index)
+    network_interface_id = element(aws_network_interface.mgmt_nic[*].id, count.index)
     device_index         = 0
   }
 
@@ -158,12 +158,12 @@ resource "aws_cloudwatch_metric_alarm" "instance" {
   actions_enabled     = true
   alarm_actions       = []
   alarm_description   = "EC2 instance StatusCheckFailed_Instance alarm"
-  alarm_name          = format("%s-instance-alarm", element(aws_instance.ec2_instance.*.id, count.index))
+  alarm_name          = format("%s-instance-alarm", element(aws_instance.ec2_instance[*].id, count.index))
   comparison_operator = "GreaterThanOrEqualToThreshold"
   count               = var.number
   datapoints_to_alarm = 2
   dimensions = {
-    InstanceId = element(aws_instance.ec2_instance.*.id, count.index)
+    InstanceId = element(aws_instance.ec2_instance[*].id, count.index)
   }
   evaluation_periods        = "2"
   insufficient_data_actions = []
@@ -185,12 +185,12 @@ resource "aws_cloudwatch_metric_alarm" "system" {
   actions_enabled     = true
   alarm_actions       = ["arn:aws:automate:${data.aws_region.current.name}:ec2:recover"]
   alarm_description   = "EC2 instance StatusCheckFailed_System alarm"
-  alarm_name          = format("%s-system-alarm", element(aws_instance.ec2_instance.*.id, count.index))
+  alarm_name          = format("%s-system-alarm", element(aws_instance.ec2_instance[*].id, count.index))
   comparison_operator = "GreaterThanOrEqualToThreshold"
   count               = var.number
   datapoints_to_alarm = 2
   dimensions = {
-    InstanceId = element(aws_instance.ec2_instance.*.id, count.index)
+    InstanceId = element(aws_instance.ec2_instance[*].id, count.index)
   }
   evaluation_periods        = "2"
   insufficient_data_actions = []
