10c9f33db9ec2fcf236d473d0c1fa5bc10772359
Zachary Hill <zhill@thinkstack.co>
2023-02-23 14:31:52 -0500

    removed testing code to show tfsec would flag the issue

--- modules/aws/cloudtrail/main.tf
+++ modules/aws/cloudtrail/main.tf
@@ -9,24 +9,6 @@ terraform {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-# Testing code which should fail tfsec
-
- resource "aws_cloudtrail" "bad_example" {
-   is_multi_region_trail = true
-
-   event_selector {
-     read_write_type           = "All"
-     include_management_events = true
-
-     data_resource {
-       type = "AWS::S3::Object"
-       values = ["${data.aws_s3_bucket.important-bucket.arn}/"]
-     }
-   }
- }
-
-
-
 ###########################
 # KMS Encryption Key
 ###########################
