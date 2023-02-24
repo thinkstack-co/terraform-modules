3fe3c0885e37a5cda6e7fec44c5a4b3cfd46b349
Zachary Hill <zhill@thinkstack.co>
2023-02-24 18:20:48 -0500

    linting ec2_instance module

--- modules/aws/ec2_instance/variables.tf
+++ modules/aws/ec2_instance/variables.tf
@@ -1,49 +1,62 @@
 variable "ami" {
+  type        = string
   description = "(Required) AMI ID to use when launching the instance"
 }
 
 variable "associate_public_ip_address" {
+  type        = bool
   description = "If true, the EC2 instance will have associated public IP address"
   default     = false
+  validation {
+    condition     = can(regex("^(true|false)$", var.disable_api_termination))
+    error_message = "The value must be either true or false."
+  }
 }
 
 variable "availability_zone" {
+  type        = string
   description = "The AZ to start the instance in"
-  default     = ""
+  default     =  ""
 }
 
 variable "number" {
+  type        = number
   description = "Number of instances to launch"
   default     = 1
 }
 
 variable "disable_api_termination" {
+  type        = bool
   description = "If true, enables EC2 Instance Termination Protection"
   default     = false
-}
-
-variable "ebs_block_device" {
-  description = "Additional EBS block devices to attach to the instance"
-  default     = []
+  validation {
+    condition     = can(regex("^(true|false)$", var.disable_api_termination))
+    error_message = "The value must be either true or false."
+  }
 }
 
 variable "ebs_optimized" {
+  type        = bool
   description = "If true, the launched EC2 instance will be EBS-optimized"
   default     = false
+  validation {
+    condition     = can(regex("^(true|false)$", var.disable_api_termination))
+    error_message = "The value must be either true or false."
+  }
 }
 
 variable "encrypted" {
   type        = bool
   description = "(Optional) Enable volume encryption. (Default: false). Must be configured to perform drift detection."
   default     = true
-}
-
-variable "ephemeral_block_device" {
-  description = "Customize Ephemeral (also known as Instance Store) volumes on the instance"
-  default     = []
+  validation {
+    condition     = can(regex("^(true|false)$", var.disable_api_termination))
+    error_message = "The value must be either true or false."
+  }
 }
 
 variable "iam_instance_profile" {
+  type        = string
   description = "The IAM Instance Profile to launch the instance with. Specified as the name of the Instance Profile."
   default     = ""
 }
@@ -51,43 +64,45 @@ variable "iam_instance_profile" {
 variable "instance_initiated_shutdown_behavior" {
   # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/terminating-instances.html#Using_ChangingInstanceInitiatedShutdownBehavior
   description = "Shutdown behavior for the instance"
-  default     = ""
+  default     = null
 }
 
 variable "instance_type" {
-  description = "The AWS instance type  to utilize for the specifications of the instance"
+  type        = string
+  description = "The AWS instance type to utilize for the specifications of the instance"
 }
 
-# variable "ipv6_address_count" {
-#   description = "A number of IPv6 addresses to associate with the primary network interface. Amazon EC2 chooses the IPv6 addresses from the range of your subnet."
-#   default     = 0
-# }
-
 variable "ipv6_addresses" {
+  type        = list(string)
   description = "Specify one or more IPv6 addresses from the range of the subnet to associate with the primary network interface"
   default     = []
 }
 
 variable "key_name" {
+  type        = string
   description = "The key name to use for the instance"
   default     = ""
 }
 
 variable "monitoring" {
+  type        = bool
   description = "If true, the launched EC2 instance will have detailed monitoring enabled"
   default     = false
 }
 
 variable "name" {
+  type        = string
   description = "Name to be used on all resources as prefix"
 }
 
 variable "placement_group" {
+  type        = string
   description = "The Placement Group to start the instance in"
   default     = ""
 }
 
 variable "private_ip" {
+  type        = string
   description = "Private IP address to associate with the instance in a VPC"
   default     = null
 }
@@ -141,23 +156,35 @@ variable "root_iops" {
 }
 
 variable "source_dest_check" {
+  type       = bool
   description = "Controls if traffic is routed to the instance when the destination address does not match the instance. Used for NAT or VPNs."
   default     = true
+  validation {
+    condition     = can(regex("^(true|false)$", var.disable_api_termination))
+    error_message = "The value must be either true or false."
+  }
 }
 
 variable "subnet_id" {
+  type       = string
   description = "The VPC Subnet ID to launch in"
   default     = ""
 }
 
 variable "tags" {
+  type       = map(string)
   description = "A mapping of tags to assign to the resource"
   default     = {}
 }
 
 variable "tenancy" {
+  type        = string
   description = "The tenancy of the instance (if the instance is running in a VPC). Available values: default, dedicated, host."
   default     = "default"
+  validation {
+    condition     = can(regex("^(default|dedicated|host)$", var.tenancy))
+    error_message = "The value must be either default, dedicated or host."
+  }
 }
 
 variable "user_data" {
@@ -171,8 +198,9 @@ variable "vpc_security_group_ids" {
 }
 
 variable "launch_template_id" {
+  type        = string
   description = "the id for launch template"
-  default = ""
+  default     = null
 }
 variable "launch_template_version" {
   description = ""
