3517f08d9c27173db3be9fbfeed8a4cffee5518b
Zachary Hill <zhill@thinkstack.co>
2023-02-27 14:45:15 -0500

    added public access blocks on kinesis_firehose module

--- modules/aws/kinesis_firehose/main.tf
+++ modules/aws/kinesis_firehose/main.tf
@@ -50,6 +50,14 @@ resource "aws_s3_bucket" "firehose_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "firehose_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.firehose_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 ###########################
 # IAM Policy
 ###########################
