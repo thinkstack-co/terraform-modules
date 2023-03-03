3a2857ec1de2a0cee08cc94d771ae57e7480582c
Zachary Hill <zhill@thinkstack.co>
2023-03-03 14:13:02 -0500

    removed unused variable from aws_backup

--- modules/thinkstack/aws_backup/variables.tf
+++ modules/thinkstack/aws_backup/variables.tf
@@ -65,12 +65,6 @@ variable "vault_prod_hourly_name" {
   type        = string
 }
 
-variable "vault_dr_hourly_name" {
-  description = "value"
-  default     = "vault_dr_hourly"
-  type        = string
-}
-
 variable "vault_prod_daily_name" {
   description = "value"
   default     = "vault_prod_daily"
