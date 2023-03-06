cc753c1de7e5aff8db74084461e2da5875852408
Zachary Hill <zhill@thinkstack.co>
2023-03-06 13:19:40 -0500

    added a tab for something for terraform fmt to do

--- modules/thinkstack/siem/main.tf
+++ modules/thinkstack/siem/main.tf
@@ -19,11 +19,11 @@ data "aws_region" "current" {}
 ###########################
 
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = var.enable_dns_hostnames
-  enable_dns_support   = var.enable_dns_support
-  instance_tenancy     = var.instance_tenancy
-  tags                 = merge(var.tags, ({ "Name" = format("%s", var.name) }))
+    cidr_block           = var.vpc_cidr
+    enable_dns_hostnames = var.enable_dns_hostnames
+    enable_dns_support   = var.enable_dns_support
+    instance_tenancy     = var.instance_tenancy
+    tags                 = merge(var.tags, ({ "Name" = format("%s", var.name) }))
 }
 
 ###########################
