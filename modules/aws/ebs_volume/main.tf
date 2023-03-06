de195df77cddcd0e9bbd57771521a974c6e9ad40
Zachary Hill <zhill@thinkstack.co>
2023-03-06 17:59:20 -0500

    updated ebs volume module to use that latest module options

--- modules/aws/ebs_volume/main.tf
+++ modules/aws/ebs_volume/main.tf
@@ -9,17 +9,24 @@ terraform {
 }
 
 resource "aws_ebs_volume" "vol" {
-  availability_zone = var.availability_zone
-  encrypted         = var.encrypted
-  # iops              = var.iops
-  size        = var.size
-  snapshot_id = var.snapshot_id
-  type        = var.type
-  tags        = var.tags
+  availability_zone    = var.availability_zone
+  encrypted            = var.encrypted
+  final_snapshot       = var.final_snapshot
+  iops                 = var.iops
+  kms_key_id           = var.kms_key_id
+  multi_attach_enabled = var.multi_attach_enabled
+  size                 = var.size
+  snapshot_id          = var.snapshot_id
+  type                 = var.type
+  tags                 = var.tags
+  throughput           = var.throughput
 }
 
 resource "aws_volume_attachment" "vol_attach" {
-  device_name = var.device_name
-  instance_id = var.instance_id
-  volume_id   = aws_ebs_volume.vol.id
+  device_name                    = var.device_name
+  force_detach                   = var.force_detach
+  instance_id                    = var.instance_id
+  skip_destroy                   = var.skip_destroy
+  stop_instance_before_detaching = var.stop_instance_before_detaching
+  volume_id                      = aws_ebs_volume.vol.id
 }
