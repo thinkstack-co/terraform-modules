2ee6aba43ad078e1b6a1172461cc543ae89fa4ae
Zachary Hill <zhill@thinkstack.co>
2023-03-09 14:03:14 -0500

    added required providers and required terraform version

--- modules/aws/launch_template/main.tf
+++ modules/aws/launch_template/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
 resource "aws_launch_template" "this" {
   ebs_optimized                        = var.ebs_optimized
   image_id                             = var.image_id
