e9007c2dfd1d2b00aaa42afe8a11d7771926db5c
Zachary Hill <zhill@thinkstack.co>
2023-03-15 11:57:05 -0400

    added required providers to the module

--- modules/aws/s3/bucket/main.tf
+++ modules/aws/s3/bucket/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
 }
 
 resource "aws_s3_bucket" "this" {
