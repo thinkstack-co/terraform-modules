7622ff7ea656ae7bb5631a64530c53049f5dd88b
Zachary Hill <zhill@thinkstack.co>
2023-03-17 16:06:22 -0400

    modified default enable_s3_bucket_logging to false

--- modules/aws/s3/bucket/variables.tf
+++ modules/aws/s3/bucket/variables.tf
@@ -240,8 +240,8 @@ variable "mfa_delete" {
 
 variable "enable_s3_bucket_logging" {
   type        = bool
-  description = "(Optional) Enable logging on the cloudtrail S3 bucket. If true, the 'target_bucket' is required. Defaults to true."
-  default     = true
+  description = "(Optional) Enable logging on the cloudtrail S3 bucket. If true, the 'target_bucket' is required. Defaults to false."
+  default     = false
   validation {
     condition     = can(regex("true|false", var.enable_s3_bucket_logging))
     error_message = "The value must be true or false."
