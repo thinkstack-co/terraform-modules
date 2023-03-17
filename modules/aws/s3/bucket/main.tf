e00b09d3f4b0e729d7b74ad46e02eb354b022c6e
Zachary Hill <zhill@thinkstack.co>
2023-03-17 17:42:05 -0400

    testing a null conditional for multipart upload

--- modules/aws/s3/bucket/main.tf
+++ modules/aws/s3/bucket/main.tf
@@ -52,7 +52,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
       status = rule.value.status
 
       dynamic "abort_incomplete_multipart_upload" {
-        for_each = rule.value.abort_incomplete_multipart_upload
+        for_each = rule.value.abort_incomplete_multipart_upload == null ? [] : [rule.value.abort_incomplete_multipart_upload]
         content {
           days_after_initiation = abort_incomplete_multipart_upload.value.days_after_initiation
         }
