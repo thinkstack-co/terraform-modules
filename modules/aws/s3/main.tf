4b9a04b461a9ed41813551eaa30bb4e72783a88a
Zachary Hill <zhill@thinkstack.co>
2023-03-21 22:49:47 -0400

    Revert "Dev s3 lifecycle"

---
+++ modules/aws/s3/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  acl           = var.acl
+  bucket_prefix = var.bucket_prefix
+  policy        = var.policy
+
+  /*logging {
+        target_bucket = var.target_bucket
+        target_prefix = var.target_prefix
+    }*/
+
+  versioning {
+    enabled    = var.versioning
+    mfa_delete = var.mfa_delete
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = var.kms_master_key_id
+        sse_algorithm     = var.sse_algorithm
+      }
+    }
+  }
+
+  tags = var.tags
+}
