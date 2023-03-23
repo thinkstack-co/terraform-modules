73b059791ebb4d069de9d3170638b67d30cefa88
Zachary Hill <zhill@thinkstack.co>
2023-03-23 17:59:02 -0400

    attempting new validation for iops fix

--- modules/aws/ebs_volume/variables.tf
+++ modules/aws/ebs_volume/variables.tf
@@ -29,7 +29,7 @@ variable "iops" {
   default     = null
   validation {
     # iops must be greater than 0 or null
-    condition     = try(var.iops == null || var.iops > 0)
+    condition     = var.iops == null || try(var.iops > 0)
     error_message = "iops must be greater than 0 or null"
   }
 }
