b7e044cfedc29657064808e9788c63cd045b1cd7
Zachary Hill <zhill@thinkstack.co>
2023-03-24 16:12:40 -0400

    added bucket for importing capability since importing into bucket_prefix wants to recreate the resource

--- modules/aws/s3/bucket/main.tf
+++ modules/aws/s3/bucket/main.tf
@@ -36,6 +36,7 @@ resource "aws_kms_alias" "s3" {
 ###########################
 
 resource "aws_s3_bucket" "this" {
+  bucket              = var.bucket
   bucket_prefix       = var.bucket_prefix
   force_destroy       = var.bucket_force_destroy
   object_lock_enabled = var.bucket_object_lock_enabled
