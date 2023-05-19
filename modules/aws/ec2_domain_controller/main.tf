805e0289c14847fd18852f32dcf3602772978df2
Wesley Bey <wbey@thinkstack.co>
2023-05-19 12:44:01 -0400

    fixed variable and args for ntp_servers and ineligible resource reference

--- modules/aws/ec2_domain_controller/main.tf
+++ modules/aws/ec2_domain_controller/main.tf
@@ -74,7 +74,7 @@ resource "aws_vpc_dhcp_options" "dc_dns" {
   count               = var.enable_dhcp_options ? 1 : 0
   domain_name         = var.domain_name
   domain_name_servers = aws_instance.ec2_instance[*].private_ip
-  ntp_servers         = var.ntp_servers != null ? var.ntp_servers : aws_default_vpc_dhcp_options.default.ntp_servers
+  ntp_servers         = var.ntp_servers #!= null ? var.ntp_servers : aws_default_vpc_dhcp_options.default.ntp_servers
   tags                = merge(var.tags, ({ "Name" = format("%s-dhcp-options", var.name) }))
 }
 
@@ -83,11 +83,6 @@ resource "aws_vpc_dhcp_options_association" "dc_dns" {
   dhcp_options_id = aws_vpc_dhcp_options.dc_dns[0].id
   vpc_id          = var.vpc_id
 }
-
-resource "aws_default_vpc_dhcp_options" "default" {
-  ntp_servers = aws_instance.ec2_instance[*].private_ip
-}
-
 
 ###################################################
 # CloudWatch Alarms
