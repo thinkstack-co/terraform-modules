029ae2a6ae684ecccbfdf9945a4145cb7fe1fc32
Wesley Bey <wbey@thinkstack.co>
2023-06-12 13:29:07 -0400

    removed duplicate arg

--- modules/aws/alb/alb_ssl_cert/variables.tf
+++ modules/aws/alb/alb_ssl_cert/variables.tf
@@ -39,9 +39,4 @@ variable "certificate_arn" {
   default     = null
 }
 
-variable "tags" {
-  type        = map(string)
-  description = "A mapping of tags to assign to the resource"
-  default     = {}
-}
 
