57717ec3fa61361485b90885e0883105602277fa
Wesley Bey <wbey@thinkstack.co>
2023-06-21 17:46:26 -0400

    dynamic block fixes for nlb

--- modules/aws/nlb/nlb_listener/main.tf
+++ modules/aws/nlb/nlb_listener/main.tf
@@ -8,43 +8,32 @@ terraform {
   }
 }
 
-# AWS Load Balancer Listener
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = var.load_balancer_arn
-  port = var.port
-  protocol = var.protocol
+  port              = var.port
+  protocol          = var.protocol
 
-  # The dynamic block here is used to conditionally create a default_action block for a single target group
-  # If target_group_arn is provided (not null), the for_each loop runs once to create one default_action block
-  # If target_group_arn is not provided (null), the for_each loop does not run, so no default_action block is created
+  # This dynamic block will be executed only when target_group_arn is provided
   dynamic "default_action" {
     for_each = var.target_group_arn != null ? [1] : []
     content {
-      # The type of action
-      type = var.action_type
-
-      # The ARN of the target group
+      type             = var.action_type
       target_group_arn = var.target_group_arn
     }
   }
 
-  # The dynamic block here is used to conditionally create a default_action block for multiple target groups
-  # If target_group_arn is not provided (null) and target_groups has at least one element, the for_each loop runs once to create one default_action block
-  # If target_group_arn is provided (not null) or target_groups is empty, the for_each loop does not run, so no default_action block is created
+  # This dynamic block will be executed only when target_groups is provided
   dynamic "default_action" {
     for_each = var.target_group_arn == null && length(var.target_groups) > 0 ? [1] : []
     content {
       type = "forward"
 
+      # Forward block for multiple target groups
       forward {
-        # This dynamic block creates a target_group block for each element in target_groups
         dynamic "target_group" {
           for_each = var.target_groups
           content {
-            # The ARN of the target group
-            arn = target_group.value["arn"]
-
-            # The weight of the target group
+            arn    = target_group.value["arn"]
             weight = target_group.value["weight"]
           }
         }
@@ -52,8 +41,8 @@ resource "aws_lb_listener" "listener" {
     }
   }
 
-  # Metadata to assign to the listener
   tags = var.tags
 }
+
 
 
