54bae80b4fe09fe8b91f907fbacfa31eb5558868
Wesley Bey <wbey@thinkstack.co>
2023-06-23 13:02:06 -0400

    add alb to outputs

--- modules/aws/alb/alb_listener/main.tf
+++ modules/aws/alb/alb_listener/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 }
 
-resource "aws_lb_listener" "listener" {
+resource "aws_alb_listener" "listener" {
   load_balancer_arn = var.load_balancer_arn
   port              = var.port
   protocol          = var.protocol
