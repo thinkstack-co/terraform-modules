d9b9c91953f1aba65c0c0529730855ec6a55e243
jakeasarus <jjones@thinkstack.co>
2023-07-13 14:34:49 -0400

    Update main.tf

--- modules/aws/alb/alb_target_group/main.tf
+++ modules/aws/alb/alb_target_group/main.tf
@@ -28,7 +28,8 @@ resource "aws_lb_target_group" "this" {
 }
 
 resource "aws_lb_target_group_attachment" "this" {
+  for_each         = toset(var.target_ids)
   target_group_arn = var.target_group_arn
-  target_id        = var.target_id[count.index]
+  target_id        = each.key
   port             = var.port
 }
