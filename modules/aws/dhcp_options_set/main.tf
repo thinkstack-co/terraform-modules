1ab49321cd1ee76d37b6116cec88f5f6bc9828f7
jakeasarus <jjones@thinkstack.co>
2023-12-05 10:17:33 -0500

    added dhcp options code

---
+++ modules/aws/dhcp_options_set/main.tf
@@ -0,0 +1,28 @@
+# Setting the required version of Terraform and AWS provider
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+
+###########################
+# VPC DHCP Options
+###########################
+
+resource "aws_vpc_dhcp_options" "dc_dns" {
+  domain_name         = var.domain_name
+  domain_name_servers = var.domain_name_servers
+  ntp_servers         = var.ntp_servers
+  tags                = merge(var.tags, ({ "Name" = format("%s-dhcp-options", var.name) }))
+}
+
+resource "aws_vpc_dhcp_options_association" "dc_dns" {
+  count           = var.enable_dhcp_options ? 1 : 0
+  dhcp_options_id = aws_vpc_dhcp_options.dc_dns[0].id
+  vpc_id          = var.vpc_id
+}
---
+++ modules/aws/dhcp_options_set/variables.tf
@@ -0,0 +1,33 @@
+######################################
+# EC2 Instance Variables
+######################################
+
+variable "domain_name" {
+  description = "Enables DHCP options for the VPC"
+  type        = number
+  default     = 125
+}
+
+variable "domain_name_servers" {
+  description = "List of IP addresses for the DNS servers"
+  type        = list(string)
+  default     = ["10.11.1.100", "10.11.2.100"]
+}
+
+variable "ntp_servers" {
+  description = "List of IP addresses for the NTP servers"
+  type        = list(string)
+  default     = ["10.11.1.100", "10.11.2.100"]
+}
+
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the object."
+  default = {
+    Name        = "prod_dhcp_options_set"
+    terraform   = "true"
+    created_by  = "ThinkStack"
+    environment = "prod"
+    description = "DHCP Option Set for the VPC"
+  }
+}
+
