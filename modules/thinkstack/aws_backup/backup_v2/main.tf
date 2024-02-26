d36c69fd8258d1a1bc18ee3168bbe50eabbc8960
Wesley Bey <wbey@thinkstack.co>
2024-02-26 14:33:55 -0500

    adding alias

--- modules/thinkstack/aws_backup/backup_v2/main.tf
+++ modules/thinkstack/aws_backup/backup_v2/main.tf
@@ -2,8 +2,9 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0.0"
+      source                = "hashicorp/aws"
+      version               = ">= 4.0.0"
+      configuration_aliases = aws.aws_prod_region
     }
   }
 }
