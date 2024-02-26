611adeece6b3f375cc4209a28316d199c1f82dc7
Wesley Bey <wbey@thinkstack.co>
2024-02-26 14:25:45 -0500

    removed config alias

--- modules/thinkstack/aws_backup/backup_v2/main.tf
+++ modules/thinkstack/aws_backup/backup_v2/main.tf
@@ -2,9 +2,8 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 4.0.0"
-      configuration_aliases = [aws.aws_prod_region]
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
     }
   }
 }
