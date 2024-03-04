14cecb492a8f7ac7d7ebda5e16343caf73ec8c4a
Wesley Bey <wbey@thinkstack.co>
2024-03-04 12:07:17 -0500

    added arn as an ouputs to ec2 for backups

--- modules/aws/ec2_instance/outputs.tf
+++ modules/aws/ec2_instance/outputs.tf
@@ -52,3 +52,9 @@ output "subnet_id" {
   description = "List of IDs of VPC subnets of instances"
   value       = aws_instance.ec2[*].subnet_id
 }
+
+output "instance_arn" {
+  description = "List of IDs of VPC subnets of instances"
+  value       = aws_instance.ec2[*].arn
+}
+
