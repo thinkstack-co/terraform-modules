45d86e92b0f73105c62f72a41b38fee9746b7408
Wesley Bey <wbey@thinkstack.co>
2025-02-24 12:41:54 -0600

    fixed outputs.tf for config module

--- modules/aws/config/outputs.tf
+++ modules/aws/config/outputs.tf
@@ -7,7 +7,7 @@ output "config_recorder_id" {
 
 output "config_bucket_arn" {
   description = "The ARN of the S3 bucket used for AWS Config recordings"
-  value       = aws_s3_bucket.config.arn
+  value       = aws_s3_bucket.config_bucket.arn
 }
 
 output "config_iam_role_arn" {
@@ -17,7 +17,7 @@ output "config_iam_role_arn" {
 
 output "config_bucket_id" {
   description = "The ID of the S3 bucket used for AWS Config recordings"
-  value       = aws_s3_bucket.config.id
+  value       = aws_s3_bucket.config_bucket.id
 }
 
 # Config Rules Outputs
@@ -35,10 +35,6 @@ output "config_rules_arns" {
   description = "Map of all Config rules ARNs"
   value = {
     password_policy = aws_config_config_rule.iam_password_policy.arn
-    ebs_encryption = aws_config_config_rule.ebs_encryption.arn
-    ec2_monitoring = aws_config_config_rule.ec2_detailed_monitoring.arn
-    s3_public_access = aws_config_config_rule.s3_public_access.arn
-    root_mfa = aws_config_config_rule.root_mfa.arn
-    iam_user_mfa = aws_config_config_rule.iam_user_mfa.arn
+    ebs_encryption  = aws_config_config_rule.ebs_encryption.arn
   }
 }
