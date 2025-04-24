0a252eaf0821fa28232b28005c29f769ee0420e8
Wesley Bey <wbey@thinkstack.co>
2025-04-24 13:39:01 -0500

    more tflint fixes and old module removals

--- modules/aws/lambda_event_source_mapping/variables.tf
+++ modules/aws/lambda_event_source_mapping/variables.tf
@@ -19,13 +19,3 @@ variable "function_name" {
   type        = string
   description = "(Required) The name or the ARN of the Lambda function that will be subscribing to events."
 }
-
-variable "starting_position" {
-  description = "Optional) The position in the stream where AWS Lambda should start reading. Must be one of AT_TIMESTAMP (Kinesis only), LATEST or TRIM_HORIZON if getting events from Kinesis or DynamoDB. Must not be provided if getting events from SQS. More information about these positions can be found in the AWS DynamoDB Streams API Reference and AWS Kinesis API Reference."
-  default     = ""
-}
-
-variable "starting_position_timestamp" {
-  description = "(Optional) A timestamp in RFC3339 format of the data record which to start reading when using starting_position set to AT_TIMESTAMP. If a record with this exact timestamp does not exist, the next later record is chosen. If the timestamp is older than the current trim horizon, the oldest available record is chosen."
-  default     = ""
-}
--- modules/aws/security_group/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = aws_security_group.sg.id
-}
-
-output "name" {
-  value = aws_security_group.sg.name
-}
