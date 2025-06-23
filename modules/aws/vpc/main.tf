f32b7de25e0bbf607cb35f984c4f5a158bd1f777
Wesley Bey <wbey@thinkstack.co>
2025-06-23 09:47:00 -0500

    updating provider

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0.0, < 4.0.0"
+      version = ">= 4.0.0"
     }
   }
 }
@@ -212,7 +212,7 @@ resource "aws_route" "public_default_route" {
 
 resource "aws_eip" "nateip" {
   count = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.azs)) : 0
-  vpc   = true
+  domain = "vpc"
 }
 
 resource "aws_nat_gateway" "natgw" {
