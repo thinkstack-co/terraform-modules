7a57f14a0d6c1a6e65f32b48a53d794ad7c7841c
Wesley Bey <wbey@thinkstack.co>
2026-01-29 15:46:04 -0500

    added vpn connection output for vpn routes

--- modules/aws/vpn/outputs.tf
+++ modules/aws/vpn/outputs.tf
@@ -9,3 +9,11 @@ output "customer_gateway_id" {
 output "customer_gateway_type" {
   value = aws_customer_gateway.customer_gateway[*].type
 }
+
+# Expose VPN connection IDs for downstream route modules.
+output "vpn_connection_id" {
+  value = concat(
+    aws_vpn_connection.vpn_connection_transit_gateway_attachment[*].id,
+    aws_vpn_connection.vpn_connection_vpn_gateway_attachment[*].id
+  )
+}
